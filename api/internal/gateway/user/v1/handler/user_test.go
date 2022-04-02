046a2f2a7a9a48ddd083eccfcc73de95d0f41b79
nishi <ta.g-h-r.1260@docomo.ne.jp>
2022-04-03 00:31:26 +0900

    feat(gateway): implement create user api (#22)

--- api/internal/gateway/user/v1/handler/user_test.go
+++ api/internal/gateway/user/v1/handler/user_test.go
@@ -79,8 +79,17 @@ func TestCreateUser(t *testing.T) {
 		expect *testResponse
 	}{
 		{
-			name:  "success",
-			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {},
+			name: "success",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				in := &user.CreateUserRequest{
+					Email:                "test-user@and-period.jp",
+					PhoneNumber:          "+819012345678",
+					Password:             "!Qaz2wsx",
+					PasswordConfirmation: "!Qaz2wsx",
+				}
+				out := &user.CreateUserResponse{UserId: "user-id"}
+				mocks.user.EXPECT().CreateUser(gomock.Any(), in).Return(out, nil)
+			},
 			req: &request.CreateUserRequest{
 				Email:                "test-user@and-period.jp",
 				PhoneNumber:          "+819012345678",
@@ -89,7 +98,30 @@ func TestCreateUser(t *testing.T) {
 			},
 			expect: &testResponse{
 				code: http.StatusOK,
-				body: &response.CreateUserResponse{},
+				body: &response.CreateUserResponse{
+					ID: "user-id",
+				},
+			},
+		},
+		{
+			name: "failed to create user",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				in := &user.CreateUserRequest{
+					Email:                "test-user@and-period.jp",
+					PhoneNumber:          "+819012345678",
+					Password:             "!Qaz2wsx",
+					PasswordConfirmation: "!Qaz2wsx",
+				}
+				mocks.user.EXPECT().CreateUser(gomock.Any(), in).Return(nil, errmock)
+			},
+			req: &request.CreateUserRequest{
+				Email:                "test-user@and-period.jp",
+				PhoneNumber:          "+819012345678",
+				Password:             "!Qaz2wsx",
+				PasswordConfirmation: "!Qaz2wsx",
+			},
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
 			},
 		},
 	}
@@ -115,14 +147,38 @@ func TestVerifyUser(t *testing.T) {
 		expect *testResponse
 	}{
 		{
-			name:  "success",
-			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {},
+			name: "success",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				in := &user.VerifyUserRequest{
+					UserId:     "user-id",
+					VerifyCode: "123456",
+				}
+				out := &user.VerifyUserResponse{}
+				mocks.user.EXPECT().VerifyUser(gomock.Any(), in).Return(out, nil)
+			},
 			req: &request.VerifyUserRequest{
 				ID:         "user-id",
 				VerifyCode: "123456",
 			},
 			expect: &testResponse{
 				code: http.StatusNoContent,
+			},
+		},
+		{
+			name: "failed to verify user",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				in := &user.VerifyUserRequest{
+					UserId:     "user-id",
+					VerifyCode: "123456",
+				}
+				mocks.user.EXPECT().VerifyUser(gomock.Any(), in).Return(nil, errmock)
+			},
+			req: &request.VerifyUserRequest{
+				ID:         "user-id",
+				VerifyCode: "123456",
+			},
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
 			},
 		},
 	}
@@ -141,17 +197,46 @@ func TestVerifyUser(t *testing.T) {
 func TestCreateUserWithOAuth(t *testing.T) {
 	t.Parallel()
 
+	now := jst.Date(2022, 1, 2, 18, 30, 0, 0)
+	u := &user.User{
+		Id:           "user-id",
+		ProviderType: user.ProviderType_PROVIDER_TYPE_EMAIL,
+		Email:        "test@and-period.jp",
+		PhoneNumber:  "+819012345678",
+		CreatedAt:    now.Unix(),
+		UpdatedAt:    now.Unix(),
+		VerifiedAt:   now.Unix(),
+	}
+
 	tests := []struct {
 		name   string
 		setup  func(t *testing.T, mocks *mocks, ctrl *gomock.Controller)
 		expect *testResponse
 	}{
 		{
-			name:  "success",
-			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {},
+			name: "success",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				in := &user.CreateUserWithOAuthRequest{AccessToken: tokenmock}
+				out := &user.CreateUserWithOAuthResponse{User: u}
+				mocks.user.EXPECT().CreateUserWithOAuth(gomock.Any(), in).Return(out, nil)
+			},
 			expect: &testResponse{
 				code: http.StatusOK,
-				body: &response.UserMeResponse{},
+				body: &response.UserMeResponse{
+					ID:          "user-id",
+					Email:       "test@and-period.jp",
+					PhoneNumber: "+819012345678",
+				},
+			},
+		},
+		{
+			name: "failed to create user",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				in := &user.CreateUserWithOAuthRequest{AccessToken: tokenmock}
+				mocks.user.EXPECT().CreateUserWithOAuth(gomock.Any(), in).Return(nil, errmock)
+			},
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
 			},
 		},
 	}
