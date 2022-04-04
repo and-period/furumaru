8ecc7a20754b405f3e897b088b3ad5eefecd90b1
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-04-04 23:11:17 +0900

    feat: i18nの設定ファイルを配置

---
+++ web/user/src/locales/ja_jp.ts
@@ -0,0 +1,16 @@
+import { I18n } from '~/types/locales'
+
+const lang: I18n = {
+  auth: {
+    signUp: {
+      tel: '電話番号',
+      email: 'メールアドレス',
+      password: 'パスワード',
+      passwordConfirm: 'パスワード（確認用）',
+      signUp: 'サインアップ',
+      alreadyHas: 'すでにアカウントをお持ちですか？',
+    },
+  },
+}
+
+export default lang
