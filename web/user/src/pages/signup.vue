7eb7ef33d2923c37d79174ff667c6faefd7bbbbe
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-04-06 00:04:29 +0900

    feat: デザインだけ実装

--- web/user/src/pages/signup.vue
+++ web/user/src/pages/signup.vue
@@ -1,33 +1,46 @@
 <template>
   <div>
-    <v-card outlined>
-      <v-card-text class="pa-12">
+    <v-card outlined class="mx-auto" :max-width="isMobile ? 360 : 440">
+      <v-card-text class="pa-md-12 pa-sm-4">
         <form>
           <v-text-field
             type="tel"
             :label="$t('auth.signUp.tel')"
             outlined
             dense
+            required
           />
           <v-text-field
             type="email"
             :label="$t('auth.signUp.email')"
             outlined
             dense
+            required
           />
           <v-text-field
             type="password"
             :label="$t('auth.signUp.password')"
             outlined
             dense
+            required
           />
           <v-text-field
             type="password"
             :label="$t('auth.signUp.passwordConfirm')"
             outlined
             dense
+            required
           />
-          <v-btn block type="submit">{{ $t('auth.signUp.signUp') }}</v-btn>
+          <v-btn
+            class="rounded-xl"
+            color="accent"
+            block
+            type="submit"
+            elevation="0"
+            :height="isMobile ? 40 : 64"
+            :x-large="!isMobile"
+            >{{ $t('auth.signUp.signUp') }}</v-btn
+          >
         </form>
       </v-card-text>
     </v-card>
@@ -38,9 +51,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, useContext, computed } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  setup() {},
+  layout: 'auth',
+  setup() {
+    const { $vuetify } = useContext()
+    const isMobile = computed(() =>
+      ['sm', 'xs'].includes($vuetify.breakpoint.name)
+    )
+
+    return {
+      isMobile,
+    }
+  },
 })
 </script>
