ea2257caa39fa2e3b34311c38665cb1ba0adc041
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-04-17 22:42:51 +0900

    chore: pincodeコンポーネントのライブラリを導入

--- web/user/nuxt.config.ts
+++ web/user/nuxt.config.ts
@@ -29,7 +29,12 @@ const config: NuxtConfig = {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    {
+      src: '@/plugins/pincodeInput',
+      mode: 'client',
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
