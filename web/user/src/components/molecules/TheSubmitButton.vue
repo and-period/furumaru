8cda0bb38ff65ae1d9d88760f5844d3bac1ef2f5
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-04-17 22:44:02 +0900

    feat: Submitボタンのコンポーネントを用意

---
+++ web/user/src/components/molecules/TheSubmitButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn
+    class="rounded-xl"
+    color="accent"
+    block
+    type="submit"
+    elevation="0"
+    :height="isMobile ? 40 : 64"
+    :x-large="!isMobile"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    isMobile: {
+      type: Boolean,
+      required: true,
+    },
+  },
+})
+</script>
