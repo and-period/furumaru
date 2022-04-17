f50f68ea84a1ff8438e3e8a4ea7b3a91331002d2
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-04-17 22:43:36 +0900

    feat: pincode用のコンポーネントを作成

---
+++ web/user/src/components/molecules/ThePincodeInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <client-only>
+    <pincode-input v-model="formData" :length="length" placeholder="*" />
+  </client-only>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  SetupContext,
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    length: {
+      type: Number,
+      default: 4,
+    },
+  },
+  setup(props, { emit }: SetupContext) {
+    const formData = computed({
+      get: () => props.value,
+      set: (val: string) => emit('update:value', val),
+    })
+
+    return { formData }
+  },
+})
+</script>
