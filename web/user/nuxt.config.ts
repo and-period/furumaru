1f269bfacdcbe69c0b0ee38c0964ab3bf79eec8e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-04-21 22:28:52 +0900

    fix: @→~

--- web/user/nuxt.config.ts
+++ web/user/nuxt.config.ts
@@ -31,7 +31,7 @@ const config: NuxtConfig = {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {
-      src: '@/plugins/pincodeInput',
+      src: '~/plugins/pincodeInput',
       mode: 'client',
     },
   ],
