0345fdaea65fe36349a36dccbd27f305e4918a1c
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-05-05 18:09:37 +0900

    fix(config): fix small

--- infra/mysql/schema/2022050302-stores-create-stores.sql
+++ infra/mysql/schema/2022050302-stores-create-stores.sql
@@ -1,7 +1,7 @@
-CREATE SCHEMA IF NOT EXISTS `shops` DEFAULT CHARACTER SET utf8mb4;
+CREATE SCHEMA IF NOT EXISTS `stores` DEFAULT CHARACTER SET utf8mb4;
 
 -- 店舗情報テーブル
-CREATE TABLE `shops`.`stores` (
+CREATE TABLE `stores`.`stores` (
   `id`            BIGINT       NOT NULL AUTO_INCREMENT, -- 商品ID
   `name`          VARCHAR(64)  NOT NULL,                -- 店舗名
   `thumbnail_url` TEXT         NOT NULL,                -- サムネイルURL
@@ -11,10 +11,10 @@ CREATE TABLE `shops`.`stores` (
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-CREATE UNIQUE INDEX `ui_stores_name` ON `shops`.`stores` (`name` ASC) VISIBLE;
+CREATE UNIQUE INDEX `ui_stores_name` ON `stores`.`stores` (`name` ASC) VISIBLE;
 
 -- 店舗スタッフ情報テーブル
-CREATE TABLE `shops`.`staffs` (
+CREATE TABLE `stores`.`staffs` (
   `store_id`     BIGINT       NOT NULL, -- 店舗ID
   `user_id`      VARCHAR(22)  NOT NULL, -- 販売者ID
   `role`         INT          NOT NULL, -- 権限
@@ -22,6 +22,8 @@ CREATE TABLE `shops`.`staffs` (
   `updated_at`   DATETIME     NOT NULL, -- 更新日時
   PRIMARY KEY (`store_id`, `user_id`),
   CONSTRAINT `fk_staffs_store_id`
-    FOREIGN KEY (`store_id`) REFERENCES `shops`.`stores` (`id`)
+    FOREIGN KEY (`store_id`) REFERENCES `stores`.`stores` (`id`)
     ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE = InnoDB;
+
+CREATE INDEX `idx_staffs_user_id` ON `stores`.`staffs` (`user_id` ASC) VISIBLE;
