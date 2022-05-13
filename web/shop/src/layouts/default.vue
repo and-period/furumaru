f0fde71f01ffc8809bd82f856539fdc19bf8566c
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-05-13 22:52:47 +0900

    Shop側のフロントエンドプロジェクトのセットアップ (#55)
    * feat: layoutファイルを整える
    
    * setup: Lintの設定を更新
    
    * feat: 最低限のページを配置

--- web/shop/src/layouts/default.vue
+++ web/shop/src/layouts/default.vue
@@ -1,92 +1,71 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer permanent app clipped>
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
+          v-for="(item, i) in navigationDrawerList"
           :key="i"
           :to="item.to"
           router
           exact
         >
-          <v-list-item-action>
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+
+    <v-app-bar flat app clipped-left>
+      <v-toolbar-title>Online Marche</v-toolbar-title>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+    <v-footer absolute app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'DefaultLayout',
-  data() {
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+interface NavigationDrawerItem {
+  to: string
+  icon: string
+  title: string
+}
+
+export default defineComponent({
+  setup() {
+    const navigationDrawerList = ref<NavigationDrawerItem[]>([
+      {
+        to: '/',
+        icon: 'mdi-home',
+        title: 'ホーム',
+      },
+      {
+        to: '/products',
+        icon: 'mdi-tag',
+        title: '商品管理',
+      },
+      {
+        to: '/livestreaming',
+        icon: 'mdi-antenna',
+        title: 'ライブ配信',
+      },
+    ])
+
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      navigationDrawerList,
     }
   },
-}
+})
 </script>
