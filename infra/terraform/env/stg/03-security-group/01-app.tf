a50770e30879ef62c6158e9db2b547f13a2fecd7
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-06-02 23:48:55 +0900

    feat(infra): setup network

---
+++ infra/terraform/env/stg/03-security-group/01-app.tf
@@ -0,0 +1,48 @@
+module "sg_app" {
+  source = "./../../../modules/security-group"
+
+  #####################################################################
+  # Common (下で使う共通の変数)
+  #####################################################################
+  vpc_name = var.vpc_name
+  tags     = var.tags
+
+  #####################################################################
+  # [必須] Security Group
+  #####################################################################
+  name        = "furumaru-stg-sg-app"
+  description = "for database"
+
+  ingress_rules = [
+    {
+      description          = "all icmp"
+      protocol             = "icmp"
+      from_port            = 0
+      to_port              = 0
+      cidr_blocks          = ["0.0.0.0/0"]
+      prefix_list_ids      = []
+      security_group_names = []
+    },
+    {
+      description          = "http from external"
+      protocol             = "tcp"
+      from_port            = 80
+      to_port              = 80
+      cidr_blocks          = ["0.0.0.0/0"]
+      prefix_list_ids      = []
+      security_group_names = []
+    },
+  ]
+
+  egress_rules = [
+    {
+      description          = "all"
+      protocol             = "-1"
+      from_port            = 0
+      to_port              = 0
+      cidr_blocks          = ["0.0.0.0/0"]
+      prefix_list_ids      = []
+      security_group_names = []
+    },
+  ]
+}
