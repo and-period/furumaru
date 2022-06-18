5d7d29dc83e2be66091f2f521df06236441c0bc9
taba2424 <tadashi-112@hotmail.co.jp>
2022-06-18 18:46:29 +0900

    refactor(gateway): remove unnecessary method

--- api/internal/gateway/util/metadata_test.go
+++ api/internal/gateway/util/metadata_test.go
@@ -1,14 +1,12 @@
 package util
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
-	gmd "google.golang.org/grpc/metadata"
 )
 
 func TestGetAuthToken(t *testing.T) {
@@ -54,34 +52,5 @@ func TestGetAuthToken(t *testing.T) {
 			assert.Equal(t, tt.isErr, err != nil, err)
 			assert.Equal(t, tt.expect, actual)
 		})
-	}
-}
-
-func TestSetMetadata(t *testing.T) {
-	t.Parallel()
-	gin.SetMode(gin.TestMode)
-
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-	c.Request = &http.Request{Header: http.Header{}}
-	c.Request.Header.Set("Authorization", "Bearer xxxxxx")
-	c.Request.Header.Set("userId", "userid")
-	c.Request.Header.Set("role", "role")
-
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, "Authorization", "Bearer xxxxxx")
-	ctx = context.WithValue(ctx, "userId", "userid")
-	ctx = context.WithValue(ctx, "role", "role")
-
-	actual := SetMetadata(c)
-	assert.IsType(t, ctx, actual)
-	md, ok := gmd.FromOutgoingContext(actual)
-	if ok {
-		assert.Contains(t, md.Get("Authorization"), "Bearer xxxxxx")
-		assert.Contains(t, md.Get("userId"), "userid")
-		assert.Contains(t, md.Get("role"), "role")
-		assert.NotContains(t, md.Get("X-Request-ID"), "")
-	} else {
-		t.FailNow()
 	}
 }
