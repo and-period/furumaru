1e0cfb2303e4f83501dcced73040318f35181e52
kaito <sea_rr10_ch@outlook.com>
2022-06-22 23:34:32 +0900

    style: change public culumn name

--- infra/mysql/schema/2022061801-messengers-create-notifications.sql
+++ infra/mysql/schema/2022061801-messengers-create-notifications.sql
@@ -9,7 +9,7 @@ CREATE TABLE `messengers`.`notifications` (
   `body`         TEXT         NOT NULL,          -- 本文
   `published_at` DATETIME     NOT NULL,          -- 掲載開始時間
   `targets`      JSON         NULL DEFAULT NULL, -- 掲載範囲(PostTarget[])
-  `is_public`    TINYINT      NOT NULL,          -- 公開フラグ
+  `public`       TINYINT      NOT NULL,          -- 公開フラグ
   `created_at`   DATETIME     NOT NULL,          -- 登録日時
   `updated_at`   DATETIME     NOT NULL,          -- 更新日時
   PRIMARY KEY(`id`)
