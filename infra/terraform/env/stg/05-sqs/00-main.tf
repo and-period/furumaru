2592455ebbf7d7d80969f1aa26e0087d8a679724
taba2424 <tadashi-112@hotmail.co.jp>
2022-06-23 02:25:48 +0900

    feat(infra): create aws sqs

---
+++ infra/terraform/env/stg/05-sqs/00-main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+terraform {
+  backend "s3" {
+    region                  = "ap-northeast-1"
+    bucket                  = "furumaru-terraform"
+    key                     = "stg/sqs.tfstate"
+    profile                 = "default"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+}
