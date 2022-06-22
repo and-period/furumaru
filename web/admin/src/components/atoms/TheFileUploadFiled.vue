343e8b8298175b459f60791a49bcf61db4343429
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-06-23 00:01:47 +0900

    fix: ラジオボタンに変更

--- web/admin/src/components/atoms/TheFileUploadFiled.vue
+++ web/admin/src/components/atoms/TheFileUploadFiled.vue
@@ -25,15 +25,17 @@
       </p>
     </div>
     <v-list v-if="files">
-      <div
-        v-for="(file, i) in files"
-        :key="i"
-        class="d-flex flex-row align-center"
-      >
-        <v-checkbox />
-        <img :src="createPreviewURL(file)" width="200" class="mx-4" />
-        <p class="mb-0">{{ file.name }}</p>
-      </div>
+      <v-radio-group>
+        <div
+          v-for="(file, i) in files"
+          :key="i"
+          class="d-flex flex-row align-center"
+        >
+          <v-radio :value="i" />
+          <img :src="createPreviewURL(file)" width="200" class="mx-4" />
+          <p class="mb-0">{{ file.name }}</p>
+        </div>
+      </v-radio-group>
     </v-list>
   </div>
 </template>
