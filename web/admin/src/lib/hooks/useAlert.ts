0f761124e1bd75dfa5deb2ab27212133e08dbfba
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-06-26 22:09:00 +0900

    feat: アラートコンポーネントを使いやすくするhookを用意

---
+++ web/admin/src/lib/hooks/useAlert.ts
@@ -0,0 +1,27 @@
+import { ref } from '@nuxtjs/composition-api'
+
+type AlertType = 'success' | 'info' | 'warning' | 'error'
+
+export function useAlert(alertType: AlertType) {
+  const isShow = ref<boolean>(false)
+  const alertText = ref<string>('')
+
+  const show = (text?: string) => {
+    if (text) {
+      alertText.value = text
+    }
+    isShow.value = true
+  }
+
+  const hide = () => {
+    isShow.value = false
+  }
+
+  return {
+    alertType,
+    isShow,
+    alertText,
+    show,
+    hide,
+  }
+}
