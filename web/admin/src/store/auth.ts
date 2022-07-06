c301ce5e4b2129793613fbcb5dd65ea8937bd7c4
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-07-06 23:48:56 +0900

    feat: リフレッシュトークンで認証情報を取得する関数の追加

--- web/admin/src/store/auth.ts
+++ web/admin/src/store/auth.ts
@@ -2,7 +2,11 @@ import { defineStore } from 'pinia'
 
 import { ApiClientFactory } from '.'
 
+import Cookies from 'universal-cookie'
+
 import { AuthApi, AuthResponse, SignInRequest } from '~/types/api'
+
+import axios from 'axios'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
@@ -22,8 +26,32 @@ export const useAuthStore = defineStore('auth', {
         const res = await authApiClient.v1SignIn(payload)
         this.isAuthenticated = true
         this.user = res.data
+
+        const cookies = new Cookies()
+        cookies.set('refreshToken', this.user.refreshToken)
       } catch (err) {
         // TODO: エラーハンドリング
+        throw new Error('Internal Server Error')
+      }
+    },
+    async getAuthByRefreshToken(refreshToken: string): Promise<void> {
+      try {
+        const factory = new ApiClientFactory()
+        const authApiClient = factory.create(AuthApi)
+        const res = await authApiClient.v1RefreshAuthToken({
+          refreshToken,
+        })
+        this.isAuthenticated = true
+        this.user = res.data
+
+        const cookies = new Cookies()
+        cookies.set('refreshToken', this.user.refreshToken)
+      } catch (error) {
+        const cookies = new Cookies()
+        cookies.remove('refreshToken')
+        if (axios.isAxiosError(error)) {
+          throw new Error(error.message)
+        }
         throw new Error('Internal Server Error')
       }
     },
