e4187c812316284f1da6a8adaab12cee9b229dae
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-07-09 23:48:26 +0900

    feat: 生産者登録の部分を実装

--- web/admin/src/store/producer.ts
+++ web/admin/src/store/producer.ts
@@ -4,7 +4,11 @@ import { useAuthStore } from './auth'
 
 import { ApiClientFactory } from '.'
 
-import { ProducerApi, ProducersResponse } from '~/types/api'
+import {
+  CreateProducerRequest,
+  ProducerApi,
+  ProducersResponse,
+} from '~/types/api'
 
 export const useProducerStore = defineStore('Producer', {
   state: () => ({
@@ -23,6 +27,25 @@ export const useProducerStore = defineStore('Producer', {
         this.producers = res.data.producers
       } catch (error) {
         // TODO: エラーハンドリング
+        throw new Error('Internal Server Error')
+      }
+    },
+
+    /**
+     * @param payload
+     */
+    async createProducer(payload: CreateProducerRequest): Promise<void> {
+      try {
+        const authStore = useAuthStore()
+        const accessToken = authStore.accessToken
+        if (!accessToken) throw new Error('認証エラー')
+
+        const factory = new ApiClientFactory()
+        const producersApiClient = factory.create(ProducerApi, accessToken)
+        await producersApiClient.v1CreateProducer(payload)
+      } catch (error) {
+        // TODO: エラーハンドリング
+        console.log(error)
         throw new Error('Internal Server Error')
       }
     },
