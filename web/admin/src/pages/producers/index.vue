38d33b957f90fafb163cf62a5051c8ad4ca61a02
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-07-10 15:28:45 +0900

    feat: 生産者一覧の実装

--- web/admin/src/pages/producers/index.vue
+++ web/admin/src/pages/producers/index.vue
@@ -3,32 +3,131 @@
     <v-card-title>生産者管理</v-card-title>
     <div class="d-flex">
       <v-spacer />
-      <v-btn outlined @click="handleClickAddButton">
+      <v-btn outlined color="primary" @click="handleClickAddButton">
         <v-icon left>mdi-plus</v-icon>
-        追加
+        生産者登録
       </v-btn>
     </div>
-    {{ producers }}
+    <v-card class="mt-4" flat :loading="fetchState.pending">
+      <v-card-text>
+        <form class="d-flex align-center" @submit.prevent="handleSearch">
+          <v-text-field v-model="search" label="絞り込み" />
+          <v-btn type="submit" class="ml-4" small outlined color="primary">
+            <v-icon>mdi-search</v-icon>
+            検索
+          </v-btn>
+          <v-spacer />
+        </form>
+        <v-data-table
+          show-select
+          :headers="headers"
+          :items="producers"
+          :search="query"
+          :no-results-text="noResultsText"
+        >
+          <template #item.thumbnail="{ item }">
+            <v-avatar>
+              <img
+                v-if="item.thumbnailUrl !== ''"
+                :src="item.thumbnailUrl"
+                :alt="`${item.storeName}-profile`"
+              />
+              <v-icon v-else>mdi-account</v-icon>
+            </v-avatar>
+          </template>
+          <template #item.name="{ item }">
+            {{ `${item.lastname} ${item.firstname}` }}
+          </template>
+          <template #item.actions="{ item }">
+            <v-btn outlined color="primary" small @click="handleEdit(item)">
+              <v-icon>mdi-plus</v-icon>
+              編集
+            </v-btn>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useFetch, useRouter } from '@nuxtjs/composition-api'
+import {
+  computed,
+  defineComponent,
+  ref,
+  useFetch,
+  useRouter,
+  watch,
+} from '@nuxtjs/composition-api'
+import { DataTableHeader } from 'vuetify'
 
 import { useProducerStore } from '~/store/producer'
+import { ProducersResponseProducers } from '~/types/api'
 
 export default defineComponent({
   setup() {
     const router = useRouter()
-    const { producers, fetchProducers } = useProducerStore()
+    const producerStore = useProducerStore()
+    const producers = computed(() => {
+      return producerStore.producers
+    })
+
+    const search = ref<string>('')
+    const query = ref<string>('')
+
+    const noResultsText = computed(() => {
+      return `「${query.value}」に一致するデータはありません。`
+    })
+
+    watch(search, () => {
+      if (search.value === '') {
+        query.value = ''
+      }
+    })
+
+    const headers: DataTableHeader[] = [
+      {
+        text: 'サムネイル',
+        value: 'thumbnail',
+      },
+      {
+        text: '農園名',
+        value: 'storeName',
+      },
+      {
+        text: '生産者名',
+        value: 'name',
+      },
+      {
+        text: 'Email',
+        value: 'email',
+      },
+      {
+        text: '電話番号',
+        value: 'phoneNumber',
+      },
+      {
+        text: 'Actions',
+        value: 'actions',
+        sortable: false,
+      },
+    ]
 
     const handleClickAddButton = () => {
       router.push('/producers/add')
     }
 
-    useFetch(async () => {
+    const handleSearch = () => {
+      query.value = search.value
+    }
+
+    const handleEdit = (item: ProducersResponseProducers) => {
+      console.log(item)
+    }
+
+    const { fetchState } = useFetch(async () => {
       try {
-        await fetchProducers()
+        await producerStore.fetchProducers()
       } catch (err) {
         console.log(err)
       }
@@ -36,7 +135,14 @@ export default defineComponent({
 
     return {
       handleClickAddButton,
+      headers,
       producers,
+      search,
+      query,
+      noResultsText,
+      fetchState,
+      handleSearch,
+      handleEdit,
     }
   },
 })
