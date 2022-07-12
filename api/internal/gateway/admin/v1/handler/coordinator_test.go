44df4a5900a76f662dd97d607214bb65b39a9452
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-07-12 23:25:31 +0900

    feat(gateway): implement coordinator api

---
+++ api/internal/gateway/admin/v1/handler/coordinator_test.go
@@ -0,0 +1,404 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/and-period/furumaru/api/internal/gateway/admin/v1/request"
+	"github.com/and-period/furumaru/api/internal/gateway/admin/v1/response"
+	"github.com/and-period/furumaru/api/internal/user"
+	uentity "github.com/and-period/furumaru/api/internal/user/entity"
+	"github.com/and-period/furumaru/api/pkg/jst"
+	"github.com/golang/mock/gomock"
+)
+
+func TestListCoordinator(t *testing.T) {
+	t.Parallel()
+
+	in := &user.ListCoordinatorsInput{
+		Limit:  20,
+		Offset: 0,
+	}
+	coordinators := uentity.Coordinators{
+		{
+			ID:               "coordinator-id01",
+			Lastname:         "&.",
+			Firstname:        "管理者",
+			LastnameKana:     "あんどどっと",
+			FirstnameKana:    "かんりしゃ",
+			CompanyName:      "&.株式会社",
+			StoreName:        "&.農園",
+			ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+			HeaderURL:        "https://and-period.jp/header.png",
+			TwitterAccount:   "twitter-id",
+			InstagramAccount: "instagram-id",
+			FacebookAccount:  "facebook-id",
+			Email:            "test-coordinator@and-period.jp",
+			PhoneNumber:      "+819012345678",
+			PostalCode:       "1000014",
+			Prefecture:       "東京都",
+			City:             "千代田区",
+			CreatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+			UpdatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+		},
+		{
+			ID:               "coordinator-id02",
+			Lastname:         "&.",
+			Firstname:        "管理者",
+			LastnameKana:     "あんどどっと",
+			FirstnameKana:    "かんりしゃ",
+			CompanyName:      "&.株式会社",
+			StoreName:        "&.農園",
+			ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+			HeaderURL:        "https://and-period.jp/header.png",
+			TwitterAccount:   "twitter-id",
+			InstagramAccount: "instagram-id",
+			FacebookAccount:  "facebook-id",
+			Email:            "test-coordinator@and-period.jp",
+			PhoneNumber:      "+819012345678",
+			PostalCode:       "1000014",
+			Prefecture:       "東京都",
+			City:             "千代田区",
+			CreatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+			UpdatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+		},
+	}
+
+	tests := []struct {
+		name   string
+		setup  func(t *testing.T, mocks *mocks, ctrl *gomock.Controller)
+		query  string
+		expect *testResponse
+	}{
+		{
+			name: "success",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				mocks.user.EXPECT().ListCoordinators(gomock.Any(), in).Return(coordinators, nil)
+			},
+			query: "",
+			expect: &testResponse{
+				code: http.StatusOK,
+				body: &response.CoordinatorsResponse{
+					Coordinators: []*response.Coordinator{
+						{
+							ID:               "coordinator-id01",
+							Lastname:         "&.",
+							Firstname:        "管理者",
+							LastnameKana:     "あんどどっと",
+							FirstnameKana:    "かんりしゃ",
+							CompanyName:      "&.株式会社",
+							StoreName:        "&.農園",
+							ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+							HeaderURL:        "https://and-period.jp/header.png",
+							TwitterAccount:   "twitter-id",
+							InstagramAccount: "instagram-id",
+							FacebookAccount:  "facebook-id",
+							Email:            "test-coordinator@and-period.jp",
+							PhoneNumber:      "+819012345678",
+							PostalCode:       "1000014",
+							Prefecture:       "東京都",
+							City:             "千代田区",
+							CreatedAt:        1640962800,
+							UpdatedAt:        1640962800,
+						},
+						{
+							ID:               "coordinator-id02",
+							Lastname:         "&.",
+							Firstname:        "管理者",
+							LastnameKana:     "あんどどっと",
+							FirstnameKana:    "かんりしゃ",
+							CompanyName:      "&.株式会社",
+							StoreName:        "&.農園",
+							ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+							HeaderURL:        "https://and-period.jp/header.png",
+							TwitterAccount:   "twitter-id",
+							InstagramAccount: "instagram-id",
+							FacebookAccount:  "facebook-id",
+							Email:            "test-coordinator@and-period.jp",
+							PhoneNumber:      "+819012345678",
+							PostalCode:       "1000014",
+							Prefecture:       "東京都",
+							City:             "千代田区",
+							CreatedAt:        1640962800,
+							UpdatedAt:        1640962800,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "invalid limit",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+			},
+			query: "?limit=a",
+			expect: &testResponse{
+				code: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "invalid offset",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+			},
+			query: "?offset=a",
+			expect: &testResponse{
+				code: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "failed to get coordinators",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				mocks.user.EXPECT().ListCoordinators(gomock.Any(), in).Return(nil, errmock)
+			},
+			query: "",
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			const prefix = "/v1/coordinators"
+			path := fmt.Sprintf("%s%s", prefix, tt.query)
+			testGet(t, tt.setup, tt.expect, path)
+		})
+	}
+}
+
+func TestGetCoordinator(t *testing.T) {
+	t.Parallel()
+
+	in := &user.GetCoordinatorInput{
+		CoordinatorID: "coordinator-id",
+	}
+	coordinator := &uentity.Coordinator{
+		ID:               "coordinator-id",
+		Lastname:         "&.",
+		Firstname:        "管理者",
+		LastnameKana:     "あんどどっと",
+		FirstnameKana:    "かんりしゃ",
+		CompanyName:      "&.株式会社",
+		StoreName:        "&.農園",
+		ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+		HeaderURL:        "https://and-period.jp/header.png",
+		TwitterAccount:   "twitter-id",
+		InstagramAccount: "instagram-id",
+		FacebookAccount:  "facebook-id",
+		Email:            "test-coordinator@and-period.jp",
+		PhoneNumber:      "+819012345678",
+		PostalCode:       "1000014",
+		Prefecture:       "東京都",
+		City:             "千代田区",
+		CreatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+		UpdatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+	}
+
+	tests := []struct {
+		name          string
+		setup         func(t *testing.T, mocks *mocks, ctrl *gomock.Controller)
+		coordinatorID string
+		expect        *testResponse
+	}{
+		{
+			name: "success",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				mocks.user.EXPECT().GetCoordinator(gomock.Any(), in).Return(coordinator, nil)
+			},
+			coordinatorID: "coordinator-id",
+			expect: &testResponse{
+				code: http.StatusOK,
+				body: &response.CoordinatorResponse{
+					Coordinator: &response.Coordinator{
+						ID:               "coordinator-id",
+						Lastname:         "&.",
+						Firstname:        "管理者",
+						LastnameKana:     "あんどどっと",
+						FirstnameKana:    "かんりしゃ",
+						CompanyName:      "&.株式会社",
+						StoreName:        "&.農園",
+						ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+						HeaderURL:        "https://and-period.jp/header.png",
+						TwitterAccount:   "twitter-id",
+						InstagramAccount: "instagram-id",
+						FacebookAccount:  "facebook-id",
+						Email:            "test-coordinator@and-period.jp",
+						PhoneNumber:      "+819012345678",
+						PostalCode:       "1000014",
+						Prefecture:       "東京都",
+						City:             "千代田区",
+						CreatedAt:        1640962800,
+						UpdatedAt:        1640962800,
+					},
+				},
+			},
+		},
+		{
+			name: "failed to get coordinator",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				mocks.user.EXPECT().GetCoordinator(gomock.Any(), in).Return(nil, errmock)
+			},
+			coordinatorID: "coordinator-id",
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			const prefix = "/v1/coordinators"
+			path := fmt.Sprintf("%s/%s", prefix, tt.coordinatorID)
+			testGet(t, tt.setup, tt.expect, path)
+		})
+	}
+}
+
+func TestCreateCoordinator(t *testing.T) {
+	t.Parallel()
+
+	in := &user.CreateCoordinatorInput{
+		Lastname:         "&.",
+		Firstname:        "生産者",
+		LastnameKana:     "あんどどっと",
+		FirstnameKana:    "せいさんしゃ",
+		CompanyName:      "&.株式会社",
+		StoreName:        "&.農園",
+		ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+		HeaderURL:        "https://and-period.jp/header.png",
+		TwitterAccount:   "twitter-id",
+		InstagramAccount: "instagram-id",
+		FacebookAccount:  "facebook-id",
+		Email:            "test-coordinator@and-period.jp",
+		PhoneNumber:      "+819012345678",
+		PostalCode:       "1000014",
+		Prefecture:       "東京都",
+		City:             "千代田区",
+		AddressLine1:     "永田町1-7-1",
+		AddressLine2:     "",
+	}
+	coordinator := &uentity.Coordinator{
+		ID:               "coordinator-id",
+		Lastname:         "&.",
+		Firstname:        "管理者",
+		LastnameKana:     "あんどどっと",
+		FirstnameKana:    "かんりしゃ",
+		CompanyName:      "&.株式会社",
+		StoreName:        "&.農園",
+		ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+		HeaderURL:        "https://and-period.jp/header.png",
+		TwitterAccount:   "twitter-id",
+		InstagramAccount: "instagram-id",
+		FacebookAccount:  "facebook-id",
+		Email:            "test-coordinator@and-period.jp",
+		PhoneNumber:      "+819012345678",
+		PostalCode:       "1000014",
+		Prefecture:       "東京都",
+		City:             "千代田区",
+		AddressLine1:     "永田町1-7-1",
+		AddressLine2:     "",
+		CreatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+		UpdatedAt:        jst.Date(2022, 1, 1, 0, 0, 0, 0),
+	}
+
+	tests := []struct {
+		name   string
+		setup  func(t *testing.T, mocks *mocks, ctrl *gomock.Controller)
+		req    *request.CreateCoordinatorRequest
+		expect *testResponse
+	}{
+		{
+			name: "success",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				mocks.user.EXPECT().CreateCoordinator(gomock.Any(), in).Return(coordinator, nil)
+			},
+			req: &request.CreateCoordinatorRequest{
+				Lastname:         "&.",
+				Firstname:        "生産者",
+				LastnameKana:     "あんどどっと",
+				FirstnameKana:    "せいさんしゃ",
+				CompanyName:      "&.株式会社",
+				StoreName:        "&.農園",
+				ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+				HeaderURL:        "https://and-period.jp/header.png",
+				TwitterAccount:   "twitter-id",
+				InstagramAccount: "instagram-id",
+				FacebookAccount:  "facebook-id",
+				Email:            "test-coordinator@and-period.jp",
+				PhoneNumber:      "+819012345678",
+				PostalCode:       "1000014",
+				Prefecture:       "東京都",
+				City:             "千代田区",
+				AddressLine1:     "永田町1-7-1",
+				AddressLine2:     "",
+			},
+			expect: &testResponse{
+				code: http.StatusOK,
+				body: &response.CoordinatorResponse{
+					Coordinator: &response.Coordinator{
+						ID:               "coordinator-id",
+						Lastname:         "&.",
+						Firstname:        "管理者",
+						LastnameKana:     "あんどどっと",
+						FirstnameKana:    "かんりしゃ",
+						CompanyName:      "&.株式会社",
+						StoreName:        "&.農園",
+						ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+						HeaderURL:        "https://and-period.jp/header.png",
+						TwitterAccount:   "twitter-id",
+						InstagramAccount: "instagram-id",
+						FacebookAccount:  "facebook-id",
+						Email:            "test-coordinator@and-period.jp",
+						PhoneNumber:      "+819012345678",
+						PostalCode:       "1000014",
+						Prefecture:       "東京都",
+						City:             "千代田区",
+						AddressLine1:     "永田町1-7-1",
+						AddressLine2:     "",
+						CreatedAt:        1640962800,
+						UpdatedAt:        1640962800,
+					},
+				},
+			},
+		},
+		{
+			name: "failed to create coordinator",
+			setup: func(t *testing.T, mocks *mocks, ctrl *gomock.Controller) {
+				mocks.user.EXPECT().CreateCoordinator(gomock.Any(), in).Return(nil, errmock)
+			},
+			req: &request.CreateCoordinatorRequest{
+				Lastname:         "&.",
+				Firstname:        "生産者",
+				LastnameKana:     "あんどどっと",
+				FirstnameKana:    "せいさんしゃ",
+				CompanyName:      "&.株式会社",
+				StoreName:        "&.農園",
+				ThumbnailURL:     "https://and-period.jp/thumbnail.png",
+				HeaderURL:        "https://and-period.jp/header.png",
+				TwitterAccount:   "twitter-id",
+				InstagramAccount: "instagram-id",
+				FacebookAccount:  "facebook-id",
+				Email:            "test-coordinator@and-period.jp",
+				PhoneNumber:      "+819012345678",
+				PostalCode:       "1000014",
+				Prefecture:       "東京都",
+				City:             "千代田区",
+				AddressLine1:     "永田町1-7-1",
+				AddressLine2:     "",
+			},
+			expect: &testResponse{
+				code: http.StatusInternalServerError,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			const path = "/v1/coordinators"
+			testPost(t, tt.setup, tt.expect, path, tt.req)
+		})
+	}
+}
