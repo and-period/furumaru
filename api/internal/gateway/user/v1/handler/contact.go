4a750748496eea0f3ba23bdb4da223856faea32c
nishi <ta.g-h-r.1260@docomo.ne.jp>
2022-07-16 00:08:13 +0900

    Revert "debug (#180)" (#181)
    This reverts commit 51f406558c17e4116c394e211882122b67397b04.

--- api/internal/gateway/user/v1/handler/contact.go
+++ api/internal/gateway/user/v1/handler/contact.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
-	"errors"
+	"net/http"
 
+	"github.com/and-period/furumaru/api/internal/gateway/user/v1/request"
+	"github.com/and-period/furumaru/api/internal/messenger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,25 +13,22 @@ func (h *handler) contactRoutes(rg *gin.RouterGroup) {
 }
 
 func (h *handler) CreateContact(ctx *gin.Context) {
-	err := errors.New("テストエラー")
-	httpError(ctx, err)
+	req := &request.CreateContactRequest{}
+	if err := ctx.BindJSON(req); err != nil {
+		badRequest(ctx, err)
+		return
+	}
 
-	// req := &request.CreateContactRequest{}
-	// if err := ctx.BindJSON(req); err != nil {
-	// 	badRequest(ctx, err)
-	// 	return
-	// }
+	in := &messenger.CreateContactInput{
+		Title:       req.Title,
+		Content:     req.Content,
+		Username:    req.Username,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+	}
+	if _, err := h.messenger.CreateContact(ctx, in); err != nil {
+		httpError(ctx, err)
+	}
 
-	// in := &messenger.CreateContactInput{
-	// 	Title:       req.Title,
-	// 	Content:     req.Content,
-	// 	Username:    req.Username,
-	// 	Email:       req.Email,
-	// 	PhoneNumber: req.PhoneNumber,
-	// }
-	// if _, err := h.messenger.CreateContact(ctx, in); err != nil {
-	// 	httpError(ctx, err)
-	// }
-
-	// ctx.JSON(http.StatusNoContent, gin.H{})
+	ctx.JSON(http.StatusNoContent, gin.H{})
 }
