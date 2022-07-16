ca6398e9df7c2a1d84058aab3ae8f4190473511c
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-07-16 23:39:24 +0900

    feat: #182の対応 (#188)

--- web/admin/src/middleware/auth.ts
+++ web/admin/src/middleware/auth.ts
@@ -5,13 +5,16 @@ import { useAuthStore } from '~/store/auth'
 
 const routing: Middleware = async (context: Context) => {
   const publicPages = ['/signin']
+  const path = context.route.path
 
-  const { isAuthenticated, getAuthByRefreshToken } = useAuthStore()
+  const { isAuthenticated, getAuthByRefreshToken, setRedirectPath } =
+    useAuthStore()
 
   if (!publicPages.includes(context.route.path)) {
     if (!isAuthenticated) {
       const cookies = new Cookies()
       const token = cookies.get('refreshToken')
+      setRedirectPath(path)
       if (token) {
         await getAuthByRefreshToken(token).catch(() => {
           context.redirect('/signin')
