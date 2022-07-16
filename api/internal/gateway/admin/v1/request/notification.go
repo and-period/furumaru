61d195ca3ce4e49b6616fb98f7d3677eeaca7daa
kaito <sea_rr10_ch@outlook.com>
2022-07-17 04:12:01 +0900

    feat: define request & response

---
+++ api/internal/gateway/admin/v1/request/notification.go
@@ -0,0 +1,18 @@
+package request
+
+type CreateNotificationRequest struct {
+	Title       string       `json:"title,omitempty"`       //タイトル
+	Body        string       `json:"body,omitempty"`        //本文
+	Targets     []TargetType `json:"targets,omitempty"`     //掲載対象一覧
+	PublishedAt int64        `json:"publishedAt,omitempty"` //掲載開始日
+	Public      bool         `json:"public,omitempty"`      //公開フラグ
+}
+
+type TargetType int32
+
+const (
+	PostTargetAll          TargetType = 0 // 全員対象
+	PostTargetUsers        TargetType = 1 // ユーザー対象
+	PostTargetProducers    TargetType = 2 // 生産者対象
+	PostTargetCoordinators TargetType = 3 // コーディネーター対象
+)
---
+++ api/internal/gateway/admin/v1/response/notification.go
@@ -0,0 +1,26 @@
+package response
+
+// Notification - お知らせ情報
+type Notification struct {
+	ID          string       `json:"id"`          // お知らせID
+	CreatedBy   string       `json:"createdBy"`   // 登録者ID
+	CreatorName string       `json:"creatorName"` // 登録者名
+	UpdatedBy   string       `json:"updatedBy"`   // 更新者ID
+	Title       string       `json:"title"`       // タイトル
+	Body        string       `json:"body"`        // 本文
+	Targets     []TargetType `json:"targets"`     // 掲載対象一覧
+	PublishedAt int64        `json:"publishedAt"` // 掲載開始日時
+	Public      bool         `json:"public"`      // 公開フラグ
+	CreatedAt   int64        `json:"createdAt"`   // 作成日時
+	UpdatedAt   int64        `json:"updatedAt"`   // 更新日時
+}
+
+// 掲載対象
+type TargetType int32
+
+const (
+	PostTargetAll          TargetType = 0 // 全員対象
+	PostTargetUsers        TargetType = 1 // ユーザー対象
+	PostTargetProducers    TargetType = 2 // 生産者対象
+	PostTargetCoordinators TargetType = 3 // コーディネーター対象
+)
