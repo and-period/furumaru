ddf4bf69c3d011d3283256561bdeede714890314
濵田広大 <hamany0326@gmail.com>
2022-07-17 23:55:38 +0900

    品物登録ができるようになった

---
+++ web/admin/src/store/product-type.ts
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia'
+
+import { useAuthStore } from './auth'
+
+import { ApiClientFactory } from '.'
+
+import {
+  CreateProductTypeRequest,
+  ProductTypeApi,
+  ProductTypesResponse,
+} from '~/types/api'
+
+export const useProductTypeStore = defineStore('ProductType', {
+  state: () => ({
+    productTypes: [] as ProductTypesResponse['productTypes'],
+  }),
+  actions: {
+    async createProductType(
+      categoryId: string,
+      payload: CreateProductTypeRequest
+    ): Promise<void> {
+      try {
+        const authStore = useAuthStore()
+        const accessToken = authStore.accessToken
+        if (!accessToken) throw new Error('認証エラー')
+
+        const factory = new ApiClientFactory()
+        const productTypeApiClient = factory.create(ProductTypeApi, accessToken)
+        const res = await productTypeApiClient.v1CreateProductType(
+          categoryId,
+          payload
+        )
+        console.log(res)
+      } catch (error) {
+        // TODO: エラーハンドリング
+        throw new Error('Internal Server Error')
+      }
+    },
+  },
+})
