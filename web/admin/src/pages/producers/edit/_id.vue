cac6fae7f0d698b23da632c0c94820827e8810fa
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-07-24 23:25:15 +0900

    wip: 編集用のページを追加

---
+++ web/admin/src/pages/producers/edit/_id.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <p class="text-h6">生産者編集</p>
+    <div v-if="!fetchState.pending">
+      {{ formData }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  useFetch,
+  useRoute,
+} from '@nuxtjs/composition-api'
+
+import { useProducerStore } from '~/store/producer'
+import { ProducerResponse } from '~/types/api'
+
+export default defineComponent({
+  setup() {
+    const route = useRoute()
+    const id = route.value.params.id
+
+    const { getProducer } = useProducerStore()
+
+    const formData = reactive<ProducerResponse>({
+      id,
+      lastname: '',
+      lastnameKana: '',
+      firstname: '',
+      firstnameKana: '',
+      addressLine1: '',
+      addressLine2: '',
+      city: '',
+      prefecture: '',
+      phoneNumber: '',
+      postalCode: '',
+      storeName: '',
+      headerUrl: '',
+      createdAt: -1,
+      updatedAt: -1,
+      thumbnailUrl: '',
+      email: '',
+    })
+
+    const { fetchState } = useFetch(async () => {
+      const producer = await getProducer(id)
+      formData.lastname = producer.lastname
+      formData.lastnameKana = producer.lastnameKana
+      formData.firstname = producer.firstname
+      formData.firstnameKana = producer.firstnameKana
+      formData.addressLine1 = producer.addressLine1
+      formData.addressLine2 = producer.addressLine2
+      formData.city = producer.city
+      formData.prefecture = producer.prefecture
+      formData.phoneNumber = producer.phoneNumber
+      formData.postalCode = producer.postalCode
+      formData.storeName = producer.storeName
+      formData.headerUrl = producer.headerUrl
+      formData.thumbnailUrl = producer.thumbnailUrl
+      formData.email = producer.email
+      formData.createdAt = producer.createdAt
+      formData.updatedAt = producer.updatedAt
+    })
+
+    return {
+      id,
+      fetchState,
+      formData,
+    }
+  },
+})
+</script>
