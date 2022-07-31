848a4c5540b841f39e6129485872a92bf77e78ce
kaito <sea_rr10_ch@outlook.com>
2022-08-01 02:49:29 +0900

    feat: add convert type method

--- api/internal/gateway/admin/v1/service/notification.go
+++ api/internal/gateway/admin/v1/service/notification.go
@@ -9,6 +9,12 @@ type Notification struct {
 	response.Notification
 }
 
+type TargetType struct {
+	response.TargetType
+}
+
+type TargetTypes []*TargetType
+
 func NewNotification(notification *entity.Notification) *Notification {
 	return &Notification{
 		Notification: response.Notification{
@@ -18,11 +24,37 @@ func NewNotification(notification *entity.Notification) *Notification {
 			UpdatedBy:   notification.UpdatedBy,
 			Title:       notification.Title,
 			Body:        notification.Body,
-			Targets:     notification.Targets,
+			Targets:     NewNotificationTargets(notification.Targets).Response(),
 			PublishedAt: notification.PublishedAt.Unix(),
 			Public:      notification.Public,
 			CreatedAt:   notification.CreatedAt.Unix(),
 			UpdatedAt:   notification.UpdatedAt.Unix(),
 		},
 	}
+}
+
+func NewNotificationTarget(target *entity.TargetType) *TargetType {
+	return &TargetType{
+		TargetType: response.TargetType(*target),
+	}
+}
+
+func (t *TargetType) Response() *response.TargetType {
+	return &t.TargetType
+}
+
+func NewNotificationTargets(targets []entity.TargetType) TargetTypes {
+	res := make(TargetTypes, len(targets))
+	for i := range targets {
+		res[i] = NewNotificationTarget(&targets[i])
+	}
+	return res
+}
+
+func (t TargetTypes) Response() []response.TargetType {
+	res := make([]response.TargetType, len(t))
+	for i := range t {
+		res[i] = *t[i].Response()
+	}
+	return res
 }
