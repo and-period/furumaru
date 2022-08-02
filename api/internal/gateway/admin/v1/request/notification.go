f144718eeaa4a2446209cb1aa6b9d9bb60f06a03
kaito <sea_rr10_ch@outlook.com>
2022-08-02 23:56:42 +0900

    feat: change request type

--- api/internal/gateway/admin/v1/request/notification.go
+++ api/internal/gateway/admin/v1/request/notification.go
@@ -1,12 +1,10 @@
 package request
 
-import "time"
-
 type CreateNotificationRequest struct {
 	Title       string       `json:"title,omitempty"`       //タイトル
 	Body        string       `json:"body,omitempty"`        //本文
 	Targets     []TargetType `json:"targets,omitempty"`     //掲載対象一覧
-	PublishedAt time.Time    `json:"publishedAt,omitempty"` //掲載開始日
+	PublishedAt int64        `json:"publishedAt,omitempty"` //掲載開始日
 	Public      bool         `json:"public,omitempty"`      //公開フラグ
 }
 
