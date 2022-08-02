4794707f7dd1043a7d9c1ec571423d3d698a93c0
taba2424 <tadashi-112@hotmail.co.jp>
2022-08-02 22:00:57 +0900

    feat(web/admin): setup amplify client

---
+++ web/admin/src/plugins/amplify.ts
@@ -0,0 +1,24 @@
+import { Amplify, Analytics } from 'aws-amplify';
+
+const enabledAnalytics: boolean = process.env.AWS_PINPOINT_APP_ID !== ''
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: process.env.AWS_COGNITO_IDENTITY_POOL_ID,
+    region: process.env.AWS_REGION,
+  },
+  Analytics: {
+    disabled: !enabledAnalytics,
+    autoSessionRecord: true,
+    AWSPinpoint: {
+      appId: process.env.AWS_PINPOINT_APP_ID,
+      region: process.env.AWS_REGION,
+      flushInterval: 5000, // 5s
+      resendLimit: 5
+    }
+  }
+})
+
+if (enabledAnalytics) {
+  Analytics.record(process.env.APP_NAME || '')
+}
