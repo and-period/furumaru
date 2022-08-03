85b8c1a7ffe66c05bbda50837298ca6562b2783c
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-08-04 00:14:34 +0900

    fix: 基底のErrorクラスに準拠する

--- web/admin/src/types/exception.ts
+++ web/admin/src/types/exception.ts
@@ -1,44 +1,45 @@
 class ApiBaseError<T> extends Error {
-  constructor(public message: string, public detail: T) {
+  constructor(public message: string, public cause?: T) {
     super(message)
+    this.cause = cause
   }
 }
 
 export class ConnectionError<T> extends ApiBaseError<T> {
-  constructor(public detail: T) {
+  constructor(public cause?: T) {
     super(
       '現在、システムが停止中です。時間をおいてから再度アクセスしてください。',
-      detail
+      cause
     )
   }
 }
 
 export class InternalServerError<T> extends ApiBaseError<T> {
-  constructor(public detail: T) {
-    super('不明なエラーが発生しました。', detail)
+  constructor(public cause?: T) {
+    super('不明なエラーが発生しました。', cause)
   }
 }
 
 export class AuthError<T> extends ApiBaseError<T> {
-  constructor(public status: number, public message: string, public detail: T) {
-    super(message, detail)
+  constructor(public status: number, public message: string, public cause?: T) {
+    super(message, cause)
   }
 }
 
 export class ValidationError<T> extends ApiBaseError<T> {
-  constructor(public status: number, public message: string, public detail: T) {
-    super(message, detail)
+  constructor(public status: number, public message: string, public cause?: T) {
+    super(message, cause)
   }
 }
 
 export class NotFoundError<T> extends ApiBaseError<T> {
-  constructor(public status: number, public message: string, public detail: T) {
-    super(message, detail)
+  constructor(public status: number, public message: string, public cause?: T) {
+    super(message, cause)
   }
 }
 
 export class ConflictError<T> extends ApiBaseError<T> {
-  constructor(public status: number, public message: string, public detail: T) {
-    super(message, detail)
+  constructor(public status: number, public message: string, public cause?: T) {
+    super(message, cause)
   }
 }
