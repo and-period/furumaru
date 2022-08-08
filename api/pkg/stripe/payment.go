bdc54602e9c1dc159466467824c23280c9fda7ce
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-08-09 01:16:33 +0900

    fix(stripe): fix stripe client

--- api/pkg/stripe/payment.go
+++ api/pkg/stripe/payment.go
@@ -10,6 +10,23 @@ import (
 	"go.uber.org/zap"
 )
 
+type OrderParams struct {
+	CustomerID        string
+	PaymentMethodID   string
+	PaymentMethodType stripe.PaymentMethodType
+	Amount            int64
+	Description       string
+	Metadata          map[string]string
+}
+
+type GuestOrderParams struct {
+	Email             string
+	PaymentMethodType stripe.PaymentMethodType
+	Amount            int64
+	Description       string
+	Metadata          map[string]string
+}
+
 // reference: https://stripe.com/docs/api/payment_methods/attach
 func (c *client) AttachPayment(ctx context.Context, customerID, paymentID string) (*stripe.PaymentMethod, error) {
 	params := &stripe.PaymentMethodAttachParams{
@@ -55,6 +72,68 @@ func (c *client) UpdateDefaultPayment(ctx context.Context, customerID, paymentID
 		return err
 	}
 	return nil
+}
+
+// reference: https://stripe.com/docs/api/payment_intents/create
+func (c *client) Order(ctx context.Context, in *OrderParams) (*stripe.PaymentIntent, error) {
+	params := &stripe.PaymentIntentParams{
+		Params: stripe.Params{
+			Context:  ctx,
+			Metadata: in.Metadata,
+		},
+		Customer:           stripe.String(in.CustomerID),
+		Description:        nullString(in.Description),
+		Amount:             stripe.Int64(in.Amount),
+		Currency:           stripe.String(string(stripe.CurrencyJPY)),
+		PaymentMethod:      stripe.String(in.PaymentMethodID),
+		PaymentMethodTypes: stripe.StringSlice([]string{string(in.PaymentMethodType)}),
+		CaptureMethod:      stripe.String(string(stripe.PaymentIntentCaptureMethodManual)),
+		UseStripeSDK:       stripe.Bool(true),
+	}
+	var pi *stripe.PaymentIntent
+	orderFn := func() (err error) {
+		pi, err = paymentintent.New(params)
+		return err
+	}
+	if err := c.do(ctx, orderFn); err != nil {
+		c.logger.Error("Failed to order",
+			zap.String("customerId", in.CustomerID),
+			zap.String("paymentMethodId", in.PaymentMethodID),
+			zap.String("paymentMethodType", string(in.PaymentMethodType)),
+			zap.Error(err))
+		return nil, err
+	}
+	return pi, nil
+}
+
+// reference: https://stripe.com/docs/api/payment_intents/create
+func (c *client) GuestOrder(ctx context.Context, in *GuestOrderParams) (*stripe.PaymentIntent, error) {
+	params := &stripe.PaymentIntentParams{
+		Params: stripe.Params{
+			Context:  ctx,
+			Metadata: in.Metadata,
+		},
+		ReceiptEmail:       stripe.String(in.Email),
+		Description:        nullString(in.Description),
+		Amount:             stripe.Int64(in.Amount),
+		Currency:           stripe.String(string(stripe.CurrencyJPY)),
+		PaymentMethodTypes: stripe.StringSlice([]string{string(in.PaymentMethodType)}),
+		CaptureMethod:      stripe.String(string(stripe.PaymentIntentCaptureMethodManual)),
+		UseStripeSDK:       stripe.Bool(true),
+	}
+	var pi *stripe.PaymentIntent
+	orderFn := func() (err error) {
+		pi, err = paymentintent.New(params)
+		return err
+	}
+	if err := c.do(ctx, orderFn); err != nil {
+		c.logger.Error("Failed to guest order",
+			zap.String("email", in.Email),
+			zap.String("paymentMethodType", string(in.PaymentMethodType)),
+			zap.Error(err))
+		return nil, err
+	}
+	return pi, nil
 }
 
 // reference: https://stripe.com/docs/api/payment_intents/capture
