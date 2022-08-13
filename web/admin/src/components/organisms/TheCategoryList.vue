67d35fbc46b95fa22f2c37087c20a5fb43fe62e2
濵田広大 <hamany0326@gmail.com>
2022-08-13 20:58:40 +0900

    Add: added category update

--- web/admin/src/components/organisms/TheCategoryList.vue
+++ web/admin/src/components/organisms/TheCategoryList.vue
@@ -6,11 +6,11 @@
           {{ `${item.name}` }}
         </template>
         <template #[`item.actions`]="{ item }">
-          <v-btn outlined color="primary" small @click="handleEdit(item)">
+          <v-btn outlined color="primary" small @click="openUpdateDialog(item)">
             <v-icon small>mdi-pencil</v-icon>
             編集
           </v-btn>
-          <v-btn outlined color="primary" small @click="openDialog(item)">
+          <v-btn outlined color="primary" small @click="openDeleteDialog(item)">
             <v-icon small>mdi-delete</v-icon>
             削除
           </v-btn>
@@ -31,23 +31,53 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-dialog v-model="updateDialog" width="500">
+                <v-card>
+            <v-card-title class="text-h6 primaryLight">
+              カテゴリー編集
+            </v-card-title>
+            <v-text-field
+              v-model="categoryFormData.name"
+              class="mx-4"
+              maxlength="32"
+              label="カテゴリー"
+            />
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="accentDarken" text @click="updateCancel">
+                キャンセル
+              </v-btn>
+              <v-btn color="primary" outlined @click="handleUpdate">
+                編集
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, useFetch } from '@nuxtjs/composition-api'
+import { reactive, ref, useFetch } from '@nuxtjs/composition-api'
 import { computed, defineComponent } from '@vue/composition-api'
 import { DataTableHeader } from 'vuetify'
 
 import { useCategoryStore } from '~/store/category'
-import { CategoriesResponseCategoriesInner } from '~/types/api'
+import { CategoriesResponseCategoriesInner, UpdateCategoryRequest } from '~/types/api'
 
 export default defineComponent({
   setup() {
     const categoryStore = useCategoryStore()
     const deleteDialog = ref<boolean>(false)
+    const updateDialog = ref<boolean>(false)
     const selectedItem = ref<string>('')
     const selectedName = ref<string>('')
+    const categoryId = ref<string>('')
+    const categoryFormData = reactive<UpdateCategoryRequest>({
+      name: '',
+    })
+
     const categories = computed(() => {
       return categoryStore.categories
     })
@@ -69,10 +99,29 @@ export default defineComponent({
       deleteDialog.value = false
     }
 
-    const openDialog = (item: CategoriesResponseCategoriesInner): void => {
+    const updateCancel = (): void => {
+      updateDialog.value = false
+    }
+
+    const openUpdateDialog = (item: CategoriesResponseCategoriesInner): void => {
+      categoryFormData.name = item.name
+      categoryId.value = item.id
+      updateDialog.value = true
+    }
+
+    const openDeleteDialog = (item: CategoriesResponseCategoriesInner): void => {
       selectedItem.value = item.id
       selectedName.value = item.name
       deleteDialog.value = true
+    }
+
+    const handleUpdate = async (): Promise<void> => {
+      try {
+        await categoryStore.updateCategory(categoryId.value, categoryFormData)
+        updateDialog.value = false
+      } catch(error) {
+        console.log(error)
+      }
     }
 
     const handleDelete = async (): Promise<void> => {
@@ -97,9 +146,14 @@ export default defineComponent({
       fetchState,
       categories,
       deleteDialog,
+      updateDialog,
       selectedName,
-      openDialog,
+      categoryFormData,
+      openUpdateDialog,
+      openDeleteDialog,
+      updateCancel,
       deleteCancel,
+      handleUpdate,
       handleDelete,
     }
   },
