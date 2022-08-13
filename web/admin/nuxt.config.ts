bc24c12559fa585f3771fb4dd9c5694eb1df4b3f
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-08-13 20:47:17 +0900

    feat: firebaseの設定を追加

--- web/admin/nuxt.config.ts
+++ web/admin/nuxt.config.ts
@@ -32,7 +32,7 @@ const config: NuxtConfig = {
   css: ['~/assets/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/amplify'],
+  plugins: ['~/plugins/amplify', '~/plugins/firebase'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
@@ -73,6 +73,15 @@ const config: NuxtConfig = {
     AWS_REGION: process.env.AWS_REGION || 'ap-northeast-1',
     AWS_COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID || '',
     AWS_PINPOINT_APP_ID: process.env.PINPOINT_APP_ID || '',
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || '',
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || '',
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || '',
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET || '',
+    FIREBASE_MESSAGING_SENDER_ID:
+      process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || '',
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID || '',
+    FIREBASE_VAPI_KEY: process.env.FIREBASE_VAPI_KEY || '',
   },
 
   router: {
@@ -103,6 +112,10 @@ const config: NuxtConfig = {
       },
       options: { customProperties: true },
     },
+  },
+
+  manifest: {
+    gcm_sender_id: '103953800507',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ web/admin/src/sw/firebase-messaging-sw.ts
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const fs = require('fs')
+
+/**
+ * firebase-messaging-sw.js - 生成用
+ */
+fs.writeFileSync(
+  './src/static/firebase-messaging-sw.js',
+  `importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
+
+const config = {
+  apiKey: "${process.env.FIREBASE_API_KEY}",
+  authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
+  projectId: "${process.env.FIREBASE_PROJECT_ID}",
+  storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+  appId: "${process.env.FIREBASE_APP_ID}",
+  measurementId: "${process.env.FIREBASE_MEASUREMENT_ID}",
+}
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+`
+)
