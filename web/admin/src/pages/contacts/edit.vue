646813e858e8bfd351624fbf685da57fc734f5e4
hagi_yuto <hagiyuuto@gmail.com>
2022-08-13 16:09:02 +0900

    お問合せ管理編集画面３

--- web/admin/src/pages/contacts/edit.vue
+++ web/admin/src/pages/contacts/edit.vue
@@ -2,81 +2,52 @@
   <div>
     <v-card-title>お問合せ管理</v-card-title>
     <v-card elevation="0">
-      <v-col cols="4">
+      <v-card-text>
         <v-text-field
           name="name"
           label="名前"
           value="古丸太郎"
           readonly
         ></v-text-field>
-      </v-col>
 
-      <v-col cols="4">
         <v-text-field
           name="subject"
           label="件名"
           value="商品が届かない件について"
           readonly
         ></v-text-field>
-      </v-col>
 
-      <v-col cols="6">
         <v-textarea
           name="contact"
           label="お問合せ内容"
           value="商品が届いていないです。どうなっていますでしょうか。ああああああああああああああああああああ"
           readonly
         ></v-textarea>
-      </v-col>
 
-      <v-row>
-        <v-col cols="2">
-          <v-subheader>優先度：</v-subheader>
-        </v-col>
-        <v-col cols="5">
-          <v-autocomplete
-            :items="priority"
-            chips
-            :item-color="red"
-            label="優先度"
-          ></v-autocomplete>
-        </v-col>
-      </v-row>
+        <v-autocomplete :items="priority" chips label="優先度"></v-autocomplete>
 
-      <v-row>
-        <v-col cols="2">
-          <v-subheader>ステータス：</v-subheader>
-        </v-col>
-        <v-col cols="5">
-          <v-autocomplete
-            :items="status"
-            chips
-            label="ステータス"
-          ></v-autocomplete>
-        </v-col>
-      </v-row>
+        <v-autocomplete
+          :items="status"
+          chips
+          label="ステータス"
+        ></v-autocomplete>
 
-      <v-col cols="4">
         <v-text-field
           name="mailAddress"
           label="メールアドレス"
           value="and-period@gmail.com"
           readonly
         ></v-text-field>
-      </v-col>
 
-      <v-col cols="4">
         <v-text-field
           name="telephoneNumber"
           label="電話番号"
           value="09012345678"
           readonly
         ></v-text-field>
-      </v-col>
 
-      <v-col cols="6">
         <v-textarea name="memo" label="メモ"></v-textarea>
-      </v-col>
+      </v-card-text>
     </v-card>
   </div>
 </template>
