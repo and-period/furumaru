f1565502d46bd7773454bcaaff023315c2576043
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-08-14 23:16:34 +0900

    chore(*): update go libraries (#254)
    * chore(*): update go libraries
    
    * fix(*): fix lint
    
    * fix(*): fix lint
    
    * fix(*): fix deprecated method

--- api/hack/create-admin/main.go
+++ api/hack/create-admin/main.go
@@ -1,9 +1,10 @@
 // 管理者を登録します
-// usage: go run ./main.go \
-//          -db-host=127.0.0.1 -db-port=3316 -db-password=12345678 \
-//          -aws-access-key=xxx -aws-secret-key=xxx \
-//          -cognito-client-id=xxx -cognito-pool-id=xxx \
-//          -email=test-admin@and-period.jp
+//
+//	usage: go run ./main.go \
+//	         -db-host=127.0.0.1 -db-port=3316 -db-password=12345678 \
+//	         -aws-access-key=xxx -aws-secret-key=xxx \
+//	         -cognito-client-id=xxx -cognito-pool-id=xxx \
+//	         -email=test-admin@and-period.jp
 package main
 
 import (
