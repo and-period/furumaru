d8501322d3222f6f1a3bd22513ce5d830e387221
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2022-08-16 20:24:05 +0900

    feat(web/admin): add live viewing client

---
+++ web/admin/src/pages/livestreaming/view.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card>
+    <v-card-title>視聴テスト用モック</v-card-title>
+    <v-card-subtitle>
+      <p>Playback URLはAWSコンソールより取得すること。</p>
+    </v-card-subtitle>
+
+    <v-container>
+      <video id="video" controls></video>
+    </v-container>
+
+    <v-card-text>
+      <v-text-field
+        v-model="formData.playbackUrl"
+        placeholder="Playback URL"
+      />
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn @click="startWatching">Start Watching</v-btn>
+      <v-btn @click="handleClickStreaming">Live Streaming</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { defineComponent, reactive, useRouter } from '@nuxtjs/composition-api'
+import HLS from 'hls.js'
+
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+    const hls = new HLS()
+
+    const formData = reactive({
+      playbackUrl: '',
+    })
+
+    const handleClickStreaming = () => {
+      router.push('/livestreaming')
+    }
+
+    const startWatching = () => {
+      if (formData.playbackUrl === '') {
+        alert('playback url is required!')
+        return
+      }
+
+      try {
+        hls.loadSource(formData.playbackUrl)
+        hls.attachMedia(document.getElementById('video'))
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    return {
+      formData,
+      handleClickStreaming,
+      startWatching,
+    }
+  },
+})
+</script>
+
+<style scoped>
+#video {
+  margin-bottom: 1.5em;
+  width: 100%;
+}
+</style>
