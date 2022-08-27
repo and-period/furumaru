772b1e96c87f2694e095046e08bb30965c8916ad
koudai hamada <hamany0326@gmail.com>
2022-08-27 11:09:41 +0900

    Add: added promotion ui (#328)
    * Add: added promotion ui
    
    * Fix: fixed ui

---
+++ web/admin/src/pages/promotions/index.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <v-card-title>セール情報</v-card-title>
+    <div class="d-flex">
+      <v-spacer />
+      <v-btn outlined color="primary" @click="handleClickAddButton">
+        <v-icon left>mdi-plus</v-icon>
+        セール情報登録
+      </v-btn>
+    </div>
+    <v-card class="mt-4" flat>
+      <v-data-table
+        :headers="headers"
+        :items="promotions"
+        :no-results-text="noResultsText"
+        no-data-text="登録されているセール情報がありません。"
+      >
+        <template #[`item.title`]="{ item }">
+          {{ item.title }}
+        </template>
+        <template #[`item.public`]="{ item }">
+          <v-chip small :color="getStatusColor(item.public)">
+            {{ getStatus(item.public) }}
+          </v-chip>
+        </template>
+        <template #[`item.code`]="{ item }">
+          {{ item.code }}
+        </template>
+        <template #[`item.discount`]="{ item }">
+          {{ getDiscount(item.discountType, item.discountRate) }}
+        </template>
+        <template #[`item.startAt`]="{ item }">
+          {{ getDay(item.startAt) }}
+        </template>
+        <template #[`item.endAt`]="{ item }">
+          {{ getDay(item.endAt) }}
+        </template>
+        <template #[`item.actions`]="{ item }">
+          <v-btn outlined color="primary" small @click="handleEdit(item)">
+            <v-icon small>mdi-pencil</v-icon>
+            編集
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, useFetch } from '@nuxtjs/composition-api'
+import { defineComponent } from '@vue/composition-api'
+import dayjs from 'dayjs'
+import { DataTableHeader } from 'vuetify'
+
+import { usePromotionStore } from '~/store/promotion'
+
+export default defineComponent({
+  setup() {
+    const promotionStore = usePromotionStore()
+    const promotions = computed(() => {
+      return promotionStore.promotions
+    })
+
+    const headers: DataTableHeader[] = [
+      {
+        text: 'タイトル',
+        value: 'title',
+      },
+      {
+        text: 'ステータス',
+        value: 'public',
+      },
+      {
+        text: '割引コード',
+        value: 'code',
+      },
+      {
+        text: '割引方法',
+        value: 'discount',
+      },
+      {
+        text: '使用可能',
+        value: 'startAt',
+      },
+      {
+        text: '使用終了',
+        value: 'endAt',
+      },
+      {
+        text: 'Actions',
+        value: 'actions',
+        sortable: false,
+      },
+    ]
+
+    const getDiscount = (
+      discountType: number,
+      discountRate: number
+    ): string => {
+      switch (discountType) {
+        case 1:
+          return '-' + discountRate + '円'
+        case 2:
+          return '-' + discountRate + '%'
+        case 3:
+          return '送料無料'
+        default:
+          return ''
+      }
+    }
+
+    const getStatus = (status: boolean): string => {
+      if (status) {
+        return '有効'
+      } else {
+        return '無効'
+      }
+    }
+
+    const getStatusColor = (status: boolean): string => {
+      if (status) {
+        return 'primary'
+      } else {
+        return 'accentDarken'
+      }
+    }
+
+    const getDay = (unixTime: number): string => {
+      return dayjs.unix(unixTime).format('YYYY/MM/DD hh:mm')
+    }
+
+    const { fetchState } = useFetch(async () => {
+      try {
+        await promotionStore.fetchPromotions()
+      } catch (err) {
+        console.log(err)
+      }
+    })
+
+    return {
+      headers,
+      promotions,
+      fetchState,
+      getDiscount,
+      getStatus,
+      getStatusColor,
+      getDay,
+    }
+  },
+})
+</script>
