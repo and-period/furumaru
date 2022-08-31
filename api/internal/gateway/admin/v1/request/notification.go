9127d9fce27a34f0a896b79b5cd02642503482a7
kaito <sea_rr10_ch@outlook.com>
2022-08-31 21:14:49 +0900

    feat: add UpdateNotification request type

--- api/internal/gateway/admin/v1/request/notification.go
+++ api/internal/gateway/admin/v1/request/notification.go
@@ -16,3 +16,11 @@ const (
 	PostTargetProducers    TargetType = 2 // 生産者対象
 	PostTargetCoordinators TargetType = 3 // コーディネーター対象
 )
+
+type UpdateNotificationRequest struct {
+	Title       string       `json:"title,omitempty"`       // タイトル
+	Body        string       `json:"body,omitempty"`        // 本文
+	Targets     []TargetType `json:"targets,omitempty"`     // 掲載対象一覧
+	PublishedAt int64        `json:"publishedAt,omitempty"` // 掲載開始日
+	Public      bool         `json:"public,omitempty"`      // 公開フラグ
+}
