5d2ec84a5e99a9bffa6c549ea539dc4b328515cf
sea-kai <sea_rr10_ch@outlook.com>
2022-09-04 23:14:33 +0900

    Pkg(ivs): add client CreateChannel (#364)
    * chore: add ivs package
    
    * feat: add ivs client
    
    * feat: add CreateChannel method
    
    * fix: change params
    
    * fix: chenge CreateChannel params and input
    
    * chore: make mockgen
    
    * fix: remove pointer
    
    * feat: add TestClient
    
    * feat: add TestStreamError
    
    * fix: change debug message
    
    * chore: go mod tidy
    
    * chore: go mod tidy

---
+++ api/pkg/ivs/channel.go
@@ -0,0 +1,29 @@
+package ivs
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	ivs "github.com/aws/aws-sdk-go-v2/service/ivs"
+	"github.com/aws/aws-sdk-go-v2/service/ivs/types"
+)
+
+type CreateChannelParams struct {
+	LatencyMode types.ChannelLatencyMode
+	Name        string
+	ChannelType types.ChannelType
+}
+
+func (c *client) CreateChannel(ctx context.Context, params *CreateChannelParams) (*ivs.CreateChannelOutput, error) {
+	in := &ivs.CreateChannelInput{
+		LatencyMode:               params.LatencyMode,
+		Name:                      aws.String(params.Name),
+		RecordingConfigurationArn: c.recordingConfigurationArn,
+		Type:                      params.ChannelType,
+	}
+	out, err := c.ivs.CreateChannel(ctx, in)
+	if err != nil {
+		return nil, c.streamError(err)
+	}
+	return out, nil
+}
