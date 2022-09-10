c8e5065c4e61c6b48e5572eb4a8f499efeb96773
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-09-10 18:53:10 +0900

    feat: コーディーネータ登録にバリデーションを追加

--- web/admin/src/lib/validations/rule.ts
+++ web/admin/src/lib/validations/rule.ts
@@ -1,4 +1,12 @@
-import { helpers } from '@vuelidate/validators'
+import { Ref } from '@vue/composition-api'
+import {
+  helpers,
+  MessageProps,
+  required as _required,
+  email as _email,
+  minLength as _minLength,
+  maxLength as _maxLength,
+} from '@vuelidate/validators'
 
 /**
  * ひらがなを判定する関数
@@ -15,4 +23,58 @@ const kanaValidator = (value: string): boolean => {
  */
 const kana = helpers.withMessage('ひらがなを入力してください。', kanaValidator)
 
-export { kana }
+/**
+ * 電話番号を判定するカスタムバリデーション
+ */
+const tel = helpers.withMessage(
+  '電話番号は0から始まる数字のみの9桁または10桁で入力してください。',
+  helpers.regex(/^0[0-9]{9,10}$/)
+)
+
+/**
+ * 郵便番号を判定するカスタムバリデーション
+ */
+const postalCode = helpers.withMessage(
+  '郵便番号は数字のみの7桁で入力してください。',
+  helpers.regex(/[0-9]{7}/)
+)
+
+/**
+ * 必須バリデーションルールをラップする関数
+ */
+const required = helpers.withMessage(
+  (_: MessageProps) => 'この項目は必須項目です。',
+  _required
+)
+
+/**
+ * メールアドレスバリデーションルールをラップする関数
+ */
+const email = helpers.withMessage(
+  'メールアドレスの形式で入力してください。',
+  _email
+)
+
+/**
+ * 文字列の最小の長さのバリデーションルールをラップする関数
+ * @param max
+ * @returns
+ */
+const minLength = (max: number | Ref<number>) =>
+  helpers.withMessage(
+    ({ $params }: MessageProps) => `${$params.max}文字以上入力してください。`,
+    _minLength(max)
+  )
+
+/**
+ * 文字列の最大の長さのバリデーションルールをラップする関数
+ * @param max
+ * @returns
+ */
+const maxLength = (max: number | Ref<number>) =>
+  helpers.withMessage(
+    ({ $params }: MessageProps) => `${$params.max}文字までです。`,
+    _maxLength(max)
+  )
+
+export { kana, tel, required, email, postalCode, minLength, maxLength }
