ece989cc60d0a94b8e50c822010228523b7a30c1
kaito <sea_rr10_ch@outlook.com>
2022-09-11 23:05:46 +0900

    fix: cheange default null

--- infra/mysql/schema/2022091002-stores-create-lives.sql
+++ infra/mysql/schema/2022091002-stores-create-lives.sql
@@ -7,11 +7,11 @@ CREATE TABLE `stores`.`lives` (
   `producer_id`   VARCHAR(22) NOT NULL,          -- 生産者ID
   `start_at`      DATETIME    NOT NULL,          -- 配信開始日時
   `end_at`        DATETIME    NOT NULL,          -- 配信終了日時
-  `cancel`        TINYINT     NOT NULL,          -- 配信中止フラグ
+  `canceled`      TINYINT     NOT NULL,          -- 配信中止フラグ
   `recommends`    JSON        NULL DEFAULT NULL, -- おすすめ商品一覧
   `created_at`    DATETIME    NOT NULL,          -- 登録日時
   `updated_at`    DATETIME    NOT NULL,          -- 更新日時
-  `deleted_at`    DATETIME    NOT NULL,          -- 削除日時
+  `deleted_at`    DATETIME    NULL DEFAULT NULL, -- 削除日時
   PRIMARY KEY(`id`),
   CONSTRAINT `fk_lives_schedule_id`
   FOREIGN KEY (`schedule_id`) REFERENCES `stores`.`schedules` (`id`)
