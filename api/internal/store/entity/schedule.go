32d962669ce4eedaf0512e759f79b06cb5fb9d67
kaito <sea_rr10_ch@outlook.com>
2022-09-16 01:13:35 +0900

    feat: add schedule entity

---
+++ api/internal/store/entity/schedule.go
@@ -0,0 +1,43 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/and-period/furumaru/api/pkg/uuid"
+	"gorm.io/gorm"
+)
+
+// Schedule - 開催スケジュール
+type Schedule struct {
+	ID          string         `gorm:"primarykey;<-:create"` // テンプレートID
+	Title       string         `gorm:""`                     // タイトル
+	Description string         `gorm:""`                     // 説明
+	ThumnailUrl string         `gorm:""`                     // サムネイルURL
+	StartAt     time.Time      `gorm:""`                     // 開催開始日時
+	EndAt       time.Time      `gorm:""`                     // 開催終了日時
+	Canceled    bool           `gorm:""`                     // 開催中止フラグ
+	CreatedAt   time.Time      `gorm:"<-:create"`            // 登録日時
+	UpdatedAt   time.Time      `gorm:""`                     // 更新日時
+	DeletedAt   gorm.DeletedAt `gorm:"default:null"`
+}
+
+type NewScheduleParams struct {
+	Title       string
+	Description string
+	ThumnailUrl string
+	StartAt     time.Time
+	EndAt       time.Time
+	Canceled    bool
+}
+
+func NewSchedule(params *NewScheduleParams) *Schedule {
+	return &Schedule{
+		ID:          uuid.Base58Encode(uuid.New()),
+		Title:       params.Title,
+		Description: params.Description,
+		ThumnailUrl: params.ThumnailUrl,
+		StartAt:     params.StartAt,
+		EndAt:       params.EndAt,
+		Canceled:    params.Canceled,
+	}
+}
