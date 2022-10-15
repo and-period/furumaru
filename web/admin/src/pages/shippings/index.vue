e9619f5025ee0a27d0795a4e363bf70244be7a4e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-10-15 20:05:23 +0900

    feat: 配送情報のstoreを実装

---
+++ web/admin/src/pages/shippings/index.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <v-card-title>配送設定一覧</v-card-title>
+    <v-card class="mt-4" flat :loading="fetchState.pending">
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :server-items-length="totalItems"
+          :footer-props="options"
+          :items="shippings"
+          show-expand
+          class="elevation-0"
+          @update:page="updateCurrentPage"
+          @update:items-per-page="handleUpdateItemsPerPage"
+        >
+          <template #[`item.hasFreeShipping`]="{ item }">
+            <v-chip small>
+              {{ item.hasFreeShipping ? '有り' : '無し' }}
+            </v-chip>
+          </template>
+
+          <template #[`item.updatedAt`]="{ item }">
+            {{ dateFormatter(item.updatedAt) }}
+          </template>
+
+          <template #expanded-item="{ headers, item }">
+            <td :colspan="headers.length" class="pa-4">
+              <div v-for="n in [60, 80, 100]" :key="n">
+                <div class="row my-2">サイズ{{ n }}詳細</div>
+                <v-row v-for="(boxRate, i) in item[`box${n}Rates`]" :key="i">
+                  <v-col cols="1">
+                    {{ boxRate.number }}
+                  </v-col>
+                  <v-col cols="1">
+                    {{ boxRate.name }}
+                  </v-col>
+                  <v-col cols="1">
+                    {{ boxRate.price }}
+                  </v-col>
+                  <v-col cols="9">
+                    <v-chip
+                      v-for="(prefecture, j) in boxRate.prefectures"
+                      :key="j"
+                      class="mr-1"
+                      small
+                    >
+                      {{ prefecture }}
+                    </v-chip>
+                  </v-col>
+                </v-row>
+              </div>
+            </td>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, useFetch } from '@nuxtjs/composition-api'
+import { DataTableHeader } from 'vuetify'
+
+import { dateFormatter } from '~/lib/formatter'
+import { usePagination } from '~/lib/hooks'
+import { useShippingStore } from '~/store/shippings'
+export default defineComponent({
+  setup() {
+    const shippingStore = useShippingStore()
+
+    const totalItems = computed(() => {
+      return shippingStore.totalItems
+    })
+
+    const shippings = computed(() => {
+      return shippingStore.shippings
+    })
+
+    const headers: DataTableHeader[] = [
+      {
+        text: '名前',
+        value: 'name',
+      },
+      {
+        text: '配送無料オプション',
+        value: 'hasFreeShipping',
+      },
+      {
+        text: '更新日',
+        value: 'updatedAt',
+      },
+    ]
+
+    const {
+      options,
+      offset,
+      itemsPerPage,
+      updateCurrentPage,
+      handleUpdateItemsPerPage,
+    } = usePagination()
+
+    const { fetchState } = useFetch(async () => {
+      try {
+        await shippingStore.fetchShippings(itemsPerPage.value, offset.value)
+      } catch (err) {
+        console.log(err)
+      }
+    })
+
+    return {
+      fetchState, // 初回ロード状況
+      totalItems, // サーバ上の配送情報の個数
+      options, // ページネーションのオプション
+      itemsPerPage, // 1ページあたりの表示件数
+      headers, // テーブルヘッダー
+      shippings, // 配送情報一覧
+      // 関数
+      dateFormatter,
+      updateCurrentPage,
+      handleUpdateItemsPerPage,
+    }
+  },
+})
+</script>
