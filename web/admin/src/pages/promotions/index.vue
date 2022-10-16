dbeebfaefb019537f7c89c09a4c1b5f1d1f3b340
濵田広大 <hamany0326@gmail.com>
2022-10-16 23:20:45 +0900

    Refactor: refactor console

--- web/admin/src/pages/promotions/index.vue
+++ web/admin/src/pages/promotions/index.vue
@@ -141,7 +141,7 @@ export default defineComponent({
       try {
         await promotionStore.deletePromotion(selectedId.value)
       } catch (err) {
-        console.log
+        console.log(err)
       }
       deleteDialog.value = false
     }
