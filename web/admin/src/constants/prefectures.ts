8ee358350834fe0a1811b46fb02dd5857c07a498
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2022-10-17 22:17:01 +0900

    feat: 都道府県のリストを定数で持っておく (#462)

---
+++ web/admin/src/constants/prefectures.ts
@@ -0,0 +1,58 @@
+interface PrefecturesListItem {
+  id: number
+  text: string
+  value: string
+}
+
+/**
+ * 都道府県一覧
+ */
+export const prefecturesList: PrefecturesListItem[] = [
+  { id: 1, text: '北海道', value: 'hokkaido' },
+  { id: 2, text: '青森県', value: 'aomori' },
+  { id: 3, text: '岩手県', value: 'iwate' },
+  { id: 4, text: '宮城県', value: 'miyagi' },
+  { id: 5, text: '秋田県', value: 'akita' },
+  { id: 6, text: '山形県', value: 'yamagata' },
+  { id: 7, text: '福島県', value: 'fukushima' },
+  { id: 8, text: '茨城県', value: 'ibaraki' },
+  { id: 9, text: '栃木県', value: 'tochigi' },
+  { id: 10, text: '群馬県', value: 'gunma' },
+  { id: 11, text: '埼玉県', value: 'saitama' },
+  { id: 12, text: '千葉県', value: 'chiba' },
+  { id: 13, text: '東京都', value: 'tokyo' },
+  { id: 14, text: '神奈川県', value: 'kanagawa' },
+  { id: 15, text: '新潟県', value: 'niigata' },
+  { id: 16, text: '富山県', value: 'toyama' },
+  { id: 17, text: '石川県', value: 'ishikawa' },
+  { id: 18, text: '福井県', value: 'fukui' },
+  { id: 19, text: '山梨県', value: 'yamanashi' },
+  { id: 20, text: '長野県', value: 'nagano' },
+  { id: 21, text: '岐阜県', value: 'gifu' },
+  { id: 22, text: '静岡県', value: 'shizuoka' },
+  { id: 23, text: '愛知県', value: 'aichi' },
+  { id: 24, text: '三重県', value: 'mie' },
+  { id: 25, text: '滋賀県', value: 'shiga' },
+  { id: 26, text: '京都府', value: 'kyoto' },
+  { id: 27, text: '大阪府', value: 'osaka' },
+  { id: 28, text: '兵庫県', value: 'hyogo' },
+  { id: 29, text: '奈良県', value: 'nara' },
+  { id: 30, text: '和歌山県', value: 'wakayama' },
+  { id: 31, text: '鳥取県', value: 'tottori' },
+  { id: 32, text: '島根県', value: 'shimane' },
+  { id: 33, text: '岡山県', value: 'okayama' },
+  { id: 34, text: '広島県', value: 'hiroshima' },
+  { id: 35, text: '山口県', value: 'yamaguchi' },
+  { id: 36, text: '徳島県', value: 'tokushima' },
+  { id: 37, text: '香川県', value: 'kagawa' },
+  { id: 38, text: '愛媛県', value: 'ehime' },
+  { id: 39, text: '高知県', value: 'kochi' },
+  { id: 40, text: '福岡県', value: 'fukuoka' },
+  { id: 41, text: '佐賀県', value: 'saga' },
+  { id: 42, text: '長崎県', value: 'nagasaki' },
+  { id: 43, text: '熊本県', value: 'kumamoto' },
+  { id: 44, text: '大分県', value: 'oita' },
+  { id: 45, text: '宮崎県', value: 'miyazaki' },
+  { id: 46, text: '鹿児島県', value: 'kagoshima' },
+  { id: 47, text: '沖縄県', value: 'okinawa' },
+]
