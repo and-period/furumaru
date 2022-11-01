147b8c5f5097868dd555bcc12156cbee25cccb52
sea-kai <sea_rr10_ch@outlook.com>
2022-11-02 03:33:17 +0900

    feat: added getStream to ivs pkg (#517)
    * feat: added getStream to ivs pkg
    
    * chore: make mockgen
    
    * fix: change input value

---
+++ api/pkg/ivs/stream.go
@@ -0,0 +1,23 @@
+package ivs
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ivs"
+)
+
+type GetStreamParams struct {
+	ChannelArn string
+}
+
+func (c *client) GetStream(ctx context.Context, params *GetStreamParams) (*ivs.GetStreamOutput, error) {
+	in := &ivs.GetStreamInput{
+		ChannelArn: aws.String(params.ChannelArn),
+	}
+	out, err := c.ivs.GetStream(ctx, in)
+	if err != nil {
+		return nil, c.streamError(err)
+	}
+	return out, nil
+}
