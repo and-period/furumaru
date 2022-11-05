85dd6293b037305c34e154054b4630d777803e66
taba2424 <tadashi-112@hotmail.co.jp>
2022-11-05 22:39:33 +0900

    feat(terraform): create sqs resource

---
+++ infra/terraform/env/shared/01-ecr/05-media-resizer.tf
@@ -0,0 +1,19 @@
+module "ecr_media_resizer" {
+  source = "./../../../modules/ecr/private"
+
+  #####################################################################
+  # ECR Repository (Private)
+  #####################################################################
+  repository_name = "furumaru/media-resizer"
+
+  image_tag_mutability = "MUTABLE"
+  image_scan_on_push   = false
+
+  lifecycle_policy_untagged_priority       = 1
+  lifecycle_policy_untagged_description    = "タグ付けなしイメージの定期削除用"
+  lifecycle_policy_untagged_retention_days = 7
+
+  lifecycle_policy_priority    = 11
+  lifecycle_policy_description = "イメージの世代管理 (5世代まで)"
+  lifecycle_policy_image_count = 5
+}
