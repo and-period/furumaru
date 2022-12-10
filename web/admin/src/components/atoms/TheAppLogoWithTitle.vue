2299701b2106baff49d1b5c990e31321a30b3068
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2022-12-11 00:54:44 +0900

    feat: アプリケーションロゴとタイトルのコンポーネントを作成
    - atomsになるかは微妙だけどとりあえず仮置

---
+++ web/admin/src/components/atoms/TheAppLogoWithTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <img
+    class="logo"
+    alt="ふるマル ロゴ"
+    src="/app-logo-with-title.svg"
+    :width="width"
+  />
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    width: {
+      type: Number,
+      default: 200,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.logo {
+  height: auto;
+  display: block;
+}
+</style>
---
+++ web/admin/src/components/atoms/TheAppTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <img class="title" alt="ふるマル" src="/app-title.svg" :width="width" />
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    width: {
+      type: Number,
+      default: 140,
+    },
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.title {
+  height: auto;
+}
+</style>
