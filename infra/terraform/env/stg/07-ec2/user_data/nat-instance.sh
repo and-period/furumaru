6c9bd3a8d838c2ecf7a3442796736140ab363bb0
taba2424 <tadashi-112@hotmail.co.jp>
2023-01-01 11:46:35 +0900

    fix(terraform): fix ec2 user data (#649)

--- infra/terraform/env/stg/07-ec2/user_data/nat-instance.sh
+++ infra/terraform/env/stg/07-ec2/user_data/nat-instance.sh
@@ -13,10 +13,11 @@ sudo localectl set-locale LC_CTYPE=ja_JP.utf8
 sudo localectl set-keymap jp106
 
 # Nat Instance (https://docs.aws.amazon.com/ja_jp/vpc/latest/userguide/VPC_NAT_Instance.html)
-sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -w net.ipv4.ip_forward=1 | sudo tee -a /etc/sysctl.conf
 sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo yum install -y iptables-services
 sudo service iptables save
+sudo systemctl enable --now iptables
 
 # Package Manager (asdf - https://asdf-vm.com/guide/getting-started.html#_1-install-dependencies)
 sudo git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
