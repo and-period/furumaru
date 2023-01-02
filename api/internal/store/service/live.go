4d60f84e46e533afcade670954ad7a32281071d7
kaito <sea_rr10_ch@outlook.com>
2023-01-03 01:53:50 +0900

    feat: add UpdateLivePublic to Service layer

--- api/internal/store/service/live.go
+++ api/internal/store/service/live.go
@@ -5,9 +5,11 @@ import (
 
 	"github.com/and-period/furumaru/api/internal/exception"
 	"github.com/and-period/furumaru/api/internal/store"
+	"github.com/and-period/furumaru/api/internal/store/database"
 	"github.com/and-period/furumaru/api/internal/store/entity"
-	"github.com/and-period/furumaru/api/pkg/ivs"
+	pivs "github.com/and-period/furumaru/api/pkg/ivs"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	ivs "github.com/aws/aws-sdk-go-v2/service/ivs"
 	"github.com/aws/aws-sdk-go-v2/service/ivs/types"
 	"golang.org/x/sync/errgroup"
 )
@@ -29,21 +31,21 @@ func (s *service) GetLive(ctx context.Context, in *store.GetLiveInput) (*entity.
 	)
 	eg, ectx := errgroup.WithContext(ctx)
 	eg.Go(func() (err error) {
-		in := &ivs.GetChannelParams{
+		in := &pivs.GetChannelParams{
 			Arn: live.ChannelArn,
 		}
 		channel, err = s.ivs.GetChannel(ectx, in)
 		return
 	})
 	eg.Go(func() (err error) {
-		in := &ivs.GetStreamParams{
+		in := &pivs.GetStreamParams{
 			ChannelArn: live.ChannelArn,
 		}
 		stream, err = s.ivs.GetStream(ectx, in)
 		return
 	})
 	eg.Go(func() (err error) {
-		in := &ivs.GetStreamKeyParams{
+		in := &pivs.GetStreamKeyParams{
 			StreamKeyArn: live.StreamKeyArn,
 		}
 		streamKey, err = s.ivs.GetStreamKey(ectx, in)
@@ -63,4 +65,32 @@ func (s *service) GetLive(ctx context.Context, in *store.GetLiveInput) (*entity.
 	}
 	live.FillIVS(*fillIvsParams)
 	return live, nil
+}
+
+func (s *service) UpdateLivePublic(ctx context.Context, in *store.UpdateLivePublicInput) error {
+	if err := s.validator.Struct(in); err != nil {
+		return exception.InternalError(err)
+	}
+	live, err := s.db.Live.Get(ctx, in.LiveID)
+	if err != nil {
+		return exception.InternalError(err)
+	}
+	params := &database.UpdateLivePublicParams{
+		Published: in.Published,
+		Canceled:  in.Canceled,
+	}
+	ivs := func(ctx context.Context) (*ivs.CreateChannelOutput, error) {
+		if live.ChannelArn != "" {
+			return nil, exception.ErrAlreadyExists
+		}
+		in := &pivs.CreateChannelParams{
+			LatencyMode: "NORMAL",
+			Name:        in.ChannelName,
+			ChannelType: "BASIC",
+		}
+		cout, err := s.ivs.CreateChannel(ctx, in)
+		return cout, err
+	}
+	err = s.db.Live.UpdateLivePublic(ctx, in.LiveID, params, ivs)
+	return exception.InternalError(err)
 }
