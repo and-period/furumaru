6adf5c21713c16026bd9ea3749e9b74259ffed8b
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-01-13 00:01:50 +0900

    feat: コンポーネントの作成

---
+++ web/admin/src/components/molecules/TheProductInventoryForm.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <v-text-field
+        v-model.number="inventoryValue"
+        :error-messages="inventoryErrorMessage"
+        type="number"
+        label="在庫数"
+      />
+      <v-spacer />
+    </div>
+
+    <div class="d-flex">
+      <v-select v-model="itemUnitValue" label="単位" :items="itemUnitItems" />
+      <v-spacer />
+    </div>
+
+    <div class="d-flex align-center">
+      <v-text-field
+        v-model="itemDescriptionValue"
+        label="単位説明"
+        :error-messages="itemDescriptionErrorMessage"
+      />
+      <p class="ml-12 mb-0">ex) 1kg → 5個入り</p>
+      <v-spacer />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    inventory: {
+      type: Number,
+      default: 0,
+    },
+    inventoryErrorMessage: {
+      type: String,
+      default: '',
+    },
+    itemUnit: {
+      type: String,
+      default: '個',
+    },
+    itemDescription: {
+      type: String,
+      default: '',
+    },
+    itemDescriptionErrorMessage: {
+      type: String,
+      default: '',
+    },
+  },
+
+  setup(props, { emit }) {
+    const itemUnitItems = ['個', '瓶']
+
+    const inventoryValue = computed({
+      get: () => props.inventory,
+      set: (val: number) => emit('update:inventory', val),
+    })
+
+    const itemUnitValue = computed({
+      get: () => props.itemUnit,
+      set: (val: string) => emit('update:itemUnit', val),
+    })
+
+    const itemDescriptionValue = computed({
+      get: () => props.itemDescription,
+      set: (val: string) => emit('update:itemDescription', val),
+    })
+
+    return {
+      // 定数
+      itemUnitItems,
+      // リアクティブ変数
+      inventoryValue,
+      itemUnitValue,
+      itemDescriptionValue,
+    }
+  },
+})
+</script>
