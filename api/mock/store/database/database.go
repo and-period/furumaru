606a616e91b4614546fab14193bfa18086fd4fcc
kaito <sea_rr10_ch@outlook.com>
2023-01-20 02:33:25 +0900

    chore: make mockgen

--- api/mock/store/database/database.go
+++ api/mock/store/database/database.go
@@ -1050,6 +1050,26 @@ func (mr *MockLiveMockRecorder) MultiGet(ctx, liveIDs interface{}, fields ...int
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MultiGet", reflect.TypeOf((*MockLive)(nil).MultiGet), varargs...)
 }
 
+// MultiGetByScheduleID mocks base method.
+func (m *MockLive) MultiGetByScheduleID(ctx context.Context, scheduleID string, fields ...string) (entity.Lives, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{ctx, scheduleID}
+	for _, a := range fields {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "MultiGetByScheduleID", varargs...)
+	ret0, _ := ret[0].(entity.Lives)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// MultiGetByScheduleID indicates an expected call of MultiGetByScheduleID.
+func (mr *MockLiveMockRecorder) MultiGetByScheduleID(ctx, scheduleID interface{}, fields ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{ctx, scheduleID}, fields...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MultiGetByScheduleID", reflect.TypeOf((*MockLive)(nil).MultiGetByScheduleID), varargs...)
+}
+
 // Update mocks base method.
 func (m *MockLive) Update(ctx context.Context, liveID string, params *database.UpdateLiveParams) error {
 	m.ctrl.T.Helper()
