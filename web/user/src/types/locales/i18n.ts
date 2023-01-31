1743cf5ac5894d5a89875bb74636e5b1584bc37b
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-02-01 00:05:15 +0900

    feat: 型定義と言語毎の文言を修正

--- web/user/src/types/locales/i18n.ts
+++ web/user/src/types/locales/i18n.ts
@@ -1,34 +1,37 @@
 export interface I18n {
   layout: {
     header: {
-      becomeShopOwner: string
-      cartEmptyMessage: string
-      cartNotEmptyMessage: string
-      signUp: string
-      signIn: string
-      changeLocaleText: string
-    }
-  }
+      becomeShopOwner: string;
+      cartEmptyMessage: string;
+      cartNotEmptyMessage: string;
+      signUp: string;
+      signIn: string;
+      changeLocaleText: string;
+    };
+  };
   auth: {
     signIn: {
-      email: string
-      password: string
-      forgetPasswordLink: string
-      signIn: string
-      dontHaveAccount: string
-      signUpLink: string
-    }
+      pageName: string;
+      email: string;
+      password: string;
+      forgetPasswordLink: string;
+      signIn: string;
+      dontHaveAccount: string;
+      signUpLink: string;
+    };
     signUp: {
-      tel: string
-      email: string
-      password: string
-      passwordConfirm: string
-      signUp: string
-      alreadyHas: string
-    }
+      pageName: string;
+      tel: string;
+      email: string;
+      password: string;
+      passwordConfirm: string;
+      signUp: string;
+      alreadyHas: string;
+    };
     verify: {
-      message: string
-      btnText: string
-    }
-  }
+      pageName: string;
+      message: string;
+      btnText: string;
+    };
+  };
 }
