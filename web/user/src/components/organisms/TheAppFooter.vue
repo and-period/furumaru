9d71f6ae1fdfc914dedc6acdaa77bc1fcdcac496
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-02-02 00:22:32 +0900

    feat: フッターコンポーネントを実装(wip)

---
+++ web/user/src/components/organisms/TheAppFooter.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <footer class="text-center bg-base relative">
+    <div class="text-center absolute w-full -top-12">
+      <the-footer-cat-icon class="inline bg-transparent" />
+    </div>
+    <div class="text-white bg-green py-5">
+      <the-stack class="gap-y-3">
+        <p class="text-lg font-bold tracking-widest">
+          Follow us !
+        </p>
+        <div class="flex justify-center gap-x-3">
+          <a href="#" class="block">
+            <the-instagram-icon class="inline w-5 h-5" />
+          </a>
+          <a href="#" class="block">
+            <the-facebook-icon class="inline w-5 h-5" />
+          </a>
+        </div>
+
+        <p class="uppercase text-sm">
+          &copy; FURUSATO MARCHE All rights Reserved.
+        </p>
+      </the-stack>
+    </div>
+  </footer>
+</template>
