05503379c97f442e47a4fc0492405698b3cd8323
kaito <sea_rr10_ch@outlook.com>
2023-02-08 23:08:10 +0900

    fix: chamge data type

--- infra/mysql/schema/2023012801-stores-add-column-lives.sql
+++ infra/mysql/schema/2023012801-stores-add-column-lives.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `stores`.`lives` DROP COLUMN `published`;
 ALTER TABLE `stores`.`lives` DROP COLUMN `canceled`;
 
-ALTER TABLE `stores`.`lives` ADD COLUMN `status` VARCHAR(16) NOT NULL;
+ALTER TABLE `stores`.`lives` ADD COLUMN `status` INT NOT NULL;
