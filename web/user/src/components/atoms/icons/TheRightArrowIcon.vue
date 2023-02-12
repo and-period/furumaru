93f1eb71ffbbd4141853f37e2221376424a345a4
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-02-12 22:14:44 +0900

    feat: 矢印のコンポーネントを追加

---
+++ web/user/src/components/atoms/icons/TheRightArrowIcon.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="25" height="25" viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M2.62573 25L0.504412 22.8787L10.7575 12.6256L0.504414 2.37258L2.62573 0.251263L15.0001 12.6256L2.62573 25Z" fill="#604C3F" />
+  </svg>
+</template>
