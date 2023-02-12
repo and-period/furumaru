9ed42fc41033228572831c65e2fdc5c98f234a5d
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-02-12 22:15:37 +0900

    feat: カルーセルコンポーネントの実装

--- web/user/src/pages/index.vue
+++ web/user/src/pages/index.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+
+const banners: string[] = [
+  '/_nuxt/img/banner.png',
+  '/_nuxt/img/banner.png',
+  '/_nuxt/img/banner.png'
+]
+</script>
+
 <template>
-  <div />
+  <div>
+    <the-carousel :images="banners" />
+  </div>
 </template>
