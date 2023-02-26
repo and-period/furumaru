08cb018ed5931660cbb4a18fe9e7c1431985a406
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-02-26 18:53:15 +0900

    feat:画面サイズがタブレット以下の場合に ヘッダーメニューを折りたたむように変更

--- web/user/src/components/organisms/header/TheAppHeader.vue
+++ web/user/src/components/organisms/header/TheAppHeader.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { HeaderMenuItem, LinkItem } from '~/types/props'
+import { FooterMenuItem, HeaderMenuItem, LinkItem } from '~/types/props'
 
 interface Props {
+  homePath: string
   menuItems: HeaderMenuItem[]
   isAuthenticated: boolean
   authenticatedAccountMenuItem: LinkItem[]
@@ -12,16 +13,35 @@ interface Props {
   cartIsEmpty: boolean
   cartMenuMessage: string
   cartItems: any[]
+  spMenuItems: LinkItem[]
+  footerMenuItems: FooterMenuItem[]
 }
 
 defineProps<Props>()
+
+const spMenuOpen = ref<boolean>(false)
+
+const handleClickMenuIconButton = () => {
+  spMenuOpen.value = !spMenuOpen.value
+}
+
+const closeSpMenu = () => {
+  spMenuOpen.value = false
+}
+
+const handleClickMenuItem = (item: HeaderMenuItem | FooterMenuItem) => {
+  item.onClick()
+  closeSpMenu()
+}
 </script>
 
 <template>
-  <div class="flex px-10 py-4 bg-base items-center">
-    <the-marche-logo class="m-0 " />
+  <div class="flex md:px-10 px-4 md:py-4 py-2 bg-base items-center relative drop-shadow-sm">
+    <nuxt-link :to="homePath" @click="closeSpMenu">
+      <the-marche-logo class="m-0 md:w-64 w-32" />
+    </nuxt-link>
     <div class="flex items-center justify-end w-full text-main">
-      <nav class="mr-16">
+      <nav class="mr-16 xl:block hidden">
         <ul class="list-none flex gap-x-10">
           <li v-for="item, i in menuItems" :key="i">
             <a href="#" :class="{'border-b border-main pb-1': item.active}" @click="item.onClick">
@@ -31,7 +51,7 @@ defineProps<Props>()
         </ul>
       </nav>
 
-      <div class="flex gap-x-8 items-center">
+      <div class="flex items-center md:gap-x-8 gap-x-2">
         <the-pc-account-menu
           :is-authenticated="isAuthenticated"
           :authenticated-menu-items="authenticatedAccountMenuItem"
@@ -50,7 +70,43 @@ defineProps<Props>()
           :cart-menu-message="cartMenuMessage"
           :cart-items="cartItems"
         />
+
+        <the-icon-button class="xl:hidden h-10 w-10" @click="handleClickMenuIconButton">
+          <the-outline-close-icon v-if="spMenuOpen" />
+          <the-menu-icon v-else />
+        </the-icon-button>
       </div>
+    </div>
+  </div>
+
+  <div
+    :class="{
+      'absolute z-[50] bg-base block w-full h-screen transition duration-300 px-16': true,
+      'opacity-100': spMenuOpen,
+      'opacity-0 invisible': !spMenuOpen
+    }"
+  >
+    <div class="flex flex-col gap-4 text-center text-main">
+      <a v-for="item, i in menuItems" :key="i" href="#" @click="handleClickMenuItem(item)">
+        {{ item.text }}
+      </a>
+      <hr class=" border-dashed border-main my-6">
+      <nuxt-link v-for="item ,i in spMenuItems" :key="i" :to="item.href">
+        {{ item.text }}
+      </nuxt-link>
+
+      <div class="flex justify-center gap-x-3 my-12">
+        <the-icon-button>
+          <the-instagram-icon id="header-instagram-icon" fill="#604C3F" />
+        </the-icon-button>
+        <the-icon-button>
+          <the-facebook-icon id="header-facebook-icon" fill="#604C3F" />
+        </the-icon-button>
+      </div>
+
+      <a v-for="item, i in footerMenuItems" :key="i" href="#" @click="handleClickMenuItem(item)">
+        {{ item.text }}
+      </a>
     </div>
   </div>
 </template>
