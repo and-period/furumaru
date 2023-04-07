f8d661fbb7023420a8f2471afec53e7c8c9ad3e3
taba2424 <ta.g-h-r.1260@docomo.ne.jp>
2023-04-07 23:50:05 +0900

    feat(web): update api types

---
+++ web/user/src/types/api/models/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthResponse';
+export * from './AuthUserResponse';
+export * from './CreateAuthRequest';
+export * from './CreateAuthResponse';
+export * from './CreateContactRequest';
+export * from './ErrorResponse';
+export * from './ForgotAuthPasswordRequest';
+export * from './InitializeAuthRequest';
+export * from './RefreshAuthTokenRequest';
+export * from './ResetAuthPasswordRequest';
+export * from './SignInRequest';
+export * from './UpdateAuthEmailRequest';
+export * from './UpdateAuthPasswordRequest';
+export * from './V1CreateAuthResponse';
+export * from './VerifyAuthEmailRequest';
+export * from './VerifyAuthRequest';
