570dd5c9f9cbe4f2171be00d6bcf1738b2489c2e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-04-28 00:05:02 +0900

    fix: メールアドレス更新フォームでフロントエンド側でバリデーションを実施する

--- web/admin/src/components/templates/AuthEditEmail.vue
+++ web/admin/src/components/templates/AuthEditEmail.vue
@@ -1,44 +1,58 @@
 <script lang="ts" setup>
+import useVuelidate from '@vuelidate/core'
 import { UpdateAuthEmailRequest } from '~/types/api'
 import { AlertType } from '~/lib/hooks'
+import { required, email, getErrorMessage } from '~/lib/validations'
 
-const props = defineProps({
-  isAlert: {
-    type: Boolean,
-    default: false
-  },
-  alertType: {
-    type: String as PropType<AlertType>,
-    default: undefined
-  },
-  alertText: {
-    type: String,
-    default: ''
-  },
-  formData: {
-    type: Object as PropType<UpdateAuthEmailRequest>,
-    default: () => ({
-      email: ''
-    })
+interface Props {
+  isAlert: boolean,
+  alertType: AlertType,
+  alertText: string,
+  formData: UpdateAuthEmailRequest
+}
+
+const props = defineProps<Props>()
+
+const emits = defineEmits<{
+  (e: 'submit'): void,
+  (e: 'update:isAlert', val: boolean): void
+  (e: 'update:formData', val: UpdateAuthEmailRequest): void
+}>()
+
+const isAlertValue = computed({
+  get: () => props.isAlert,
+  set: (val: boolean) => emits('update:isAlert', val)
+})
+
+const formDataValue = computed({
+  get: () => props.formData,
+  set: (val: UpdateAuthEmailRequest) => emits('update:formData', val)
+})
+
+const rules = computed(() => {
+  return {
+    email: { required, email }
   }
 })
 
-const emit = defineEmits<{
-  (e: 'submit'): void
-}>()
+const v$ = useVuelidate(rules, formDataValue)
 
-const onSubmit = (): void => {
-  emit('submit')
+const onSubmit = async () => {
+  const result = await v$.value.$validate()
+  if (!result) {
+    return
+  }
+  emits('submit')
 }
 </script>
 
 <template>
-  <v-alert v-model="props.isAlert" :type="props.alertType" :text="props.alertText" />
+  <v-alert v-model="isAlertValue" :type="props.alertType" :text="props.alertText" class="mb-2" />
   <v-card elevation="0">
     <v-card-title>メールアドレス変更</v-card-title>
     <v-form @submit.prevent="onSubmit">
       <v-card-text>
-        <v-text-field v-model="props.formData.email" label="新規メールアドレス" />
+        <v-text-field v-model="v$.email.$model" :error-messages="getErrorMessage(v$.email.$errors)" label="新規メールアドレス" />
       </v-card-text>
       <v-card-actions>
         <v-btn type="submit" block color="primary" variant="outlined">
