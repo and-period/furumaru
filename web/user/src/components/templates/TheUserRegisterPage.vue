204a5af2d38ea88b6aebff3fdfea6742eed6ff11
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-05-13 00:18:19 +0900

    feat: ユーザー登録ページのフロントデザイン実装

---
+++ web/user/src/components/templates/TheUserRegisterPage.vue
@@ -0,0 +1,75 @@
+<script setup lang='ts'>
+interface Props {
+  pageName: string
+  buttonText: string
+  name: string
+  nameLabel: string
+  namePlaceholder: string
+  nameErrorMessage: string
+  username: string
+  usernameLabel: string
+  usernamePlaceholder: string
+  usernameErrorMessage: string
+  cautionText: string
+}
+
+const porps = defineProps<Props>()
+
+interface Emits {
+  (e: 'update:name', val: string): void
+  (e: 'update:username', val: string): void
+}
+
+const emits = defineEmits<Emits>()
+
+const nameValue = computed({
+  get: () => porps.name,
+  set: (val: string) => emits('update:name', val)
+})
+
+const usernameValue = computed({
+  get: () => porps.username,
+  set: (val: string) => emits('update:username', val)
+})
+</script>
+
+<template>
+  <div class="block m-auto sm:w-[560px]">
+    <the-marche-logo class="mb-10" />
+    <the-card>
+      <the-card-title>
+        {{ pageName }}
+      </the-card-title>
+      <the-card-content>
+        <form>
+          <the-stack class="gap-y-6">
+            <the-text-input
+              v-model="nameValue"
+              :label="nameLabel"
+              :placeholder="namePlaceholder"
+              :with-label="false"
+              :error-message="nameErrorMessage"
+              type="text"
+              required
+            />
+            <the-text-input
+              v-model="usernameValue"
+              :label="usernameLabel"
+              :placeholder="usernamePlaceholder"
+              :with-label="false"
+              :error-message="usernameErrorMessage"
+              type="text"
+              required
+            />
+
+            <p class="my-6 whitespace-pre-wrap" v-html="cautionText" />
+
+            <the-submit-button>
+              {{ buttonText }}
+            </the-submit-button>
+          </the-stack>
+        </form>
+      </the-card-content>
+    </the-card>
+  </div>
+</template>
---
+++ web/user/src/pages/register.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { I18n } from '~/types/locales'
+definePageMeta({
+  layout: 'auth'
+})
+
+const i18n = useI18n()
+const t = (str: keyof I18n['auth']['register']) => {
+  return i18n.t(`auth.register.${str}`)
+}
+
+const name = ref<string>('')
+const username = ref<string>('')
+
+</script>
+
+<template>
+  <the-user-register-page
+    v-model:name="name"
+    v-model:username="username"
+    :page-name="t('pageName')"
+    :name-label="t('name')"
+    name-error-message=""
+    :name-placeholder="t('name')"
+    :username-label="t('username')"
+    :username-placeholder="t('username')"
+    username-error-message=""
+    :button-text="t('btnText')"
+    :caution-text="i18n.t('auth.register.cautionText', {privacyPolicyLinkText: `<a class='underline' href='#''>${t('privacyPolicyLinkText')}</a>`, termsOfService: `<a class='underline' href='#''>${t('termsOfServiceLink')}</a>`})"
+  />
+</template>
