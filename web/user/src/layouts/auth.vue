e8049225388b5b8d1c945589751479c2a6b679f9
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-05-18 23:54:34 +0900

    fix: 認証系画面でoverflowの設定がなくレイアウトが崩れて見えていたので修正

--- web/user/src/layouts/auth.vue
+++ web/user/src/layouts/auth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen bg-base flex flex-col items-center pt-16">
+  <div class="h-screen bg-base flex flex-col items-center py-16 overflow-auto">
     <slot />
   </div>
 </template>
