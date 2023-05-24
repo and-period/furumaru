fdebb81283f2f6adc316c489bcaa0f376591ed59
濵田広大 <hamany0326@gmail.com>
2023-05-25 01:44:22 +0900

    Add: added about page

--- web/user/src/pages/about.vue
+++ web/user/src/pages/about.vue
@@ -46,25 +46,30 @@ const circleItems = computed<CircleItem[]>(() => [
 
 <template>
   <div>
-    <div class="flex flex-wrap items-center justify-center gap-x-7 mt-10">
+    <div class="flex flex-wrap items-center justify-center gap-x-[120px] pt-[80px]">
       <div class="text-main mb-10">
-        <p class="text-2xl font-semibold mb-12">
+        <p class="text-[32px] font-semibold mb-12">
           {{ t('leadSentence') }}
         </p>
-        <p class="leading-7" v-html="t('description')" />
+        <p class="leading-10 text-xl whitespace-pre-wrap" v-html="t('description')" />
       </div>
       <the-concept />
     </div>
 
-    <div class="flex flex-wrap gap-10 my-10 justify-center">
+    <div class="flex flex-wrap gap-10 my-20 justify-center">
       <the-circle v-for="item, i in circleItems" :key="i" :num="i + 1">
         <p class="font-bold text-2xl mb-6" v-html="item.title" />
-        <p class="text-xl leading-9 text-left break-words">
+        <p class="text-xl leading-9 text-left break-words whitespace-pre-wrap">
           {{ item.description }}
         </p>
-        <div v-if="item.linkItem" class="mt-10 font-semibold text-lg">
+        <div v-if="item.linkItem" class="mt-10 font-semibold text-xl">
           <nuxt-link :to="item.linkItem.href">
-            {{ item.linkItem.text }}
+            <div class="">
+              {{ item.linkItem.text }}
+              <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.49023 16.9707L0.0356148 15.5161L7.06628 8.48542L0.0356165 1.45476L1.49024 0.000140667L9.97552 8.48542L1.49023 16.9707Z" fill="#604C3F"/>
+              </svg>
+            </div>
           </nuxt-link>
         </div>
       </the-circle>
