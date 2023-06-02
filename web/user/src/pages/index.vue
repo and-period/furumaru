bd1b5635c27ddcff736398bd870056bbd2ada5bb
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-06-03 00:11:59 +0900

    feat: アーカイブ部分の横スクロールのボタンを配置

--- web/user/src/pages/index.vue
+++ web/user/src/pages/index.vue
@@ -6,6 +6,47 @@ const shoppingStore = useShoppingStore()
 
 shoppingStore.setupDummyData()
 
+const archiveRef = ref<HTMLDivElement | null>(null)
+const archiveRefScrollLeft = ref<number>(0)
+
+const updateScrollLeft = () => {
+  if (archiveRef.value) {
+    archiveRefScrollLeft.value = archiveRef.value.scrollLeft
+  }
+}
+
+onMounted(() => {
+  if (archiveRef.value) {
+    archiveRef.value.addEventListener('scroll', updateScrollLeft)
+  }
+})
+
+onUnmounted(() => {
+  if (archiveRef.value) {
+    archiveRef.value.removeEventListener('scroll', updateScrollLeft)
+  }
+})
+
+const handleClickArchiveLeftButton = () => {
+  if (archiveRef.value) {
+    archiveRef.value.scrollTo({
+      left: archiveRef.value.scrollLeft - 368,
+      behavior: 'smooth'
+    })
+    updateScrollLeft()
+  }
+}
+
+const handleClickArchiveRightButton = () => {
+  if (archiveRef.value) {
+    archiveRef.value.scrollTo({
+      left: archiveRef.value.scrollLeft + 368,
+      behavior: 'smooth'
+    })
+    updateScrollLeft()
+  }
+}
+
 const banners: string[] = [
   '/img/banner.png',
   '/img/banner.png',
@@ -45,14 +86,26 @@ const banners: string[] = [
         title="archive"
         sub-title="過去のマルシェ"
       >
-        <div class="flex flex-nowrap gap-x-8 overflow-x-scroll">
-          <the-archive-item
-            v-for="archiveItem in MOCK_ARCHIVES_ITEMS"
-            :id="archiveItem.id"
-            :key="archiveItem.id"
-            :title="archiveItem.title"
-            :img-src="archiveItem.imgSrc"
-          />
+        <div class="relative flex">
+          <div class="absolute left-4 flex items-center h-[208px]">
+            <the-icon-button class="bg-white bg-opacity-50 hover:bg-opacity-100" @click="handleClickArchiveLeftButton">
+              <the-left-arrow-icon />
+            </the-icon-button>
+          </div>
+          <div ref="archiveRef" class="flex flex-nowrap gap-x-8 overflow-x-scroll">
+            <the-archive-item
+              v-for="archiveItem in MOCK_ARCHIVES_ITEMS"
+              :id="archiveItem.id"
+              :key="archiveItem.id"
+              :title="archiveItem.title"
+              :img-src="archiveItem.imgSrc"
+            />
+          </div>
+          <div class="absolute right-4 flex items-center h-[208px]">
+            <the-icon-button class="bg-white bg-opacity-50 hover:bg-opacity-100" @click="handleClickArchiveRightButton">
+              <the-right-arrow-icon />
+            </the-icon-button>
+          </div>
         </div>
 
         <div class="w-full text-center mt-10">
