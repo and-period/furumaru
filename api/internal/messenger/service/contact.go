141bdddaf84abdbd352b89b6d0c73dbe5a1e2289
kaito <sea_rr10_ch@outlook.com>
2023-06-16 00:30:47 +0900

    feat: implement createContact Service layer

--- api/internal/messenger/service/contact.go
+++ api/internal/messenger/service/contact.go
@@ -15,3 +15,24 @@ func (s *service) GetContact(ctx context.Context, in *messenger.GetContactInput)
 	contact, err := s.db.Contact.Get(ctx, in.ContactID)
 	return contact, exception.InternalError(err)
 }
+
+func (s *service) CreateContact(ctx context.Context, in *messenger.CreateContactInput) (*entity.Contact, error) {
+	if err := s.validator.Struct(in); err != nil {
+		return nil, exception.InternalError(err)
+	}
+	params := &entity.NewContactParams{
+		Title:       in.Title,
+		Content:     in.Content,
+		Username:    in.Username,
+		Email:       in.Email,
+		PhoneNumber: in.PhoneNumber,
+		Note:        in.Note,
+	}
+	contact := entity.NewContact(params)
+	contact.Fill(in.CategoryID, in.UserID, in.ResponderID)
+	if err := s.db.Contact.Create(ctx, contact); err != nil {
+		return nil, exception.InternalError(err)
+	}
+
+	return contact, nil
+}
