d444bf20940185ed7c65f94045081a3f79543dd3
sea-kai <sea_rr10_ch@outlook.com>
2023-06-20 14:50:43 +0900

    Feat create contact handler (#1012)

--- api/internal/gateway/admin/v1/handler/contact.go
+++ api/internal/gateway/admin/v1/handler/contact.go
@@ -2,18 +2,100 @@ package handler
 
 import (
 	"context"
+	"errors"
 	"net/http"
 
+	"github.com/and-period/furumaru/api/internal/exception"
+	"github.com/and-period/furumaru/api/internal/gateway/admin/v1/request"
 	"github.com/and-period/furumaru/api/internal/gateway/admin/v1/response"
 	"github.com/and-period/furumaru/api/internal/gateway/admin/v1/service"
 	"github.com/and-period/furumaru/api/internal/gateway/util"
 	"github.com/and-period/furumaru/api/internal/messenger"
+	"github.com/and-period/furumaru/api/internal/messenger/entity"
+	"github.com/and-period/furumaru/api/internal/user"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
 )
 
 func (h *handler) contactRoutes(rg *gin.RouterGroup) {
 	arg := rg.Use(h.authentication)
-	arg.GET("/:contactId")
+	arg.GET("/:contactId", h.GetContact)
+}
+
+func (h *handler) CreateContact(ctx *gin.Context) {
+	req := &request.CreateContactRequest{}
+	if err := ctx.BindJSON(req); err != nil {
+		badRequest(ctx, err)
+		return
+	}
+
+	eg, ectx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		if req.UserID == "" {
+			return nil
+		}
+		in := &user.GetUserInput{
+			UserID: req.UserID,
+		}
+		_, err := h.user.GetUser(ectx, in)
+		return err
+	})
+	eg.Go(func() error {
+		if req.ResponderID == "" {
+			return nil
+		}
+		in := &user.GetAdminInput{
+			AdminID: req.ResponderID,
+		}
+		_, err := h.user.GetAdmin(ectx, in)
+		return err
+	})
+	eg.Go(func() error {
+		_, err := h.getContactCategory(ectx, req.CategoryID)
+		return err
+	})
+	err := eg.Wait()
+	if errors.Is(err, exception.ErrNotFound) {
+		badRequest(ctx, err)
+		return
+	}
+	if err != nil {
+		httpError(ctx, err)
+		return
+	}
+
+	in := &messenger.CreateContactInput{
+		Title:       req.Title,
+		Content:     req.Content,
+		CategoryID:  req.CategoryID,
+		Username:    req.Username,
+		UserID:      req.UserID,
+		Email:       req.Email,
+		PhoneNumber: req.PhoneNumber,
+		ResponderID: req.ResponderID,
+		Note:        req.Note,
+	}
+	scontact, err := h.messenger.CreateContact(ctx, in)
+	if err != nil {
+		httpError(ctx, err)
+		return
+	}
+	threadIn := &messenger.CreateThreadInput{
+		ContactID: scontact.ID,
+		UserID:    req.UserID,
+		UserType:  2,
+		Content:   req.Content,
+	}
+	sthread, err := h.messenger.CreateThread(ctx, threadIn)
+	if err != nil {
+		httpError(ctx, err)
+		return
+	}
+	res := &response.ContactResponse{
+		Contact: service.NewContact(scontact).Response(),
+		Threads: service.NewThreads(entity.Threads{sthread}).Response(),
+	}
+	ctx.JSON(http.StatusOK, res)
 }
 
 func (h *handler) GetContact(ctx *gin.Context) {
