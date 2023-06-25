182acd7d1f9e2ef84c001128fd42f52a4934917f
sea-kai <sea_rr10_ch@outlook.com>
2023-06-26 01:21:33 +0900

    Feat: implement  ListContactCategories db layer (#1042)
    * feat: add ContactCategory Delete method to db
    
    * feat: implement ListContactCategories db layer
    
    * chore: make mockgen

--- api/internal/messenger/database/contact_category_test.go
+++ api/internal/messenger/database/contact_category_test.go
@@ -16,6 +16,73 @@ func TestContactCategory(t *testing.T) {
 	assert.NotNil(t, NewContactCategory(nil))
 }
 
+func TestContactCategory_List(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	db := dbClient
+	now := func() time.Time {
+		return current
+	}
+
+	err := deleteAll(ctx)
+	require.NoError(t, err)
+
+	contactCategories := make(entity.ContactCategories, 3)
+	contactCategories[0] = testContactCategory("contactCategory-id01", "種別1", now())
+	contactCategories[1] = testContactCategory("contactCategory-id02", "種別2", now())
+	contactCategories[2] = testContactCategory("contactCategory-id03", "種別3", now())
+	err = db.DB.Create(&contactCategories).Error
+	require.NoError(t, err)
+
+	type args struct {
+		params *ListContactCategoriesParams
+	}
+	type want struct {
+		contactCategories entity.ContactCategories
+		hasErr            bool
+	}
+	tests := []struct {
+		name  string
+		setup func(ctx context.Context, t *testing.T, db *database.Client)
+		args  args
+		want  want
+	}{
+		{
+			name:  "success",
+			setup: func(ctx context.Context, t *testing.T, db *database.Client) {},
+			args: args{
+				params: &ListContactCategoriesParams{
+					Limit:  3,
+					Offset: 0,
+				},
+			},
+			want: want{
+				contactCategories: contactCategories[:3],
+				hasErr:            false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			tt.setup(ctx, t, db)
+
+			db := &contactCategory{db: db, now: now}
+			actual, err := db.List(ctx, tt.args.params)
+			assert.Equal(t, tt.want.hasErr, err != nil, err)
+			assert.ElementsMatch(t, tt.want.contactCategories, actual)
+		})
+	}
+}
+
 func TestContactCategory_Get(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
