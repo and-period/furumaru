16c3dabbbb1bd6d98ace501f9b49c226c55d2838
kaito <sea_rr10_ch@outlook.com>
2023-06-26 00:17:28 +0900

    feat: add UpdateContactRequest

--- api/internal/gateway/admin/v1/request/contact.go
+++ api/internal/gateway/admin/v1/request/contact.go
@@ -22,3 +22,16 @@ const (
 	ContactStatusDone                            // 完了
 	ContactStatusDiscard                         // 対応不要
 )
+
+type UpdateContactRequest struct {
+	Title       string        `json:"title,omitempty"`       // お問い合わせ件名
+	CategoryID  string        `json:"categoryId,omitempty"`  // お問い合わせ種別ID
+	Content     string        `json:"content,omitempty"`     // お問い合わせ内容
+	Username    string        `json:"username,omitempty"`    // 氏名
+	UserID      string        `json:"userId"`                // 問い合わせ作成者ID(null許容)
+	Email       string        `json:"email,omitempty"`       // メールアドレス
+	PhoneNumber string        `json:"phoneNumber,omitempty"` // 電話番号
+	ResponderID string        `json:"responderId"`           // 対応者ID(null許容)
+	Note        string        `json:"note"`                  // 対応者メモ
+	Status      ContactStatus `json:"status,omitempty"`      // お問い合わせステータス
+}
