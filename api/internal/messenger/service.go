e92f4912a92d7ff8a75c51c7626666f4a8df33e2
kaito <sea_rr10_ch@outlook.com>
2023-06-26 00:59:14 +0900

    feat: add DeleteContact service

--- api/internal/messenger/service.go
+++ api/internal/messenger/service.go
@@ -35,6 +35,8 @@ type Service interface {
 	CreateContact(ctx context.Context, in *CreateContactInput) (*entity.Contact, error)
 	// お問い合わせ編集
 	UpdateContact(ctx context.Context, in *UpdateContactInput) error
+	// お問い合わせ削除
+	DeleteContact(ctx context.Context, in *DeleteContactInput) error
 	// お問い合わせ種別取得
 	GetContactCategory(ctx context.Context, in *GetContactCategoryInput) (*entity.ContactCategory, error)
 	// お問い合わせ会話履歴一覧取得(お問い合わせID指定)
