030dccd087e58cee36276536be98d7873678d5a1
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-08-05 22:14:21 +0900

    feat: 買い物かごページを途中まで実装
    - 段ボール部分はまだ
    - 買い物かご情報に入ってくるデータ構造の仕様確認が必要なので一旦ここまで

---
+++ web/user/src/pages/purchase/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { MOCK_CART_ITEMS } from '~/constants/mock'
+
+const cartItems = MOCK_CART_ITEMS
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <div class="text-center text-[20px] font-bold tracking-[2px] text-main">
+      買い物カゴ
+    </div>
+
+    <div class="my-10 border border-orange bg-white px-6 py-7 text-orange">
+      <div>現在のカゴの数：{{ cartItems.length }}</div>
+
+      <ul class="list-disc px-6">
+        <li>マルシェごとのご注文手続き・お届けとなります。</li>
+        <li>
+          買い物カゴごとに送料がかかります。詳しくはこちらからご確認ください。
+        </li>
+      </ul>
+    </div>
+
+    <div class="mt-10 flex flex-col gap-y-10">
+      <the-marche-cart-item
+        v-for="(cartItem, i) in cartItems"
+        :key="i"
+        :marche="cartItem.marche"
+        :box-type="cartItem.boxType"
+        :box-size="cartItem.boxSize"
+        :items="cartItem.items"
+      />
+    </div>
+  </div>
+</template>
