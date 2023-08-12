76af8f89bd36c971d60bedb0b0739646a68f419d
kaito <sea_rr10_ch@outlook.com>
2023-08-12 14:02:54 +0900

    fix: move ectarctDomain State

--- infra/serverless/functions/extractMediaStoreDomain/handler.js
+++ infra/serverless/functions/extractMediaStoreDomain/handler.js
@@ -1,6 +1,6 @@
 exports.extractDomainName = async (event) => {
     console.log(event);
-    const domainName = event[2].ContainerStatus.Container.Endpoint;
+    const domainName = event.ContainerStatus.Container.Endpoint;
     const domainNameWithoutHttp = domainName.replace('https://', '');
     console.log(domainNameWithoutHttp);
     return domainNameWithoutHttp;
