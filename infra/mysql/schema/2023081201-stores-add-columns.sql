00cb2505676f150f7269c94c8dd267e37cea3838
taba2424 <tadashi-112@hotmail.co.jp>
2023-08-14 22:33:23 +0900

    fix(store): fix small

--- infra/mysql/schema/2023081201-stores-add-columns.sql
+++ infra/mysql/schema/2023081201-stores-add-columns.sql
@@ -3,3 +3,4 @@ ALTER TABLE `stores`.`broadcasts` ADD COLUMN `media_live_channel_arn` TEXT NULL 
 ALTER TABLE `stores`.`broadcasts` ADD COLUMN `media_live_rtmp_input_arn` TEXT NULL DEFAULT NULL;
 ALTER TABLE `stores`.`broadcasts` ADD COLUMN `media_live_mp4_input_arn` TEXT NULL DEFAULT NULL;
 ALTER TABLE `stores`.`broadcasts` ADD COLUMN `media_store_container_arn` TEXT NULL DEFAULT NULL;
+ALTER TABLE `stores`.`broadcasts` ADD COLUMN `archive_url` TEXT NULL DEFAULT NULL;
