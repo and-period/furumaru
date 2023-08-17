c769713efaed88abf65cd5136395525f91250987
taba2424 <tadashi-112@hotmail.co.jp>
2023-08-17 23:46:00 +0900

    fix(store): バッチ処理にログの追加

--- api/internal/store/broadcast/scheduler/starter.go
+++ api/internal/store/broadcast/scheduler/starter.go
@@ -51,19 +51,29 @@ func (s *starter) Lambda(ctx context.Context) (err error) {
 }
 
 func (s *starter) Run(ctx context.Context, target time.Time) error {
-	return s.run(ctx, target)
+	if err := s.run(ctx, target); err != nil {
+		s.logger.Error("Failed to run", zap.Time("target", target), zap.Error(err))
+		return err
+	}
+	return nil
 }
 
 // run - ライブ配信のリソース作成と開始処理
 func (s *starter) run(ctx context.Context, target time.Time) error {
 	if err := s.startChannel(ctx, target); err != nil {
+		s.logger.Error("Failed to start channel", zap.Time("target", target), zap.Error(err))
 		return err
 	}
-	return s.createChannel(ctx, target)
+	if err := s.createChannel(ctx, target); err != nil {
+		s.logger.Error("Failed to create channel", zap.Time("target", target), zap.Error(err))
+		return err
+	}
+	return nil
 }
 
 // startChannel - ライブ配信を開始 (5分前)
 func (s *starter) startChannel(ctx context.Context, target time.Time) error {
+	s.logger.Debug("Starting channel...", zap.Time("target", target))
 	params := &database.ListSchedulesParams{
 		StartAtLt: target.Add(5 * time.Minute), // マルシェ開催開始5分前〜
 		EndAtGte:  target,                      // マルシェ開催終了前
@@ -72,6 +82,7 @@ func (s *starter) startChannel(ctx context.Context, target time.Time) error {
 	if err != nil {
 		return err
 	}
+	s.logger.Debug("Got schedules to start", zap.Int("total", len(schedules)))
 	if len(schedules) == 0 {
 		return nil
 	}
@@ -90,9 +101,12 @@ func (s *starter) startChannel(ctx context.Context, target time.Time) error {
 				return err
 			}
 			if broadcast.Status != entity.BroadcastStatusIdle {
+				s.logger.Debug("Channels excluded from start",
+					zap.String("scheduleId", schedule.ID), zap.Int("status", int(broadcast.Status)))
 				return nil // 停止中の場合のみ、起動処理を進める
 			}
 			// TODO: ライブ配信リソースの起動処理
+			s.logger.Info("Not implemented start function", zap.String("scheduleId", schedule.ID))
 			params := &database.UpdateBroadcastParams{
 				Status: entity.BroadcastStatusActive,
 			}
@@ -104,6 +118,7 @@ func (s *starter) startChannel(ctx context.Context, target time.Time) error {
 
 // createChannel - ライブ配信リソースの作成を開始 (30分前)
 func (s *starter) createChannel(ctx context.Context, target time.Time) error {
+	s.logger.Debug("Creating channel...", zap.Time("target", target))
 	params := &database.ListSchedulesParams{
 		StartAtLt: target.Add(30 * time.Minute), // マルシェ開催開始30分前〜
 		EndAtGte:  target,                       // マルシェ開催終了前
@@ -112,6 +127,7 @@ func (s *starter) createChannel(ctx context.Context, target time.Time) error {
 	if err != nil {
 		return err
 	}
+	s.logger.Debug("Got schedules to create", zap.Int("total", len(schedules)))
 	if len(schedules) == 0 {
 		return nil
 	}
@@ -130,6 +146,8 @@ func (s *starter) createChannel(ctx context.Context, target time.Time) error {
 				return err
 			}
 			if broadcast.Status != entity.BroadcastStatusDisabled {
+				s.logger.Debug("Channels excluded from creation",
+					zap.String("scheduleId", schedule.ID), zap.Int("status", int(broadcast.Status)))
 				return nil // リソース未作成の場合のみ、作成処理を進める
 			}
 			payload := &CreatePayload{
@@ -146,9 +164,12 @@ func (s *starter) createChannel(ctx context.Context, target time.Time) error {
 					StreamName: streamName,
 				},
 			}
+			s.logger.Info("Calling step function", zap.String("scheduleId", schedule.ID))
 			if err := s.sfn.StartExecution(ectx, payload); err != nil {
+				s.logger.Error("Succeeded step function", zap.String("scheduleId", schedule.ID), zap.Error(err))
 				return err
 			}
+			s.logger.Info("Succeeded step function", zap.String("scheduleId", schedule.ID))
 			params := &database.UpdateBroadcastParams{
 				Status: entity.BroadcastStatusWaiting,
 			}
