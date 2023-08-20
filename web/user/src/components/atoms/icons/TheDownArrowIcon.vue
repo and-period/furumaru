23355fb2c0c016a22ceb0cbd7684f6871742e2ab
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-08-20 22:07:29 +0900

    feat: 下向き矢印と上向き矢印のコンポーネントを追加

---
+++ web/user/src/components/atoms/icons/TheDownArrowIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="8"
+    viewBox="0 0 12 8"
+    fill="none"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M0 1.65625L0.969746 0.686503L5.65686 5.37361L10.344 0.686502L11.3137 1.65625L5.65686 7.3131L0 1.65625Z"
+      fill="#604C3F"
+    />
+  </svg>
+</template>
