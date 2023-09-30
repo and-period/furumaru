2d3e8929d95cf9ea7453ad2662244135b6bbef40
濵田広大 <hamany0326@gmail.com>
2023-09-30 23:51:18 +0900

    Add: added thanks message

---
+++ web/user/src/pages/v1/purchase/complete.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const localePath = useLocalePath()
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center gap-x-[40px]">
+    <div class="text-xl font-bold tracking-[2px] text-main">
+      <p> ご注文ありがとうございます！ </p>
+      <p> ご購入手続きが完了しました。</p>
+    </div>
+    <div>
+      <img src="/img/purchase/complete.svg">
+    </div>
+  </div>
+</template>
