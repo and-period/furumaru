32b688b9e3bb696b9b626098ca3fe6c72e1d9897
taba2424 <tadashi-112@hotmail.co.jp>
2023-10-26 10:21:52 +0900

    feat(api): 商品関連APIの定義 (#1432)
    * feat(api): 商品関連APIの定義
    
    * fix(docs): fix docs

---
+++ api/internal/gateway/user/v1/response/producer.go
@@ -0,0 +1,16 @@
+package response
+
+// Producer - 生産者情報
+type Producer struct {
+	ID                string   `json:"id"`                // 生産者ID
+	CoordinatorID     string   `json:"coordinatorId"`     // 担当コーディネータID
+	Username          string   `json:"username"`          // 生産者名
+	Profile           string   `json:"profile"`           // 紹介文
+	ThumbnailURL      string   `json:"thumbnailUrl"`      // サムネイルURL
+	Thumbnails        []*Image `json:"thumbnails"`        // サムネイルURL(リサイズ済み)一覧
+	HeaderURL         string   `json:"headerUrl"`         // ヘッダー画像URL
+	Headers           []*Image `json:"headers"`           // ヘッダー画像URL(リサイズ済み)一覧
+	PromotionVideoURL string   `json:"promotionVideoUrl"` // 紹介映像URL
+	InstagramID       string   `json:"instagramId"`       // Instagramアカウント
+	FacebookID        string   `json:"facebookId"`        // Facebookアカウント
+}
--- web/user/src/types/api/apis/index.ts
+++ web/user/src/types/api/apis/index.ts
@@ -2,4 +2,5 @@
 /* eslint-disable */
 export * from './AuthApi';
 export * from './ContactApi';
+export * from './ProductApi';
 export * from './TopApi';
