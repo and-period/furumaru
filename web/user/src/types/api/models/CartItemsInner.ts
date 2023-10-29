18758b20dcd5a354c302fc8fa9a6549a46a67de1
taba2424 <tadashi-112@hotmail.co.jp>
2023-10-30 03:05:18 +0900

    fix(docs): APIドキュメントの修正

--- web/user/src/types/api/models/CartItemsInner.ts
+++ web/user/src/types/api/models/CartItemsInner.ts
@@ -20,7 +20,7 @@ import { exists, mapValues } from '../runtime';
  */
 export interface CartItemsInner {
     /**
-     * 
+     * 商品ID
      * @type {string}
      * @memberof CartItemsInner
      */
