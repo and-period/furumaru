a1e945f79a096414aea2173d3eb1096a9ffc0bdf
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2023-11-02 00:20:27 +0900

    feat: 商品一覧の情報を整形して利用できるgettersを用意

--- web/user/src/store/product.ts
+++ web/user/src/store/product.ts
@@ -14,7 +14,7 @@ export const useProductStore = defineStore('product', {
     return {
       isLoading: false,
       total: 0,
-      products: [] as Product[],
+      _products: [] as Product[],
       producers: [] as Producer[],
       coordinators: [] as Coordinator[],
       categories: [] as Category[],
@@ -28,6 +28,8 @@ export const useProductStore = defineStore('product', {
       this.isLoading = true
       const {
         total,
+        coordinators,
+        categories,
         products,
         producers,
         productTags,
@@ -37,11 +39,47 @@ export const useProductStore = defineStore('product', {
         offset,
       })
       this.total = total
-      this.products = products
+      this._products = products
       this.producers = producers
       this.productTags = productTags
       this.productTypes = productTypes
+      this.coordinators = coordinators
+      this.categories = categories
       this.isLoading = false
+    },
+  },
+
+  getters: {
+    products(state) {
+      return state._products.map((product) => {
+        return {
+          ...product,
+          // 在庫があるかのフラグ
+          hasStock: product.inventory > 0,
+          // サムネイル画像のマッピング
+          thumbnail: product.media.find((m) => m.isThumbnail),
+          // 生産者情報をマッピング
+          producer: state.producers.find(
+            (producer) => producer.id === product.producerId,
+          ),
+          // 商品タイプをマッピング
+          productType: state.productTypes.find(
+            (productType) => productType.id === product.productTypeId,
+          ),
+          // コーディネーター情報をマッピング
+          coordinator: state.coordinators.find(
+            (coordinator) => coordinator.id === product.coordinatorId,
+          ),
+          // カテゴリ情報をマッピング
+          category: state.categories.find(
+            (category) => category.id === product.categoryId,
+          ),
+          // 商品タグをマッピング
+          productTags: product.productTagIds.map((id) =>
+            state.productTags.find((productTag) => productTag.id === id),
+          ),
+        }
+      })
     },
   },
 })
