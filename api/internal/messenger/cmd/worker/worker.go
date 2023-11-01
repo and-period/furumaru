b3c99423c6fbb0869d5eacf5a1d3d0ae72c27303
taba2424 <tadashi-112@hotmail.co.jp>
2023-11-02 02:08:37 +0900

    fix(messenger): Firebaseの認証情報を複数つめられるように (#1479)

--- api/internal/messenger/cmd/worker/worker.go
+++ api/internal/messenger/cmd/worker/worker.go
@@ -16,33 +16,35 @@ import (
 
 type app struct {
 	*cobra.Command
-	waitGroup             *sync.WaitGroup
-	worker                worker.Worker
-	AppName               string `envconfig:"APP_NAME" default:"messenger-worker"`
-	Environment           string `envconfig:"ENV" default:"none"`
-	RunMethod             string `envconfig:"RUN_METHOD" default:"lambda"`
-	LogPath               string `envconfig:"LOG_PATH" default:""`
-	LogLevel              string `envconfig:"LOG_LEVEL" default:"info"`
-	DBSocket              string `envconfig:"DB_SOCKET" default:"tcp"`
-	DBHost                string `envconfig:"DB_HOST" default:"127.0.0.1"`
-	DBPort                string `envconfig:"DB_PORT" default:"3306"`
-	DBUsername            string `envconfig:"DB_USERNAME" default:"root"`
-	DBPassword            string `envconfig:"DB_PASSWORD" default:""`
-	DBTimeZone            string `envconfig:"DB_TIMEZONE" default:"Asia/Tokyo"`
-	DBEnabledTLS          bool   `envconfig:"DB_ENABLED_TLS" default:"false"`
-	DBSecretName          string `envconfig:"DB_SECRET_NAME" default:""`
-	AWSRegion             string `envconfig:"AWS_REGION" default:"ap-northeast-1"`
-	SendGridAPIKey        string `envconfig:"SENDGRID_API_KEY" default:""`
-	SendGridTemplatePath  string `envconfig:"SENDGRID_TEMPLATE_PATH" default:""`
-	SendGridSecretName    string `envconfig:"SENDGRID_SECRET_NAME" default:""`
-	MailFromName          string `envconfig:"MAIL_FROM_NAME" default:""`
-	MailFromAddress       string `envconfig:"MAIL_FROM_ADDRESS" default:""`
-	LINEChannelToken      string `envconfig:"LINE_CHANNEL_TOKEN" default:""`
-	LINEChannelSecret     string `envconfig:"LINE_CHANNEL_SECRET" default:""`
-	LINERoomID            string `envconfig:"LINE_ROOM_ID" default:""`
-	LINESecretName        string `envconfig:"LINE_SECRET_NAME" default:""`
-	GoogleCredentialsJSON string `envconfig:"GOOGLE_CREDENTIALS_JSON" default:""`
-	GoogleSecretName      string `envconfig:"GOOGLE_SECRET_NAME" default:""`
+	waitGroup                    *sync.WaitGroup
+	worker                       worker.Worker
+	AppName                      string `envconfig:"APP_NAME" default:"messenger-worker"`
+	Environment                  string `envconfig:"ENV" default:"none"`
+	RunMethod                    string `envconfig:"RUN_METHOD" default:"lambda"`
+	LogPath                      string `envconfig:"LOG_PATH" default:""`
+	LogLevel                     string `envconfig:"LOG_LEVEL" default:"info"`
+	DBSocket                     string `envconfig:"DB_SOCKET" default:"tcp"`
+	DBHost                       string `envconfig:"DB_HOST" default:"127.0.0.1"`
+	DBPort                       string `envconfig:"DB_PORT" default:"3306"`
+	DBUsername                   string `envconfig:"DB_USERNAME" default:"root"`
+	DBPassword                   string `envconfig:"DB_PASSWORD" default:""`
+	DBTimeZone                   string `envconfig:"DB_TIMEZONE" default:"Asia/Tokyo"`
+	DBEnabledTLS                 bool   `envconfig:"DB_ENABLED_TLS" default:"false"`
+	DBSecretName                 string `envconfig:"DB_SECRET_NAME" default:""`
+	AWSRegion                    string `envconfig:"AWS_REGION" default:"ap-northeast-1"`
+	SendGridAPIKey               string `envconfig:"SENDGRID_API_KEY" default:""`
+	SendGridTemplatePath         string `envconfig:"SENDGRID_TEMPLATE_PATH" default:""`
+	SendGridSecretName           string `envconfig:"SENDGRID_SECRET_NAME" default:""`
+	MailFromName                 string `envconfig:"MAIL_FROM_NAME" default:""`
+	MailFromAddress              string `envconfig:"MAIL_FROM_ADDRESS" default:""`
+	LINEChannelToken             string `envconfig:"LINE_CHANNEL_TOKEN" default:""`
+	LINEChannelSecret            string `envconfig:"LINE_CHANNEL_SECRET" default:""`
+	LINERoomID                   string `envconfig:"LINE_ROOM_ID" default:""`
+	LINESecretName               string `envconfig:"LINE_SECRET_NAME" default:""`
+	AdminFirebaseCredentialsJSON string `envconfig:"ADMIN_FIREBASE_CREDENTIALS_JSON" default:""`
+	AdminFirebaseSecretName      string `envconfig:"ADMIN_FIREBASE_SECRET_NAME" default:""`
+	UserFirebaseCredentialsJSON  string `envconfig:"USER_FIREBASE_CREDENTIALS_JSON" default:""`
+	UserFirebaseSecretName       string `envconfig:"USER_FIREBASE_SECRET_NAME" default:""`
 }
 
 //nolint:revive
