d11c3ed524951cec770c9b8afe67248778194a36
taba2424 <tadashi-112@hotmail.co.jp>
2023-11-02 00:49:15 +0900

    feat(api): アドレス帳周りのエンティティロジックを実装

---
+++ api/internal/gateway/user/v1/service/address.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"github.com/and-period/furumaru/api/internal/codes"
+	"github.com/and-period/furumaru/api/internal/gateway/user/v1/response"
+	"github.com/and-period/furumaru/api/internal/store/entity"
+)
+
+type Address struct {
+	response.Address
+}
+
+type Addresses []*Address
+
+func NewAddress(address *entity.Address) *Address {
+	return &Address{
+		Address: response.Address{
+			ID:           address.ID,
+			IsDefault:    address.IsDefault,
+			Lastname:     address.Lastname,
+			Firstname:    address.Firstname,
+			PostalCode:   address.PostalCode,
+			Prefecture:   codes.PrefectureNames[address.Prefecture],
+			City:         address.City,
+			AddressLine1: address.AddressLine1,
+			AddressLine2: address.AddressLine2,
+			PhoneNumber:  address.PhoneNumber,
+		},
+	}
+}
+
+func (a *Address) Response() *response.Address {
+	return &a.Address
+}
+
+func NewAddresses(addresses entity.Addresses) Addresses {
+	res := make(Addresses, len(addresses))
+	for i := range addresses {
+		res[i] = NewAddress(addresses[i])
+	}
+	return res
+}
+
+func (as Addresses) Response() []*response.Address {
+	res := make([]*response.Address, len(as))
+	for i := range as {
+		res[i] = as[i].Response()
+	}
+	return res
+}
