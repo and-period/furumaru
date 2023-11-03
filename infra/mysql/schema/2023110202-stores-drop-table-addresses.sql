975ae3b65fc3faab2473f2055a6509866bf95205
taba2424 <tadashi-112@hotmail.co.jp>
2023-11-03 15:33:09 +0900

    fix(api): fix small

--- infra/mysql/schema/2023110202-stores-drop-table-addresses.sql
+++ infra/mysql/schema/2023110202-stores-drop-table-addresses.sql
@@ -1,1 +1,4 @@
-DROP TABLE IF EXISTS `stores`.`addresses` CASCADE CONSTRAINTS;
+ALTER TABLE `stores`.`payments` DROP FOREIGN KEY `fk_payments_address_id`;
+ALTER TABLE `stores`.`fulfillments` DROP FOREIGN KEY `fk_fulfillments_address_id`;
+
+DROP TABLE IF EXISTS `stores`.`addresses`;
