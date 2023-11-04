6b2f2b4410acf4a1ec7b46d4c95fbd9a7eccb695
taba2424 <tadashi-112@hotmail.co.jp>
2023-11-04 17:24:15 +0900

    refactor(*): 未使用ファイルの削除 (#1493)

--- infra/terraform/env/shared/01-ecr/99-variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "access_key" {}
-variable "secret_key" {}
-
-variable "db_master_username" {}
-variable "db_master_password" {}
-variable "vpc_name" {}
-variable "tags" {}
--- infra/terraform/modules/route53/99-locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  subdomains = {
-    for s in var.subdomains : s.domain => s
-  }
-}
