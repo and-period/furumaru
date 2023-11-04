0e306e5b0adbfbd9373cf9a0cc5e0eaae3e13621
taba2424 <tadashi-112@hotmail.co.jp>
2023-11-04 17:21:15 +0900

    refactor(*): 未使用ファイルの削除

--- infra/terraform/env/stg/06-dynamodb/00-locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  table_prefix = "stg"
-}
--- infra/terraform/modules/network-acl/99-outputs.tf
+++
@@ -1,7 +0,0 @@
-##################################################
-# Network ACL
-##################################################
-output "network_acls" {
-  description = "Network ACL"
-  value       = aws_network_acl.this
-}
