d41ee050f441ce11cb8411fd4bb1f4495ba49f6e
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2023-11-04 19:59:38 +0900

    トップページ用のAPIを呼び出すクライアントの追加 (#1490)
    * feat: トップページ用のAPIを呼び出すクライアントの追加
    
    * fix: 型定義を更新

---
+++ web/user/src/store/home.ts
@@ -0,0 +1,20 @@
+import { TopArchive, TopCommonResponse, TopLive } from '~/types/api'
+
+export const useTopPageStore = defineStore('top-page', {
+  state: () => {
+    return {
+      _lives: [] as TopLive[],
+      _archives: [] as TopArchive[],
+    }
+  },
+
+  actions: {
+    async getHomeContent() {
+      const response: TopCommonResponse =
+        await this.topPageApiClient().v1TopCommon()
+
+      this._lives = response.lives
+      this._archives = response.archives
+    },
+  },
+})
