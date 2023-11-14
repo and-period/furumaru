9c0f49ba97e981648437df290a48eba89080eb25
taba2424 <tadashi-112@hotmail.co.jp>
2023-11-14 23:22:16 +0900

    fix(mysql): fix small

--- infra/mysql/schema/2023111402-stores-create-payment-systems.sql
+++ infra/mysql/schema/2023111402-stores-create-payment-systems.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS `stores`.`payment_systems` (
   `status`      INT         NOT NULL, -- 決済システム状態
   `created_at`  DATETIME(3) NOT NULL, -- 登録日時
   `updated_at`  DATETIME(3) NOT NULL, -- 更新日時
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`method_type`)
 );
