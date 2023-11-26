d141a9fc682e1c29a580bffa2632c2f0766ba3d9
wf-yamaday <wf.yamaday.job@gmail.com>
2023-11-26 22:43:32 +0900

    feat: 住所一覧を取得する関数の実装

--- web/user/src/store/address.ts
+++ web/user/src/store/address.ts
@@ -1,13 +1,31 @@
-import { CreateAddressRequest } from '~/types/api'
+import { Address, AddressesResponse, CreateAddressRequest } from '~/types/api'
 
 export const useAdressStore = defineStore('address', {
   state: () => {
-    return {}
+    return {
+      total: 0,
+      addresses: [] as Address[],
+      addressesFetchState: {
+        isLoading: false,
+      },
+    }
   },
 
   actions: {
     async registerAddress(payload: CreateAddressRequest) {
       await this.addressApiClient().v1CreateAddress({ body: payload })
+    },
+
+    async fetchAddresses(limit: number = 20, offset: number = 0) {
+      this.addressesFetchState.isLoading = true
+      const res: AddressesResponse =
+        await this.addressApiClient().v1ListAddresses({
+          limit,
+          offset,
+        })
+      this.addresses = res.addresses
+      this.total = res.total
+      this.addressesFetchState.isLoading = false
     },
   },
 })
