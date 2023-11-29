99ec1136e4a09441742865fce4324f7251c25f2b
濵田広大 <hamany0326@gmail.com>
2023-11-30 01:41:27 +0900

    Add: added purchase complete moblie page

--- web/user/src/pages/v1/purchase/complete.vue
+++ web/user/src/pages/v1/purchase/complete.vue
@@ -7,23 +7,32 @@ const handleBackTopPageButton = () => {
 </script>
 <template>
   <div class="text-main">
-    <div class="flex items-center justify-center gap-x-[40px]">
-      <div class="text-xl font-bold tracking-[2px]">
-        <p>ご注文ありがとうございます！</p>
-        <p>ご購入手続きが完了しました。</p>
-      </div>
-      <div>
+    <div class="hidden md:block">
+      <div class="flex items-center justify-center gap-x-[40px]">
+        <div class="text-xl font-bold tracking-[2px]">
+          <p>ご注文ありがとうございます！</p>
+          <p>ご購入手続きが完了しました。</p>
+        </div>
         <img src="/img/purchase/complete.svg" />
       </div>
     </div>
-    <div class="mt-20 flex flex-col items-center justify-center">
-      <p class="text-xl font-medium tracking-[2px]">
+    <div class="mt-[40px] block md:hidden">
+      <div class="flex justify-center">
+        <img src="/img/purchase/complete.svg" />
+      </div>
+      <div class="text-[16px] font-bold tracking-[1.6px]">
+        <p class="mt-[40px] flex justify-center">ご注文ありがとうございます！</p>
+        <p class="mt-2 flex justify-center">ご購入手続きが完了しました。</p>
+      </div>
+    </div>
+    <div class="mt-[40px] flex flex-col items-center justify-center md:mt-20">
+      <p class="text-[14px] font-medium tracking-[2px] md:text-xl">
         お客様の注文番号は「1111111」です
       </p>
       <div
-        class="flex flex-col items-center justify-center text-[16px] font-medium"
+        class="flex flex-col items-center justify-center px-[15px] text-[14px] font-medium md:px-0 md:text-[16px]"
       >
-        <p class="mt-10">
+        <p class="mt-[40px] md:mt-10">
           ご登録いただいたメールアドレスに確認メールをお送りしましたのでご確認ください。
         </p>
         <p>
@@ -33,7 +42,7 @@ const handleBackTopPageButton = () => {
           注文の詳細は、アカウントページの注文履歴からもご確認いただけます。
         </p>
       </div>
-      <div class="mb-4 mt-20 flex w-full justify-center">
+      <div class="mb-4 mt-[40px] flex w-full justify-center md:mt-20 px-16 md:px-0">
         <button
           class="w-[400px] bg-main py-2 tracking-[1.6px] text-white"
           @click="handleBackTopPageButton"
