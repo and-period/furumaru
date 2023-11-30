daced1fec0bd4c47adb352d688ae7a53257787e9
wf-yamaday <wf.yamaday.job@gmail.com>
2023-11-30 22:10:38 +0900

    fix: アイコンのサイズを調整

--- web/user/src/components/molecules/TheAlert.vue
+++ web/user/src/components/molecules/TheAlert.vue
@@ -1,6 +1,6 @@
 <template>
   <the-alert-box>
-    <the-info-icon class="mr-1 inline h-5 w-5" />
+    <the-info-icon class="mr-1 inline h-6 w-6" />
     <p class="inline h-6 align-middle">
       <slot />
     </p>
