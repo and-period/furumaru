68d1c260ce8fa94625a3ab98cf8ee03c1479af30
wf-yamaday <wf.yamaday.job@gmail.com>
2023-11-30 22:30:30 +0900

    fix: クレジットカードアイコンの位置を調整

--- web/user/src/pages/v1/purchase/confirmation.vue
+++ web/user/src/pages/v1/purchase/confirmation.vue
@@ -107,28 +107,74 @@ const handleClickNextStepButton = () => {
               お支払い情報
             </div>
             <div class="pt-4">
-              <input
-                class="check:before:border-main relative float-left mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-main checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:bg-main checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-main checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                type="radio"
-                checked
-              />
-              <label class="pl-2 text-[14px] text-main">
-                クレジットカード支払い
-              </label>
-              <div class="flex w-full items-center gap-4">
+              <div class="flex items-center justify-between">
+                <div class="flex">
+                  <input
+                    class="check:before:border-main relative float-left mr-1 mt-0.5 h-5 w-5 appearance-none rounded-full border-2 border-solid border-neutral-300 before:pointer-events-none before:absolute before:h-4 before:w-4 before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] after:absolute after:z-[1] after:block after:h-4 after:w-4 after:rounded-full after:content-[''] checked:border-main checked:before:opacity-[0.16] checked:after:absolute checked:after:left-1/2 checked:after:top-1/2 checked:after:h-[0.625rem] checked:after:w-[0.625rem] checked:after:rounded-full checked:after:bg-main checked:after:content-[''] checked:after:[transform:translate(-50%,-50%)] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:border-main checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:border-neutral-600 dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                    type="radio"
+                    checked
+                  />
+                  <label class="pl-2 text-[14px] text-main">
+                    クレジットカード支払い
+                  </label>
+                </div>
+                <div
+                  class="flex h-[18px] min-w-max items-center gap-2 md:hidden"
+                >
+                  <img
+                    src="~/assets/img/cc/visa.png"
+                    alt="visa icon"
+                    class="h-full"
+                  />
+                  <img
+                    src="~/assets/img/cc/jcb.png"
+                    alt="jcb icon"
+                    class="h-full"
+                  />
+                  <img
+                    src="~/assets/img/cc/amex.png"
+                    alt="amex icon"
+                    class="h-full"
+                  />
+                  <img
+                    src="~/assets/img/cc/master.png"
+                    alt="master icon"
+                    class="h-full"
+                  />
+                </div>
+              </div>
+              <div class="mt-4 flex w-full items-center gap-4">
                 <the-text-input
                   placeholder="カード番号"
                   :with-label="false"
                   name="cc-number"
                   type="text"
-                  class="mt-4 w-full"
+                  class="w-full"
                   required
                 />
-                <div class="flex h-[24px] min-w-max gap-2">
-                  <img src="~/assets/img/cc/visa.png" alt="visa icon" />
-                  <img src="~/assets/img/cc/jcb.png" alt="jcb icon" />
-                  <img src="~/assets/img/cc/amex.png" alt="amex icon" />
-                  <img src="~/assets/img/cc/master.png" alt="master icon" />
+                <div
+                  class="hidden h-[24px] min-w-max items-center gap-2 md:flex"
+                >
+                  <img
+                    src="~/assets/img/cc/visa.png"
+                    alt="visa icon"
+                    class="h-full"
+                  />
+                  <img
+                    src="~/assets/img/cc/jcb.png"
+                    alt="jcb icon"
+                    class="h-full"
+                  />
+                  <img
+                    src="~/assets/img/cc/amex.png"
+                    alt="amex icon"
+                    class="h-full"
+                  />
+                  <img
+                    src="~/assets/img/cc/master.png"
+                    alt="master icon"
+                    class="h-full"
+                  />
                 </div>
               </div>
               <the-text-input
