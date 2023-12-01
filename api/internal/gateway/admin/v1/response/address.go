75c989f7b5258611b48381495bd2baca4ebc94b4
taba2424 <tadashi-112@hotmail.co.jp>
2023-12-01 21:35:04 +0900

    fix(api): コメント文の修正

--- api/internal/gateway/admin/v1/response/address.go
+++ api/internal/gateway/admin/v1/response/address.go
@@ -8,7 +8,7 @@ type Address struct {
 	LastnameKana   string `json:"lastnameKana"`   // 氏名(姓:かな)
 	FirstnameKana  string `json:"firstnameKana"`  // 氏名(名:かな)
 	PostalCode     string `json:"postalCode"`     // 郵便番号
-	PrefectureCode int32  `json:"prefectureCode"` // 都道府県
+	PrefectureCode int32  `json:"prefectureCode"` // 都道府県コード
 	City           string `json:"city"`           // 市区町村
 	AddressLine1   string `json:"addressLine1"`   // 町名・番地
 	AddressLine2   string `json:"addressLine2"`   // ビル名・号室など
