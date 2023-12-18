1d97fc1f17df184d42edd30397d71c8d69b79657
wf-yamaday <wf.yamaday.job@gmail.com>
2023-12-19 00:21:40 +0900

    fix: リフレッシュトークン取得の部分でPromise.rejectをするように修正

--- web/user/src/store/auth.ts
+++ web/user/src/store/auth.ts
@@ -8,6 +8,7 @@ import type {
   SignInRequest,
   VerifyAuthRequest,
 } from '~/types/api'
+import { AuthError } from '~/types/exception'
 
 /**
  * 認証情報を管理するグローバルステート
@@ -90,7 +91,9 @@ export const useAuthStore = defineStore('auth', {
       if (!refreshToken) {
         console.debug('リフレッシュトークンが存在しません')
         this.$reset()
-        return
+        return Promise.reject(
+          new AuthError('リフレッシュトークンが存在しません'),
+        )
       }
       try {
         const res = await this.authApiClient().v1RefreshAuthToken({
@@ -100,7 +103,7 @@ export const useAuthStore = defineStore('auth', {
         this.refreshToken = res.refreshToken
         this.setExpiredAt(res.expiresIn)
       } catch (error) {
-        this.errorHandler(error)
+        return this.errorHandler(error)
       }
     },
 
