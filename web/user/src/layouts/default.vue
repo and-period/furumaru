c73155956276437afec7e1c7e81741617f8237f9
wf-yamaday <wf.yamaday.job@gmail.com>
2023-12-21 01:51:57 +0900

    fix: スナックバーの位置調整

--- web/user/src/layouts/default.vue
+++ web/user/src/layouts/default.vue
@@ -159,7 +159,7 @@ const handleClickRemoveItemFromCartButton = (
     />
     <div
       id="snackbar"
-      class="absolute top-[120px] z-[70] flex min-h-min w-full flex-col gap-2 bg-transparent"
+      class="absolute top-[28px] z-[70] flex min-h-min w-full flex-col gap-2 bg-transparent md:top-[120px]"
     ></div>
   </div>
   <div class="flex min-h-screen flex-col bg-base">
