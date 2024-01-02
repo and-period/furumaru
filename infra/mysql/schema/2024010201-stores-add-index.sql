7cb910fb8cde9b3b3ce822c598b903638c6d24e4
taba2424 <tadashi-112@hotmail.co.jp>
2024-01-02 23:40:30 +0900

    feat(database): 全文検索用のインデックス追加

---
+++ infra/mysql/schema/2024010201-stores-add-index.sql
@@ -0,0 +1,5 @@
+CREATE FULLTEXT INDEX `ftx_categories` ON `stores`.`categories` (`name`) WITH PARSER ngram;
+CREATE FULLTEXT INDEX `ftx_products` ON `stores`.`products` (`name`, `description`) WITH PARSER ngram;
+CREATE FULLTEXT INDEX `ftx_product_tags` ON `stores`.`product_tags` (`name`) WITH PARSER ngram;
+CREATE FULLTEXT INDEX `ftx_product_types` ON `stores`.`product_types` (`name`) WITH PARSER ngram;
+CREATE FULLTEXT INDEX `ftx_promotions` ON `stores`.`promotions` (`name`) WITH PARSER ngram;
