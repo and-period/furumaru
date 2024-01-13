86d845d43f92d6cef719f1ea426daf9c5b5d0a95
wf-yamaday <wf.yamaday.job@gmail.com>
2024-01-13 21:49:17 +0900

    feat: エラーを表示するようした

--- web/user/src/pages/account/orders/[...id].vue
+++ web/user/src/pages/account/orders/[...id].vue
@@ -14,13 +14,19 @@ const orderStore = useOrderStore()
 const { orderHistory } = storeToRefs(orderStore)
 const { fetchOrderHistory } = orderStore
 
-useAsyncData(`orders-${orderId.value}`, () => {
+const { error } = useAsyncData(`orders-${orderId.value}`, () => {
   return fetchOrderHistory(orderId.value)
 })
 </script>
 
 <template>
-  <div class="container">
+  <div class="container mx-auto p-4 xl:p-0">
+    <template v-if="error">
+      <div class="border border-orange bg-white p-4 text-main">
+        指定した注文を取得できませんでした。
+        {{ error.message }}
+      </div>
+    </template>
     <template v-if="!orderHistory"> </template>
     <template v-else>
       {{ orderHistory }}
