c2472f2f67df9d0f35959a8c57fff847ae568769
濵田広大 <hamany0326@gmail.com>
2024-01-17 13:50:52 +0900

    Fix: fixed typo

--- web/user/src/components/molecules/TheCoordinatorLiveItem.vue
+++ web/user/src/components/molecules/TheCoordinatorLiveItem.vue
@@ -16,8 +16,8 @@ const formattedStartAt = computed(() => {
   return dayjs.unix(props.startAt).format('YYYY/MM/DD HH:mm')
 })
 
-const isLiveStreaming = (stasus: ScheduleStatus) => {
-  if (stasus === ScheduleStatus.LIVE) {
+const isLiveStreaming = (status: ScheduleStatus) => {
+  if (status === ScheduleStatus.LIVE) {
     return true
   } else {
     return false
