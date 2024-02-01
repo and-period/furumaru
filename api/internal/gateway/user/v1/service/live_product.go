42f9bd1dd739a5de2a8afc4006ac7a35bc9b14d3
taba2424 <tadashi-112@hotmail.co.jp>
2024-02-02 00:22:01 +0900

    fix(gateway): ライブ配信ページの商品の並び順を修正 (#2008)

--- api/internal/gateway/user/v1/service/live_product.go
+++ api/internal/gateway/user/v1/service/live_product.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"sort"
+
 	"github.com/and-period/furumaru/api/internal/gateway/user/v1/response"
 	"github.com/and-period/furumaru/api/internal/store/entity"
 )
 
 type LiveProduct struct {
 	response.LiveProduct
+	isSale bool
 }
 
 type LiveProducts []*LiveProduct
@@ -15,6 +18,7 @@ func NewLiveProduct(product *entity.Product) *LiveProduct {
 	var (
 		thumbnailURL string
 		thumbnails   Images
+		isSale       bool
 	)
 	for _, media := range product.Media {
 		if !media.IsThumbnail {
@@ -23,6 +27,9 @@ func NewLiveProduct(product *entity.Product) *LiveProduct {
 		thumbnailURL = media.URL
 		thumbnails = NewImages(media.Images)
 		break
+	}
+	if product.Status == entity.ProductStatusForSale && product.Inventory > 0 {
+		isSale = true
 	}
 	return &LiveProduct{
 		LiveProduct: response.LiveProduct{
@@ -33,6 +40,7 @@ func NewLiveProduct(product *entity.Product) *LiveProduct {
 			ThumbnailURL: thumbnailURL,
 			Thumbnails:   thumbnails.Response(),
 		},
+		isSale: isSale,
 	}
 }
 
@@ -45,7 +53,17 @@ func NewLiveProducts(products entity.Products) LiveProducts {
 	for i := range products {
 		res[i] = NewLiveProduct(products[i])
 	}
-	return res
+	return res.SortByIsSale()
+}
+
+func (ps LiveProducts) SortByIsSale() LiveProducts {
+	sort.SliceStable(ps, func(i, j int) bool {
+		if !ps[j].isSale {
+			return ps[i].isSale
+		}
+		return ps[i].ProductID <= ps[j].ProductID
+	})
+	return ps
 }
 
 func (ps LiveProducts) Response() []*response.LiveProduct {
