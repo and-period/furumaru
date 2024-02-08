b97d14dd34d3382ff58717ffcbaffd2cb2ae94e1
koudai hamada <hamany0326@gmail.com>
2024-02-09 02:34:47 +0900

    Add: added line contact (#2025)

--- web/user/src/pages/contact.vue
+++ web/user/src/pages/contact.vue
@@ -11,14 +11,10 @@ useSeoMeta({
     </div>
     <div>
       <p class="mt-8">
-        ふるマルのアプリに関してや、販売商品に関してのお問い合わせは以下のメールアドレスにお問い合わせください。
+        ふるマルのアプリに関してや、販売商品に関してはライン友達追加後お問い合わせください。
       </p>
       <div class="mt-8 flex justify-center underline">
-        <a
-          href="mailto:support@and-period.co.jp?subject=%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B&body=%E4%B8%8B%E8%A8%98%E9%A0%85%E7%9B%AE%E3%82%92%E3%81%9D%E3%82%8C%E3%81%9E%E3%82%8C%E8%A8%98%E5%85%A5%E3%81%97%E3%81%A6%E9%80%81%E4%BF%A1%E3%81%97%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84%E3%80%82%0D%0A%0D%0A%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%0D%0A%E6%B0%8F%E5%90%8D%EF%BC%9A%E3%80%80%0D%0A%0D%0A%E3%83%A1%E3%83%BC%E3%83%AB%E3%82%A2%E3%83%89%E3%83%AC%E3%82%B9%EF%BC%9A%0D%0A%0D%0A%E9%9B%BB%E8%A9%B1%E7%95%AA%E5%8F%B7%EF%BC%9A%0D%0A%0D%0A%E5%86%85%E5%AE%B9%EF%BC%9A%0D%0A%0D%0A%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D%3D"
-        >
-          support@and-period.co.jp
-        </a>
+        <a href="https://lin.ee/49SOeUC"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36"></a>
       </div>
       <p class="mt-8">※2〜3営業日以内に、ご返信させていただきます。</p>
     </div>
