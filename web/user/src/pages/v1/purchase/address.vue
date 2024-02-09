3c2977d8fad82f134372763317f9d230dcf08a99
wf-yamaday <wf.yamaday.job@gmail.com>
2024-02-09 23:40:20 +0900

    feat: プロモーションコードの適用ができるように実装を追加

--- web/user/src/pages/v1/purchase/address.vue
+++ web/user/src/pages/v1/purchase/address.vue
@@ -16,7 +16,7 @@ const { fetchAddresses, searchAddressByPostalCode, registerAddress } =
 
 const shoppingCartStore = useShoppingCartStore()
 const { calcCartResponseItem } = storeToRefs(shoppingCartStore)
-const { calcCartItemByCoordinatorId } = shoppingCartStore
+const { calcCartItemByCoordinatorId, verifyPromotionCode } = shoppingCartStore
 
 const targetAddress = ref<string>('default')
 const calcCartResponseItemState = ref<{
@@ -39,6 +39,10 @@ const formData = ref<CreateAddressRequest>({
   isDefault: true,
 })
 
+const promotionCodeFormValue = ref<string>('')
+const invalidPromotion = ref<boolean>(false)
+const validPromotion = ref<boolean>(false)
+
 const coordinatorId = computed<string>(() => {
   const id = route.query.coordinatorId
   if (id) {
@@ -58,6 +62,15 @@ const cartNumber = computed<number | undefined>(() => {
     return undefined
   }
   return idNumber
+})
+
+const promotionCode = computed<string | undefined>(() => {
+  const code = route.query.promotionCode
+  if (typeof code === 'string') {
+    return code
+  } else {
+    return undefined
+  }
 })
 
 const priceFormatter = (price: number) => {
@@ -89,6 +102,7 @@ const handleSubmitNewAddressForm = async () => {
       id: registerdAddress.id,
       coordinatorId: coordinatorId.value,
       cartNumber: cartNumber.value,
+      promotionCode: promotionCode.value,
     },
   })
 }
@@ -100,9 +114,77 @@ const handleClickNextStepButton = (id: string) => {
       id,
       coordinatorId: coordinatorId.value,
       cartNumber: cartNumber.value,
+      promotionCode: promotionCode.value,
     },
   })
 }
+
+/**
+ * クーポンコードを適用するボタンをクリックしたときの処理
+ */
+const handleClickUsePromotionCodeButton = async () => {
+  const result = await verifyPromotionCode(promotionCodeFormValue.value)
+  if (result) {
+    invalidPromotion.value = false
+    validPromotion.value = true
+  } else {
+    invalidPromotion.value = true
+    validPromotion.value = false
+  }
+}
+
+/**
+ * 適用されているクーポンコードを取り消すボタンをクリックしたときの処理
+ */
+const handleClickCancelPromotionCodeButton = async () => {
+  promotionCodeFormValue.value = ''
+  invalidPromotion.value = false
+  validPromotion.value = false
+  await calcCartItemByCoordinatorId(
+    coordinatorId.value,
+    cartNumber.value,
+    undefined, // 都道府県
+    undefined,
+  )
+}
+
+watch(validPromotion, (newValue, oldValue) => {
+  if (newValue && promotionCodeFormValue.value) {
+    // フォーム入力でvalidPromotionがtrueになったときに、クーポンコードを適用して再計算する
+    calcCartItemByCoordinatorId(
+      coordinatorId.value,
+      cartNumber.value,
+      undefined, // 都道府県
+      promotionCodeFormValue.value,
+    )
+    // リロード対策で、クエリパラメータにクーポンコードを追加する
+    router.push({
+      path: '/v1/purchase/address',
+      query: {
+        coordinatorId: coordinatorId.value,
+        cartNumber: cartNumber.value,
+        promotionCode: promotionCodeFormValue.value,
+      },
+    })
+  }
+  if (oldValue && !newValue) {
+    calcCartItemByCoordinatorId(
+      coordinatorId.value,
+      cartNumber.value,
+      undefined, // 都道府県
+      undefined,
+    )
+    // クーポンコードが適用されている状態から、適用されていない状態に変わったときに、クエリパラメータからクーポンコードを削除する
+    router.push({
+      path: '/v1/purchase/address',
+      query: {
+        coordinatorId: coordinatorId.value,
+        cartNumber: cartNumber.value,
+        promotionCode: undefined,
+      },
+    })
+  }
+})
 
 onMounted(() => {
   fetchAddresses()
@@ -114,8 +196,13 @@ onMounted(async () => {
     await calcCartItemByCoordinatorId(
       coordinatorId.value,
       cartNumber.value,
-      undefined,
+      undefined, // 都道府県
+      promotionCode.value,
     )
+    // クーポンコードが指定されている場合は、クーポンコードを適用する
+    if (promotionCode.value) {
+      validPromotion.value = true
+    }
   } catch (error) {
     calcCartResponseItemState.value.hasError = true
     if (error instanceof ApiBaseError) {
@@ -271,23 +358,73 @@ useSeoMeta({
                 </div>
               </div>
 
-              <div class="my-4 flex gap-2">
-                <div class="grow">
-                  <input
-                    type="text"
-                    class="w-full border border-gray-300 bg-gray-50 p-2.5 text-[14px] md:text-[16px]"
-                    placeholder="クーポンコード"
-                  />
-                </div>
-                <button
-                  class="whitespace-nowrap bg-main p-2 text-[14px] text-white md:text-[16px]"
+              <template v-if="validPromotion">
+                <div
+                  class="leading-[1.4px mt-4 flex justify-between rounded-lg border border-orange p-2 text-[12px] text-orange"
                 >
-                  適用する
-                </button>
-              </div>
+                  <div class="flex items-center gap-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                    クーポンコード適用済み
+                  </div>
+                  <button @click="handleClickCancelPromotionCodeButton">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18 18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </template>
+
+              <template v-else>
+                <div class="mt-4 flex gap-2">
+                  <div class="grow">
+                    <input
+                      v-model="promotionCodeFormValue"
+                      type="text"
+                      class="w-full border border-gray-300 bg-gray-50 p-2.5 text-[14px] md:text-[16px]"
+                      placeholder="クーポンコード"
+                    />
+                  </div>
+                  <button
+                    class="whitespace-nowrap bg-main p-2 text-[14px] text-white md:text-[16px]"
+                    @click="handleClickUsePromotionCodeButton"
+                  >
+                    適用する
+                  </button>
+                </div>
+                <div
+                  v-if="invalidPromotion"
+                  class="mt-2 px-1 text-[12px] leading-[1.2px]"
+                >
+                  指定したクーポンコードは無効です。
+                </div>
+              </template>
 
               <div
-                class="grid grid-cols-5 gap-y-4 border-y border-main py-6 text-[12px] tracking-[1.4px] md:grid-cols-2 md:text-[14px]"
+                class="mt-4 grid grid-cols-5 gap-y-4 border-y border-main py-6 text-[12px] tracking-[1.4px] md:grid-cols-2 md:text-[14px]"
               >
                 <div class="col-span-2 md:col-span-1">商品合計（税込）</div>
                 <div class="col-span-3 text-right md:col-span-1">
