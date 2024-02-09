61709eb9b08f9e6706c4d5e0ca7bcb728a5fabff
taba2424 <tadashi-112@hotmail.co.jp>
2024-02-09 17:29:23 +0900

    fix(gateway): 都道府県をレスポンスに詰める

--- api/internal/gateway/admin/v1/response/address.go
+++ api/internal/gateway/admin/v1/response/address.go
@@ -8,6 +8,7 @@ type Address struct {
 	LastnameKana   string `json:"lastnameKana"`   // 氏名(姓:かな)
 	FirstnameKana  string `json:"firstnameKana"`  // 氏名(名:かな)
 	PostalCode     string `json:"postalCode"`     // 郵便番号
+	Prefecture     string `json:"prefecture"`     // 都道府県
 	PrefectureCode int32  `json:"prefectureCode"` // 都道府県コード
 	City           string `json:"city"`           // 市区町村
 	AddressLine1   string `json:"addressLine1"`   // 町名・番地
