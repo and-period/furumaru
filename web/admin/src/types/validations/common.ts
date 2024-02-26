51e2d04ec41f7b7669560398d55548743e03b34d
sea-kai <sea_rr10_ch@outlook.com>
2024-02-27 00:00:08 +0900

    feat: 商品ページのバリデーション追加と修正 (#2064)
    * fix: 商品選択時に発生する不明なエラーを修正
    
    * feat: 値が違うか確認するバリデーションを追加
    
    * feat: 販売終了日時にバリデーションを追加
    
    * fix: 商品説明でバリデーションエラーメッセージが出るように修正
    
    * chore: yarn lint fix
    
    ---------
    
    Co-authored-by: taba2424 <tadashi-112@hotmail.co.jp>

--- web/admin/src/types/validations/common.ts
+++ web/admin/src/types/validations/common.ts
@@ -1,7 +1,12 @@
 import type { ValidationArgs } from '@vuelidate/core'
-import { required } from '~/lib/validations'
+import { required, notSameAs } from '~/lib/validations'
 
 export const TimeDataValidationRules: ValidationArgs = {
   date: { required },
   time: { required }
 }
+
+export const NotSameTimeDataValidationRules = (startAt: number, otherName?: string): ValidationArgs => ({
+  startAt: { required },
+  endAt: { required, notSameas: notSameAs(startAt, otherName) }
+})
