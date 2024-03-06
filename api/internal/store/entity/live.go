57e392a9c22239ee13409733b680081f4d797c93
taba2424 <tadashi-112@hotmail.co.jp>
2024-03-07 02:46:50 +0900

    fix(store): ライブ商品の並び順を修正

--- api/internal/store/entity/live.go
+++ api/internal/store/entity/live.go
@@ -53,7 +53,7 @@ func NewLive(params *NewLiveParams) *Live {
 }
 
 func (l *Live) Fill(products LiveProducts) {
-	l.ProductIDs = products.ProductIDs()
+	l.ProductIDs = products.SortByCreatedAt().ProductIDs()
 	l.LiveProducts = products
 }
 
