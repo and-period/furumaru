76e7b3e5d17884deb5ff4d8fac2a8fcc1d7d2e3f
taba2424 <tadashi-112@hotmail.co.jp>
2024-03-21 03:29:01 +0900

    chore(admin): update dependencies (#2095)
    * chore(admin): update dependencies
    
    * fix(admin): fix for reviews
    
    * fix(admin): fix small

--- web/admin/stylelint.config.js
+++ web/admin/stylelint.config.js
@@ -2,8 +2,7 @@ module.exports = {
   customSyntax: 'postcss-html',
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-recommended-vue',
-    'stylelint-config-prettier'
+    'stylelint-config-recommended-vue'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
