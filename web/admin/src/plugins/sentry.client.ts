65c52924991597d6c4573012893aa56cec3cd156
taba2424 <tadashi-112@hotmail.co.jp>
2024-03-30 13:22:12 +0900

    feat(admin): Sentryの設定を環境変数経由で渡せるように (#2115)
    * feat(admin): Sentryの設定を環境変数経由で渡せるように
    
    * fix: small

--- web/admin/src/plugins/sentry.client.ts
+++ web/admin/src/plugins/sentry.client.ts
@@ -18,9 +18,9 @@ export default defineNuxtPlugin((nuxtApp) => {
       browserTracingIntegration({ router }),
       replayIntegration({ maskAllText: false, blockAllMedia: false })
     ],
-    tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0,
-    replaysSessionSampleRate: 1.0,
-    replaysOnErrorSampleRate: 1.0
+    tracesSampleRate: runtimeConfig.public.SENTRY_TRACES_SAMPLE_RATE,
+    profilesSampleRate: runtimeConfig.public.SENTRY_PROFILES_SAMPLE_RATE,
+    replaysSessionSampleRate: runtimeConfig.public.SENTRY_REPLAYS_SESSION_SAMPLE_RATE,
+    replaysOnErrorSampleRate: runtimeConfig.public.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE
   })
 })
