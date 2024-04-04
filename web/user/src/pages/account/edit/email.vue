f2545779c76bb1bccf831bc109e6be8b6795b3a8
wf-yamaday <wf.yamaday.job@gmail.com>
2024-04-05 00:02:19 +0900

    feat: メールアドレス変更処理の追加

--- web/user/src/pages/account/edit/email.vue
+++ web/user/src/pages/account/edit/email.vue
@@ -1,16 +1,35 @@
 <script setup lang="ts">
 import { useAuthStore } from '~/store/auth'
 
-const { user } = useAuthStore()
+const router = useRouter()
+const { user, updateEmail } = useAuthStore()
 
 const formData = ref<string>('')
+const errorMessage = ref<string>('')
+
+const handleSubmit = async () => {
+  if (formData.value === '') {
+    return
+  }
+  try {
+    await updateEmail(formData.value)
+    router.push('/account/edit/complete?from=email')
+  } catch (error) {
+    errorMessage.value = error.message
+  }
+}
 </script>
 
 <template>
   <div class="container mx-auto p-4 md:p-0">
     <template v-if="user">
       <the-account-edit-card title="メールアドレスの変更" class="mt-6">
-        <div class="flex w-full flex-col gap-6">
+        <!-- エラーメッセージ -->
+        <the-alert v-if="errorMessage" class="mt-4 w-full">
+          {{ errorMessage }}
+        </the-alert>
+
+        <form class="flex w-full flex-col gap-6" @submit.prevent="handleSubmit">
           <div class="my-10 flex w-full flex-col justify-center gap-14">
             <div>
               <p class="mb-4">現在のメールアドレス</p>
@@ -23,11 +42,14 @@ const formData = ref<string>('')
               class="w-full"
               type="email"
               placeholder="新しいメールアドレス"
+              required
             />
           </div>
 
           <div class="flex w-full flex-col gap-4">
-            <button class="w-ful bg-main px-4 py-2 text-white">変更する</button>
+            <button class="w-ful bg-main px-4 py-2 text-white" type="submit">
+              変更する
+            </button>
             <nuxt-link
               class="border border-main px-4 py-2 text-center text-main"
               to="/account"
@@ -44,7 +66,7 @@ const formData = ref<string>('')
               マイページに戻る
             </nuxt-link>
           </div>
-        </div>
+        </form>
       </the-account-edit-card>
     </template>
 
