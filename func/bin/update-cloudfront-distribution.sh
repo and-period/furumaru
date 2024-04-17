972c967fe21010e61745c8737b47a62694b4a82a
taba2424 <tadashi-112@hotmail.co.jp>
2024-04-17 23:41:33 +0900

    fix(workflow): ワークフローの修正

--- func/bin/update-cloudfront-distribution.sh
+++ func/bin/update-cloudfront-distribution.sh
@@ -4,17 +4,13 @@
 # - ORIGIN_RESPONSE_ARN: オリジンレスポンス用のLambda@Edge関数のARN
 # - CLOUDFRONT_DISTRIBUTION_ID: CloudFrontディストリビューションID
 
-### Fetch the latest version of the Lambda@Edge function
-# For origin response
-ORIGIN_RESPONSE_LATEST_ARN=$(aws lambda list-versions-by-function --function-name ${ORIGIN_RESPONSE_ARN} | jq -r '.Versions[-1].FunctionArn')
-
 # Get CloudFront distribution configuration
 CLOUDFRONT_ETAG=$(aws cloudfront get-distribution-config --id ${CLOUDFRONT_DISTRIBUTION_ID} | jq -r '.ETag')
 
 # Edit the CloudFront distribution configuration to include the new Lambda@Edge function
 aws cloudfront get-distribution-config --id ${CLOUDFRONT_DISTRIBUTION_ID} | \
   jq '.DistributionConfig' | \
-  jq "(.DefaultCacheBehavior.LambdaFunctionAssociations.Items[] | select(.EventType == \"origin-response\") | .LambdaFunctionARN) |= \"${ORIGIN_RESPONSE_LATEST_ARN}\"" \
+  jq "(.DefaultCacheBehavior.LambdaFunctionAssociations.Items[] | select(.EventType == \"origin-response\") | .LambdaFunctionARN) |= \"${ORIGIN_RESPONSE_ARN}\"" \
   > ./config.json
 
 ls -la
