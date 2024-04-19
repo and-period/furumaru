cce28374c4438ba43091a115d52e24f729327d52
taba2424 <tadashi-112@hotmail.co.jp>
2024-04-20 07:56:01 +0900

    fix(workflow): Lambda@Edgeのデプロイフローを修正 (#2170)

--- func/bin/update-cloudfront-distribution.sh
+++ func/bin/update-cloudfront-distribution.sh
@@ -7,11 +7,21 @@
 # Get CloudFront distribution configuration
 CLOUDFRONT_ETAG=$(aws cloudfront get-distribution-config --id ${CLOUDFRONT_DISTRIBUTION_ID} | jq -r '.ETag')
 
+if [ -z "${CLOUDFRONT_ETAG}" ]; then
+  echo "Failed to get the CloudFront distribution configuration"
+  exit 1
+fi
+
 # Edit the CloudFront distribution configuration to include the new Lambda@Edge function
 aws cloudfront get-distribution-config --id ${CLOUDFRONT_DISTRIBUTION_ID} | \
   jq '.DistributionConfig' | \
   jq "(.DefaultCacheBehavior.LambdaFunctionAssociations.Items[] | select(.EventType == \"origin-response\") | .LambdaFunctionARN) |= \"${ORIGIN_RESPONSE_ARN}\"" \
   > ./config.json
+
+if [ ! -s ./config.json ]; then
+  echo "Failed to update the Lambda@Edge function in the CloudFront distribution"
+  exit 1
+fi
 
 # Update the CloudFront distribution with the new Lambda@Edge function
 aws cloudfront update-distribution --id ${CLOUDFRONT_DISTRIBUTION_ID} --if-match ${CLOUDFRONT_ETAG} --distribution-config file://config.json
