4361aa12f576ad23c0f639800c9cf1aa913b6f56
koudai hamada <hamany0326@gmail.com>
2024-05-22 00:33:24 +0900

    Add: added privacy in admin page (#2222)
    * Add: added privacy in admin page
    
    * Refactor: based on coderabbit
    
    * Refactor: fixed lint
    
    * Refactor: fixed lint

---
+++ web/admin/src/pages/legal-notice.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card>
+          <v-card-text class="markdown-content" v-html="legalNoticeMarkdown" />
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { marked } from 'marked'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      legalNoticeMarkdown: ''
+    }
+  },
+  mounted () {
+    this.loadMarkdown()
+  },
+  methods: {
+    async loadMarkdown () {
+      const response = await fetch('/_content/legalPolicy.md')
+      const markdownText = await response.text()
+      this.legalNoticeMarkdown = marked(markdownText)
+    }
+  }
+}
+
+definePageMeta({
+  layout: 'empty'
+})
+</script>
+
+<style>
+.markdown-content {
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+.markdown-content ol {
+  padding: inherit;
+}
+
+.markdown-content h1 {
+  font-size: 1.5em;
+  margin: 1em 0;
+}
+
+.markdown-content h2 {
+  font-size: 1.2em;
+  margin: 1em 0;
+}
+
+.markdown-content p {
+  margin: 1em 0;
+}
+</style>
