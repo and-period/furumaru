357b9675f8233f2f5cf9366b0c773fd63a81785d
wf-yamaday <wf.yamaday.job@gmail.com>
2024-05-29 22:59:25 +0900

    feat: YouTube連携完了画面をとりあえず作成

--- web/admin/src/pages/auth/youtube/complete.vue
+++ web/admin/src/pages/auth/youtube/complete.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import { mdiCheck } from '@mdi/js'
+
+const router = useRouter()
+
+const handleClickBackToTopButton = () => {
+  router.push('/')
+}
+</script>
+
 <template>
-  <div>
-    <h1>WIP）Youtube Complete</h1>
-  </div>
+  <v-card>
+    <v-card-text>
+      <div class="text-center">
+        <v-icon :icon="mdiCheck" color="primary" size="64" />
+        <p class="text-h5">
+          YouTube 連携が完了しました
+        </p>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn variant="outlined" block @click="handleClickBackToTopButton">
+        トップに戻る
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
