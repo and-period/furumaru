8f5620f86a8392eb386a01533d25a16a4c91dd24
wf-yamaday <wf.yamaday.job@gmail.com>
2024-06-07 00:56:30 +0900

    feat: ソート可能な商品リストコンポーネントの実装

---
+++ web/admin/src/components/molecules/SortableProductList.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { useSortable } from '@vueuse/integrations/useSortable'
+import { mdiDrag } from '@mdi/js'
+import { getResizedImages } from '~/lib/helpers'
+import type { Product, ProductMediaInner } from '~/types/api'
+
+const model = defineModel()
+
+interface Props {
+  products: Product[]
+}
+
+defineProps<Props>()
+
+const sortableRef = ref<HTMLElement | null>(null)
+useSortable(sortableRef, model)
+
+const getProductThumbnailUrl = (product: Product): string => {
+  const thumbnail = product.media?.find((media: ProductMediaInner) => {
+    return media.isThumbnail
+  })
+  return thumbnail ? thumbnail.url : ''
+}
+
+const getProductThumbnails = (product: Product): string => {
+  const thumbnail = product.media?.find((media: ProductMediaInner) => {
+    return media.isThumbnail
+  })
+  return thumbnail ? getResizedImages(thumbnail.url) : ''
+}
+
+const getProductInventoryColor = (product: Product): string => {
+  return product.inventory > 0 ? '' : 'text-error'
+}
+</script>
+
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th width="5" />
+        <th />
+        <th>商品名</th>
+        <th>価格</th>
+        <th>在庫</th>
+      </tr>
+    </thead>
+
+    <tbody ref="sortableRef">
+      <tr
+        v-for="product in products"
+        :key="product.id"
+      >
+        <td>
+          <v-icon :icon="mdiDrag" />
+        </td>
+        <td>
+          <v-img
+            aspect-ratio="1/1"
+            :max-height="56"
+            :max-width="80"
+            :src="getProductThumbnailUrl(product)"
+            :srcset="getProductThumbnails(product)"
+          />
+        </td>
+        <td>{{ product.name }}</td>
+        <td>{{ product.price }}</td>
+        <td :class="getProductInventoryColor(product)">
+          {{ product.inventory }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
