7a89c5dcad260c2952e2fd93aaac4275bf3beb94
wf-yamaday <wf.yamaday.job@gmail.com>
2024-07-24 22:45:14 +0900

    fix: 商品詳細ページのレイアウトを調整

--- web/user/src/pages/items/[...id].vue
+++ web/user/src/pages/items/[...id].vue
@@ -157,257 +157,262 @@ useSeoMeta({
   </template>
 
   <template v-if="!productFetchState.isLoading && product.thumbnail">
-    <div
-      class="gap-4 bg-white px-4 pb-6 pt-[40px] text-main md:grid md:grid-cols-2 md:px-[112px]"
-    >
-      <div class="mx-auto w-full max-w-[500px]">
-        <div class="flex aspect-square h-full w-full justify-center">
-          <nuxt-img
-            provider="cloudFront"
-            fill="contain"
-            class="block h-full w-full object-contain border"
-            :src="
-              selectedMediaIndex === -1
-                ? product.thumbnail.url
-                : product.media[selectedMediaIndex].url
-            "
-            :alt="itemThumbnailAlt"
-          />
-        </div>
-        <div
-          class="hidden-scrollbar mt-2 grid w-full grid-flow-col justify-start gap-2 overflow-x-scroll"
-        >
-          <nuxt-img
-            v-for="(m, i) in product.media"
-            :key="i"
-            width="72px"
-            fill="contain"
-            provider="cloudFront"
-            :src="m.url"
-            :alt="`${itemThumbnailAlt}_${i}`"
-            class="aspect-square w-[72px] cursor-pointer object-contain border"
-            @click="handleClickMediaItem(i)"
-          />
-        </div>
-      </div>
-
-      <div class="mt-4 flex w-full flex-col gap-4">
-        <div
-          class="break-words text-[16px] tracking-[1.6px] md:text-[24px] md:tracking-[2.4px]"
-        >
-          {{ product.name }}
-        </div>
-
-        <div
-          v-if="product.producer"
-          class="flex flex-col leading-[32px] md:mt-4"
-        >
+    <div class="bg-white w-full">
+      <div
+        class="gap-10 px-4 pb-6 pt-[40px] text-main md:grid md:grid-cols-2 lg:px-[112px] w-full max-w-[1440px] mx-auto"
+      >
+        <div class="mx-auto w-full max-w-[100%]">
+          <div class="flex aspect-square h-full w-full justify-center">
+            <nuxt-img
+              provider="cloudFront"
+              fill="contain"
+              class="block h-full w-full object-contain border"
+              :src="
+                selectedMediaIndex === -1
+                  ? product.thumbnail.url
+                  : product.media[selectedMediaIndex].url
+              "
+              :alt="itemThumbnailAlt"
+            />
+          </div>
           <div
-            class="text-[14px] tracking-[1.4px] md:text-[16px] md:tracking-[1.6px]"
+            class="hidden-scrollbar mt-2 grid w-full grid-flow-col justify-start gap-2 overflow-x-scroll"
           >
-            {{ dt("producerLabel") }}:
-            <a
-              href="#"
-              class="font-bold underline"
+            <nuxt-img
+              v-for="(m, i) in product.media"
+              :key="i"
+              width="72px"
+              fill="contain"
+              provider="cloudFront"
+              :src="m.url"
+              :alt="`${itemThumbnailAlt}_${i}`"
+              class="aspect-square w-[72px] cursor-pointer object-contain border"
+              @click="handleClickMediaItem(i)"
+            />
+          </div>
+        </div>
+
+        <div class="mt-4 flex w-full flex-col gap-4">
+          <div
+            class="break-words text-[16px] tracking-[1.6px] md:text-[24px] md:tracking-[2.4px]"
+          >
+            {{ product.name }}
+          </div>
+
+          <div
+            v-if="product.producer"
+            class="flex flex-col leading-[32px] md:mt-4"
+          >
+            <div
+              class="text-[14px] tracking-[1.4px] md:text-[16px] md:tracking-[1.6px]"
             >
-              {{ product.producer.username }}
-            </a>
+              {{ dt("producerLabel") }}:
+              <a
+                href="#"
+                class="font-bold underline"
+              >
+                {{ product.producer.username }}
+              </a>
+            </div>
+            <div class="text-[12px] tracking-[1.4px] md:text-[14px]">
+              {{ product.originPrefecture }} {{ product.originCity }}
+            </div>
           </div>
-          <div class="text-[12px] tracking-[1.4px] md:text-[14px]">
-            {{ product.originPrefecture }} {{ product.originCity }}
+
+          <div
+            v-if="product && product.recommendedPoint1"
+            class="w-full rounded-2xl bg-base px-[20px] py-[28px] text-main md:mt-8"
+          >
+            <p
+              class="mb-[12px] text-[12px] font-medium tracking-[1.4px] md:text-[14px]"
+            >
+              {{ dt("highlightsLabel") }}
+            </p>
+            <ol
+              class="recommend-list flex flex-col divide-y divide-dashed divide-main px-[4px] pl-[24px]"
+            >
+              <li
+                v-if="product.recommendedPoint1"
+                class="py-3 text-[14px] font-medium md:text-[16px]"
+              >
+                {{ product.recommendedPoint1 }}
+              </li>
+              <li
+                v-if="product.recommendedPoint2"
+                class="py-3 text-[14px] font-medium md:text-[16px]"
+              >
+                {{ product.recommendedPoint2 }}
+              </li>
+              <li
+                v-if="product.recommendedPoint3"
+                class="py-3 text-[14px] font-medium md:text-[16px]"
+              >
+                {{ product.recommendedPoint3 }}
+              </li>
+            </ol>
           </div>
+
+          <div>
+            <div class="flex items-end justify-end md:justify-start">
+              <div
+                class="text-[24px] font-bold md:mt-[60px] md:flex md:flex-row md:text-[32px]"
+              >
+                {{ priceString }}
+              </div>
+              <p class="pb-1 pl-2 text-[12px] md:text-[16px]">
+                {{ dt("itemPriceTaxIncludedText") }}
+              </p>
+            </div>
+
+            <div
+              v-if="product"
+              class="mt-4 inline-flex items-center md:mt-8"
+            >
+              <label class="mr-2 block text-[14px] md:text-[16px]">{{
+                dt("quantityLabel")
+              }}</label>
+              <select
+                v-model="quantity"
+                class="h-full border-[1px] border-main px-2"
+                :disabled="!product.hasStock"
+              >
+                <option
+                  v-for="(_, i) in Array.from({
+                    length: product.inventory < 10 ? product.inventory : 10,
+                  })"
+                  :key="i + 1"
+                  :value="i + 1"
+                >
+                  {{ i + 1 }}
+                </option>
+              </select>
+            </div>
+          </div>
+
+          <button
+            class="mt-2 w-full bg-main py-4 text-center text-white disabled:cursor-not-allowed disabled:bg-main/60 md:mt-8"
+            :disabled="!canAddCart"
+            @click="handleClickAddCartButton"
+          >
+            {{ dt("addToCartText") }}
+          </button>
+
+          <div class="mt-4 inline-flex gap-4">
+            <span
+              v-for="productTag in product.productTags"
+              :key="productTag?.id"
+              class="rounded-2xl border border-main px-4 py-1 text-[14px] md:text-[16px]"
+            >
+              {{ productTag?.name }}
+            </span>
+          </div>
+        </div>
+
+        <div class="col-span-2 mt-[40px] pb-10 md:mt-[80px] md:pb-16">
+          <article
+            class="text-[14px] leading-[32px] tracking-[1.4px] md:text-[16px] md:tracking-[1.6px] whitespace-pre-wrap"
+            v-text="product.description"
+          />
         </div>
 
         <div
-          v-if="product && product.recommendedPoint1"
-          class="w-full rounded-2xl bg-base px-[20px] py-[28px] text-main md:mt-8"
+          class="col-span-2 flex flex-col divide-y divide-dashed divide-main border-y border-dashed border-main text-[14px] md:text-[16px]"
         >
-          <p
-            class="mb-[12px] text-[12px] font-medium tracking-[1.4px] md:text-[14px]"
-          >
-            {{ dt("highlightsLabel") }}
-          </p>
-          <ol
-            class="recommend-list flex flex-col divide-y divide-dashed divide-main px-[4px] pl-[24px]"
-          >
-            <li
-              v-if="product.recommendedPoint1"
-              class="py-3 text-[14px] font-medium md:text-[16px]"
-            >
-              {{ product.recommendedPoint1 }}
-            </li>
-            <li
-              v-if="product.recommendedPoint2"
-              class="py-3 text-[14px] font-medium md:text-[16px]"
-            >
-              {{ product.recommendedPoint2 }}
-            </li>
-            <li
-              v-if="product.recommendedPoint3"
-              class="py-3 text-[14px] font-medium md:text-[16px]"
-            >
-              {{ product.recommendedPoint3 }}
-            </li>
-          </ol>
-        </div>
-
-        <div>
-          <div class="flex items-end justify-end md:justify-start">
-            <div
-              class="text-[24px] font-bold md:mt-[60px] md:flex md:flex-row md:text-[32px]"
-            >
-              {{ priceString }}
-            </div>
-            <p class="pb-1 pl-2 text-[12px] md:text-[16px]">
-              {{ dt("itemPriceTaxIncludedText") }}
+          <div class="grid grid-cols-5 py-4">
+            <p class="col-span-2 md:col-span-1">
+              {{ dt("expirationDateLabel") }}
+            </p>
+            <p class="col-span-3 md:col-span-4">
+              {{ expirationDateText }}
             </p>
           </div>
-
-          <div
-            v-if="product"
-            class="mt-4 inline-flex items-center md:mt-8"
-          >
-            <label class="mr-2 block text-[14px] md:text-[16px]">{{
-              dt("quantityLabel")
-            }}</label>
-            <select
-              v-model="quantity"
-              class="h-full border-[1px] border-main px-2"
-              :disabled="!product.hasStock"
-            >
-              <option
-                v-for="(_, i) in Array.from({
-                  length: product.inventory < 10 ? product.inventory : 10,
-                })"
-                :key="i + 1"
-                :value="i + 1"
-              >
-                {{ i + 1 }}
-              </option>
-            </select>
+          <div class="grid grid-cols-5 py-4">
+            <p class="col-span-2 md:col-span-1">
+              {{ dt("weightLabel") }}
+            </p>
+            <p class="col-span-3 md:col-span-4">
+              {{ product.weight }}kg
+            </p>
           </div>
-        </div>
-
-        <button
-          class="mt-2 w-full bg-main py-4 text-center text-white disabled:cursor-not-allowed disabled:bg-main/60 md:mt-8"
-          :disabled="!canAddCart"
-          @click="handleClickAddCartButton"
-        >
-          {{ dt("addToCartText") }}
-        </button>
-
-        <div class="mt-4 inline-flex gap-4">
-          <span
-            v-for="productTag in product.productTags"
-            :key="productTag?.id"
-            class="rounded-2xl border border-main px-4 py-1 text-[14px] md:text-[16px]"
-          >
-            {{ productTag?.name }}
-          </span>
-        </div>
-      </div>
-
-      <div class="col-span-2 mt-[40px] pb-10 md:mt-[80px] md:pb-16">
-        <article
-          class="text-[14px] leading-[32px] tracking-[1.4px] md:text-[16px] md:tracking-[1.6px] whitespace-pre-wrap"
-          v-text="product.description"
-        />
-      </div>
-
-      <div
-        class="col-span-2 flex flex-col divide-y divide-dashed divide-main border-y border-dashed border-main text-[14px] md:text-[16px]"
-      >
-        <div class="grid grid-cols-5 py-4">
-          <p class="col-span-2 md:col-span-1">
-            {{ dt("expirationDateLabel") }}
-          </p>
-          <p class="col-span-3 md:col-span-4">
-            {{ expirationDateText }}
-          </p>
-        </div>
-        <div class="grid grid-cols-5 py-4">
-          <p class="col-span-2 md:col-span-1">
-            {{ dt("weightLabel") }}
-          </p>
-          <p class="col-span-3 md:col-span-4">
-            {{ product.weight }}kg
-          </p>
-        </div>
-        <div class="grid grid-cols-5 py-4">
-          <p class="col-span-2 md:col-span-1">
-            {{ dt("deliveryTypeLabel") }}
-          </p>
-          <p class="col-span-3 md:col-span-4">
-            {{ getDeliveryType(product.deliveryType) }}
-          </p>
-        </div>
-        <div class="grid grid-cols-5 py-4">
-          <p class="col-span-2 md:col-span-1">
-            {{ dt("storageTypeLabel") }}
-          </p>
-          <p class="col-span-3 md:col-span-4">
-            {{ getStorageMethodType(product.storageMethodType) }}
-          </p>
+          <div class="grid grid-cols-5 py-4">
+            <p class="col-span-2 md:col-span-1">
+              {{ dt("deliveryTypeLabel") }}
+            </p>
+            <p class="col-span-3 md:col-span-4">
+              {{ getDeliveryType(product.deliveryType) }}
+            </p>
+          </div>
+          <div class="grid grid-cols-5 py-4">
+            <p class="col-span-2 md:col-span-1">
+              {{ dt("storageTypeLabel") }}
+            </p>
+            <p class="col-span-3 md:col-span-4">
+              {{ getStorageMethodType(product.storageMethodType) }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </template>
 
   <template v-if="product.producer">
-    <div class="mx-auto mt-[40px] w-full px-4 xl:px-28">
-      <div
-        class="flex w-full flex-col rounded-3xl bg-white px-8 py-10 text-main xl:px-16"
-      >
-        <p
-          class="mx-auto w-full rounded-full bg-base py-2 text-center text-[14px] font-bold text-main md:text-[16px]"
+    <div class="w-full">
+      <div class="mx-auto mt-[40px] w-full px-4 xl:px-28 max-w-[1440px]">
+        <div
+          class="flex w-full flex-col rounded-3xl bg-white px-8 py-10 text-main xl:px-16"
         >
-          {{ dt("producerInformationTitle") }}
-        </p>
-
-        <div class="mt-[64px] flex w-full flex-col gap-4 md:flex-row lg:gap-10">
-          <div
-            class="flex min-w-max flex-col items-center justify-center gap-4 md:flex-row"
+          <p
+            class="mx-auto w-full rounded-full bg-base py-2 text-center text-[14px] font-bold text-main md:text-[16px]"
           >
-            <template v-if="product.producer.thumbnailUrl">
-              <nuxt-img
-                provider="cloudFront"
-                sizes="96px md:120px"
-                fit="cover"
-                :src="product.producer.thumbnailUrl"
-                :alt="`${product.producer.username}`"
-                class="mx-auto block aspect-square w-[96px] rounded-full md:w-[120px] object-cover"
-              />
-            </template>
-            <template v-else>
-              <img
-                class="mx-auto block aspect-square w-[96px] rounded-full md:w-[120px] object-cover"
-                src="/img/account.png"
-              >
-            </template>
+            {{ dt("producerInformationTitle") }}
+          </p>
+
+          <div
+            class="mt-[64px] flex w-full flex-col gap-4 md:flex-row lg:gap-10"
+          >
             <div
-              class="flex min-w-max grow flex-col items-center gap-2 md:items-start md:gap-2 md:whitespace-nowrap"
+              class="flex min-w-max flex-col items-center justify-center gap-4 md:flex-row"
             >
-              <p class="text-sm font-[500] tracking-[1.4px]">
-                {{ `${product.originPrefecture} ${product.originCity}` }}
-              </p>
+              <template v-if="product.producer.thumbnailUrl">
+                <nuxt-img
+                  provider="cloudFront"
+                  sizes="96px md:120px"
+                  fit="cover"
+                  :src="product.producer.thumbnailUrl"
+                  :alt="`${product.producer.username}`"
+                  class="mx-auto block aspect-square w-[96px] rounded-full md:w-[120px] object-cover"
+                />
+              </template>
+              <template v-else>
+                <img
+                  class="mx-auto block aspect-square w-[96px] rounded-full md:w-[120px] object-cover"
+                  src="/img/account.png"
+                >
+              </template>
               <div
-                class="flex flex-row items-baseline grow text-[16px] tracking-[1.4px] md:text-[24px]"
+                class="flex min-w-max grow flex-col items-center gap-2 md:items-start md:gap-2 md:whitespace-nowrap"
               >
-                <p class="mr-1 text-[14px] font-medium">
-                  {{ dt("producerLabel") }}
+                <p class="text-sm font-[500] tracking-[1.4px]">
+                  {{ `${product.originPrefecture} ${product.originCity}` }}
                 </p>
-                <p>{{ product.producer.username }}</p>
+                <div
+                  class="flex flex-row items-baseline grow text-[16px] tracking-[1.4px] md:text-[24px]"
+                >
+                  <p class="mr-1 text-[14px] font-medium">
+                    {{ dt("producerLabel") }}
+                  </p>
+                  <p>{{ product.producer.username }}</p>
+                </div>
               </div>
             </div>
+            <div
+              class="pt-2 text-[14px] tracking-[1.4px] md:pt-0 md:text-[16px] md:tracking-[1.6px]"
+            >
+              {{ product.producer.profile }}
+            </div>
           </div>
-          <div
-            class="pt-2 text-[14px] tracking-[1.4px] md:pt-0 md:text-[16px] md:tracking-[1.6px]"
-          >
-            {{ product.producer.profile }}
-          </div>
-        </div>
 
-        <!--
+          <!--
         <div class="mt-4 w-full text-right">
           <button class="inline-flex items-center">
             詳しく見る
@@ -415,6 +420,7 @@ useSeoMeta({
           </button>
         </div>
         -->
+        </div>
       </div>
     </div>
   </template>
