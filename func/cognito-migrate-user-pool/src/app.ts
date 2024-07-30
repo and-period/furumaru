eec4287407ace1fc649db518d5439070ab19b78f
taba2424 <tadashi-112@hotmail.co.jp>
2024-07-31 02:06:14 +0900

    fix(func): Cognitoユーザープールの移行関数の修正

--- func/cognito-migrate-user-pool/src/app.ts
+++ func/cognito-migrate-user-pool/src/app.ts
@@ -37,6 +37,7 @@ export const lambdaHandler = async (event: CognitoUserPoolTriggerEvent): Promise
     } catch (err: any) {
       throw new Error(`failed to initiate auth. err=${err.message}`);
     }
+    event.response.finalUserStatus = 'CONFIRMED';
     console.log('success to initiate auth', JSON.stringify(auth));
   }
 
@@ -49,7 +50,6 @@ export const lambdaHandler = async (event: CognitoUserPoolTriggerEvent): Promise
   console.log('success to get user', JSON.stringify(user));
 
   event.response.userAttributes = toUserAttributes(user);
-  event.response.finalUserStatus = 'CONFIRMED';
   event.response.messageAction = 'SUPPRESS';
 
   console.log('return event', JSON.stringify(event));
