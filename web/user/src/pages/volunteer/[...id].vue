1431213045f132ba3ea1e3c27a216a0c8896db43
wf-yamaday <wf.yamaday.job@gmail.com>
2024-08-01 00:44:36 +0900

    feat: ページネーションなどを追加

--- web/user/src/pages/volunteer/[...id].vue
+++ web/user/src/pages/volunteer/[...id].vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
-const route = useRoute();
+import type { VolunteerBlogItemResponse } from '~/types/cms/volunteer'
+
+const route = useRoute()
 
 const id = computed(() => {
-  return route.params.id as string;
-});
+  return route.params.id as string
+})
 
-const { data } = await useAsyncData(`volunteer-${id.value}`, () => {
-  return $fetch(`/api/cms/volunteer/${id.value}`);
-});
+const { data } = await useAsyncData<VolunteerBlogItemResponse>(
+  `volunteer-${id.value}`,
+  () => {
+    return $fetch(`/api/cms/volunteer/${id.value}`)
+  },
+)
 
 const title = computed(() => {
-  return data.value.title;
-});
+  return data.value?.title || ''
+})
 
 useSeoMeta({
   title,
-});
+})
 </script>
 
 <template>
@@ -25,7 +30,7 @@ useSeoMeta({
         to="/volunteer"
         class="md:text-[14px] text-[12px] font-bold tracking-[2px] text-main md:mb-4 mb-8 inline-flex gap-1 items-center"
       >
-        <TheLeftArrowIcon class="h-3" />
+        <the-left-arrow-icon class="h-3" />
         一覧に戻る
       </nuxt-link>
     </div>
@@ -33,14 +38,14 @@ useSeoMeta({
     <div
       class="container mx-auto text-center text-[14px] font-bold tracking-[2px] md:text-[20px]"
     >
-      <template v-if="data.title">
+      <template v-if="data">
         {{ data.title }}
       </template>
     </div>
     <div class="grow container mx-auto">
-      <hr class="mt-[40px] mb-[20px]" />
+      <hr class="mt-[40px] mb-[20px]">
       <div
-        v-if="data.content"
+        v-if="data"
         class="prose md:prose-lg px-4 max-w-full"
         v-html="data.content"
       />
--- web/user/src/pages/volunteer/index.vue
+++ web/user/src/pages/volunteer/index.vue
@@ -1,7 +1,52 @@
 <script setup lang="ts">
-const { data } = await useAsyncData('volunteer-list', () => {
-  return $fetch('/api/cms/volunteer')
+import type { VolunteerBlogListResponse } from '~/types/cms/volunteer'
+
+const route = useRoute()
+const router = useRouter()
+
+const { data } = await useAsyncData<VolunteerBlogListResponse>(
+  'volunteer-list',
+  () => {
+    return $fetch('/api/cms/volunteer')
+  },
+)
+
+const totalCount = computed(() => {
+  return data.value?.totalCount || 0
 })
+
+// 現在のページ番号
+const currentPage = computed<number>(() => {
+  return route.query.page ? Number(route.query.page) : 1
+})
+
+// 1ページ当たりに表示する商品数
+const pagePerItems = ref<number>(20)
+
+// ページネーション情報
+const pagination = computed<{
+  limit: number
+  offset: number
+  pageArray: number[]
+}>(() => {
+  const totalPage = Math.ceil(totalCount.value / pagePerItems.value)
+  const pageArray = Array.from({ length: totalPage }, (_, i) => i + 1)
+
+  return {
+    limit: pagePerItems.value,
+    offset: pagePerItems.value * (currentPage.value - 1),
+    pageArray,
+  }
+})
+
+const handleClickPage = (page: number) => {
+  router.push({
+    query: {
+      ...route.query,
+      page,
+    },
+  })
+}
 
 useSeoMeta({
   title: 'ひろしま援農計画',
@@ -17,12 +62,13 @@ useSeoMeta({
     </div>
     <div class="grow container mx-auto">
       <hr class="mt-[40px] mb-[20px]">
-      <template v-if="data.contents">
+      <template v-if="data">
         <div class="grid md:grid-cols-3 w-full px-4">
           <nuxt-link
             v-for="content in data.contents"
             :key="content.id"
             :to="`/volunteer/${content.id}`"
+            class="flex flex-col gap-4"
           >
             <img
               :src="content.eyecatch.url"
@@ -32,20 +78,35 @@ useSeoMeta({
               <h2 class="text-[18px] font-semibold tracking-[2px]">
                 {{ content.title }}
               </h2>
+
+              <div class="text-gray flex flex-col gap-2 tracking-[10%]">
+                <div>{{ content.name }}</div>
+                <div class="inline-flex gap-2 items-center">
+                  <the-map-pin-icon class="h-4 w-4" />
+                  {{ content.location }}
+                </div>
+              </div>
+
               <div
                 class="inline-flex gap-4 [&>span]:px-3 [&>span]:py-1 [&>span]:border [&>span]:border-main [&>span]:text-main [&>span]:rounded-full text-[14px]"
               >
-                <span v-if="content.category1">
-                  {{ content.category1.name }}
-                </span>
-                <span v-if="content.category2">
-                  {{ content.category2.name }}
+                <span
+                  v-for="category in content.category"
+                  :key="category.id"
+                >
+                  {{ category }}
                 </span>
               </div>
             </div>
           </nuxt-link>
         </div>
       </template>
+      <the-pagination
+        class="mt-8"
+        :current-page="currentPage"
+        :page-array="pagination.pageArray"
+        @change-page="handleClickPage"
+      />
     </div>
   </div>
 </template>
