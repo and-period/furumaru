16e194e51ac6adf935744c5383055f69580bdfa5
wf-yamaday <wf.yamaday.job@gmail.com>
2024-08-06 17:56:39 +0900

    fix: タイトルを消す

--- web/user/src/pages/volunteer/[...id].vue
+++ web/user/src/pages/volunteer/[...id].vue
@@ -29,25 +29,17 @@ useSeoMeta({
 
 <template>
   <div class="w-full bg-white flex flex-col py-[48px] text-main">
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4">
       <nuxt-link
         to="/volunteer"
-        class="md:text-[14px] text-[12px] font-bold tracking-[2px] text-main md:mb-4 mb-8 inline-flex gap-1 items-center"
+        class="md:text-[14px] text-[12px] font-bold tracking-[2px] text-main mb-8 inline-flex gap-1 items-center"
       >
         <the-left-arrow-icon class="h-3" />
         一覧に戻る
       </nuxt-link>
     </div>
 
-    <div
-      class="container mx-auto text-center text-[14px] font-bold tracking-[2px] md:text-[20px]"
-    >
-      <template v-if="data">
-        {{ data.title }}
-      </template>
-    </div>
     <div class="grow container mx-auto">
-      <hr class="mt-[40px] mb-[20px]">
       <div
         v-if="data"
         class="prose md:prose-lg px-4 max-w-full"
