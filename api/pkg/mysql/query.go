5a8faa0adf226c0340f9ad0430a90e7a757f87a7
taba2424 <tadashi-112@hotmail.co.jp>
2024-08-28 22:55:00 +0900

    feat(mysql): MySQLでGEOMETRY型が扱えるようなstructを定義 (#2360)
    * feat(mysql): MySQLでGEOMETRY型が扱えるようなstructを定義
    
    * fix(mysql): テストコードの修正

--- api/pkg/mysql/query.go
+++ api/pkg/mysql/query.go
@@ -1,5 +1,17 @@
 package mysql
 
+import (
+	"bytes"
+	"context"
+	"database/sql/driver"
+	"encoding/binary"
+	"fmt"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
+)
+
 type Integer interface {
 	~int | ~int32 | ~int64
 }
@@ -18,4 +30,74 @@ func NullString(val string) any {
 		return nil
 	}
 	return val
+}
+
+// Geometry 座標情報
+type Geometry struct {
+	X float64
+	Y float64
+}
+
+func (g Geometry) Value() (driver.Value, error) {
+	if g.X == 0 && g.Y == 0 {
+		return "POINT(0.000000 0.000000)", nil
+	}
+	return fmt.Sprintf("POINT(%f %f)", g.X, g.Y), nil
+}
+
+func (g *Geometry) Scan(value interface{}) error {
+	if value == nil {
+		*g = Geometry{}
+		return nil
+	}
+
+	var longitude, latitude float64
+	// @see: https://dev.mysql.com/doc/refman/8.0/en/gis-data-formats.html
+	switch v := value.(type) {
+	case []byte:
+		if len(v) != 25 {
+			return fmt.Errorf("query: expected []bytes with length 25, got %d", len(v))
+		}
+		buf := bytes.NewReader(v[9:17])
+		if err := binary.Read(buf, binary.LittleEndian, &longitude); err != nil {
+			return err
+		}
+		buf = bytes.NewReader(v[17:])
+		if err := binary.Read(buf, binary.LittleEndian, &latitude); err != nil {
+			return err
+		}
+	case string:
+		if _, err := fmt.Sscanf(v, "POINT(%f %f)", &longitude, &latitude); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("query: unsupported Geometry type: %T", value)
+	}
+
+	*g = Geometry{X: longitude, Y: latitude}
+	return nil
+}
+
+func (g Geometry) GormDataType() string {
+	return "geometry"
+}
+
+func (g Geometry) GormDBDataType(db *gorm.DB, field *schema.Field) string {
+	switch db.Dialector.Name() {
+	case "mysql":
+		return "GEOMETRY"
+	default:
+		return ""
+	}
+}
+
+func (g Geometry) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
+	data, _ := g.Value()
+
+	switch db.Dialector.Name() {
+	case "mysql":
+		return gorm.Expr("ST_GeomFromText(?)", data)
+	default:
+		return gorm.Expr("?", data)
+	}
 }
--- api/pkg/mysql/query_test.go
+++ api/pkg/mysql/query_test.go
@@ -1,9 +1,13 @@
 package mysql
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func TestNullInt(t *testing.T) {
@@ -43,5 +47,111 @@ func TestNullString(t *testing.T) {
 	t.Run("null string", func(t *testing.T) {
 		t.Parallel()
 		assert.Equal(t, nil, NullString(""))
+	})
+}
+
+func TestGeometry_Value(t *testing.T) {
+	t.Parallel()
+	t.Run("null geometry", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+		value, err := geometry.Value()
+		assert.NoError(t, err)
+		assert.Equal(t, "POINT(0.000000 0.000000)", value)
+	})
+	t.Run("non null geometry", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{X: 1, Y: 1}
+		value, err := geometry.Value()
+		assert.NoError(t, err)
+		assert.Equal(t, "POINT(1.000000 1.000000)", value)
+	})
+}
+
+func TestGeometry_Scan(t *testing.T) {
+	t.Parallel()
+	t.Run("null geometry", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+		err := geometry.Scan(nil)
+		assert.NoError(t, err)
+		assert.Equal(t, Geometry{}, geometry)
+	})
+	t.Run("valid byte type", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+
+		err := geometry.Scan([]byte{0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 240, 63, 0, 0, 0, 0, 0, 0, 240, 191})
+		assert.NoError(t, err)
+		assert.Equal(t, Geometry{X: 1, Y: -1}, geometry)
+	})
+	t.Run("invalid byte format", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+
+		err := geometry.Scan([]byte{0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 240, 63, 0, 0, 0, 0, 0, 0})
+		assert.Error(t, err)
+		assert.Equal(t, Geometry{}, geometry)
+	})
+	t.Run("valid string type", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+
+		err := geometry.Scan("POINT(1.000000 -1.000000)")
+		assert.NoError(t, err)
+		assert.Equal(t, Geometry{X: 1, Y: -1}, geometry)
+	})
+	t.Run("invalid string format", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+
+		err := geometry.Scan("POINT(1.000000 -1.000000")
+		assert.Error(t, err)
+		assert.Equal(t, Geometry{}, geometry)
+	})
+	t.Run("invalid type", func(t *testing.T) {
+		t.Parallel()
+		geometry := Geometry{}
+
+		err := geometry.Scan(1)
+		assert.Error(t, err)
+		assert.Equal(t, Geometry{}, geometry)
+	})
+}
+
+func TestGeometry_GormDataType(t *testing.T) {
+	t.Parallel()
+	t.Run("geometry", func(t *testing.T) {
+		geometry := Geometry{X: 1, Y: 1}
+		dataType := geometry.GormDataType()
+		assert.Equal(t, "geometry", dataType)
+	})
+}
+
+func TestGeometry_GormDBDataType(t *testing.T) {
+	t.Parallel()
+	t.Run("mysql", func(t *testing.T) {
+		t.Parallel()
+		dialector := mysql.New(mysql.Config{})
+		db := &gorm.DB{Config: &gorm.Config{Dialector: dialector}}
+		field := &schema.Field{}
+
+		geometry := Geometry{X: 1, Y: 1}
+		dbDataType := geometry.GormDBDataType(db, field)
+		assert.Equal(t, "GEOMETRY", dbDataType)
+	})
+}
+
+func TestGeometry_GormValue(t *testing.T) {
+	t.Parallel()
+	t.Run("mysql", func(t *testing.T) {
+		t.Parallel()
+		dialector := mysql.New(mysql.Config{})
+		db := &gorm.DB{Config: &gorm.Config{Dialector: dialector}}
+
+		geometry := Geometry{X: 1, Y: 1}
+		expr := geometry.GormValue(context.Background(), db)
+		assert.Equal(t, "ST_GeomFromText(?)", expr.SQL)
+		assert.Equal(t, []interface{}{"POINT(1.000000 1.000000)"}, expr.Vars)
 	})
 }
