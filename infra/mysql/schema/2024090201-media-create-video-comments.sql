9f662ed94d55a8f9b82899ee2e55825c70e02baf
taba2424 <tadashi-112@hotmail.co.jp>
2024-09-02 00:56:15 +0900

    Update 2024090201-media-create-video-comments.sql

--- infra/mysql/schema/2024090201-media-create-video-comments.sql
+++ infra/mysql/schema/2024090201-media-create-video-comments.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `media`.`video_comments` (
   `created_at` DATETIME(3)  NOT NULL, -- 登録日時
   `updated_at` DATETIME(3)  NOT NULL, -- 更新日時
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_broadcast_comments_video_id`
+  CONSTRAINT `fk_video_comments_video_id`
     FOREIGN KEY (`video_id`) REFERENCES `media`.`videos` (`id`)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
