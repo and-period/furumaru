bb4ade7cd9da199b5665e1e8f036be8c5db404e6
wf-yamaday <wf.yamaday.job@gmail.com>
2024-09-02 15:41:39 +0900

    feat: 動画新規登録に動画アップロード、サムネイル画像アップロードの関数を移植

--- web/admin/src/pages/videos/new.vue
+++ web/admin/src/pages/videos/new.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useVideoStore } from '~/store'
 import type { CreateVideoRequest, Product, Experience } from '~/types/api'
+import { ApiBaseError } from '~/types/exception'
 
 const router = useRouter()
 const videoStore = useVideoStore()
@@ -23,6 +24,91 @@ const formData = ref<CreateVideoRequest>({
 const selectedProducts = ref<Product[]>([])
 const selectedExperiences = ref<Experience[]>([])
 
+/**
+ * 動画ファイルアップロードステータス
+ */
+const uploadVideoStatus = ref<{
+  isUploading: boolean
+  hasError: boolean
+  errorMessage: string
+}>({
+  isUploading: false,
+  hasError: false,
+  errorMessage: '',
+})
+
+/**
+ * 動画ファイルアップロード関数
+ */
+const handleUploadVideo = async (file: File) => {
+  try {
+    uploadVideoStatus.value.isUploading = true
+    const newUrl = await videoStore.uploadVideoFile(file)
+    formData.value.videoUrl = newUrl
+  }
+  catch (error) {
+    uploadVideoStatus.value.hasError = true
+    if (error instanceof ApiBaseError) {
+      uploadVideoStatus.value.errorMessage = error.message
+    }
+    else {
+      uploadVideoStatus.value.errorMessage
+        = '動画ファイルのアップロードに失敗しました。不明なエラーが発生しました。'
+    }
+  }
+  finally {
+    uploadVideoStatus.value.isUploading = false
+  }
+}
+
+/**
+ * サムネイル画像アップロードステータス
+ */
+const uploadThumbnailStatus = ref<{
+  isUploading: boolean
+  hasError: boolean
+  errorMessage: string
+}>({
+  isUploading: false,
+  hasError: false,
+  errorMessage: '',
+})
+
+/**
+ * サムネイル画像ファイルアップロード関数
+ * @param file
+ */
+const handleUploadThumbnail = async (file: File) => {
+  try {
+    uploadThumbnailStatus.value.isUploading = true
+    const newUrl = await videoStore.uploadThumbnailFile(file)
+    formData.value.thumbnailUrl = newUrl
+  }
+  catch (error) {
+    uploadThumbnailStatus.value.hasError = true
+    if (error instanceof ApiBaseError) {
+      uploadThumbnailStatus.value.errorMessage = error.message
+    }
+    else {
+      uploadThumbnailStatus.value.errorMessage
+        = 'サムネイル画像のアップロードに失敗しました。不明なエラーが発生しました。'
+    }
+  }
+  finally {
+    uploadThumbnailStatus.value.isUploading = false
+  }
+}
+
+const handleSubmit = async () => {
+  try {
+    await videoStore.createVideo(formData.value)
+    router.push('/videos')
+  }
+  catch (error) {
+    console.log('create error')
+  }
+}
+
 const handleClickBackButton = () => {
   router.back()
 }
@@ -38,6 +124,15 @@ const handleClickBackButton = () => {
           v-model="formData"
           :selected-products="selectedProducts"
           :selected-experiences="selectedExperiences"
+          :video-is-uploading="uploadVideoStatus.isUploading"
+          :video-has-error="uploadVideoStatus.hasError"
+          :video-error-message="uploadVideoStatus.errorMessage"
+          :thumbnail-is-uploading="uploadThumbnailStatus.isUploading"
+          :thumbnail-has-error="uploadThumbnailStatus.hasError"
+          :thumbnail-error-message="uploadThumbnailStatus.errorMessage"
+          @update:video="handleUploadVideo"
+          @update:thumbnail="handleUploadThumbnail"
+          @submit="handleSubmit"
         />
       </v-card-text>
     </v-card>
@@ -55,6 +150,8 @@ const handleClickBackButton = () => {
         <v-btn
           color="primary"
           variant="outlined"
+          type="submit"
+          form="new-video-form"
         >
           保存
         </v-btn>
