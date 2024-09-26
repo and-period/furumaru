333b16a57dafee5956b6e2590d8a559095d1797f
wf-yamaday <wf.yamaday.job@gmail.com>
2024-09-27 00:38:46 +0900

    feat: 体験ページのひな型を配置

---
+++ web/user/src/pages/experiences/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+const config = useRuntimeConfig()
+
+const center = { lat: 40.689247, lng: -74.044502 }
+</script>
+
+<template>
+  <div
+    class="flex flex-col bg-white px-[15px] py-[48px] text-main md:px-[36px]"
+  >
+    <GoogleMap
+      :api-key="config.public.GOOGLE_MAPS_API_KEY"
+      style="width: 100%; height: 500px"
+      :center="center"
+      :zoom="15"
+    >
+      <Marker :options="{ position: center }" />
+    </GoogleMap>
+  </div>
+</template>
