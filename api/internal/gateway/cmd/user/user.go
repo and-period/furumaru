030317502d18ce2238e8f1debefacad3824d0132
taba2424 <tadashi-112@hotmail.co.jp>
2024-10-11 12:48:39 +0900

    feat(store): 即時決済機能を有効に (#2436)

--- api/internal/gateway/cmd/user/user.go
+++ api/internal/gateway/cmd/user/user.go
@@ -61,12 +61,12 @@ type app struct {
 	KomojuClientID       string  `default:""               envconfig:"KOMOJU_CLIENT_ID"`
 	KomojuClientPassword string  `default:""               envconfig:"KOMOJU_CLIENT_PASSWORD"`
 	KomojuSecretName     string  `default:""               envconfig:"KOMOJU_SECRET_NAME"`
+	CheckoutAutoCaptured bool    `default:"false"          envconfig:"CHECKOUT_AUTO_CAPTURED"`
 	SlackAPIToken        string  `default:""               envconfig:"SLACK_API_TOKEN"`
 	SlackChannelID       string  `default:""               envconfig:"SLACK_CHANNEL_ID"`
 	SlackSecretName      string  `default:""               envconfig:"SLACK_SECRET_NAME"`
 	AminWebURL           string  `default:""               envconfig:"ADMIN_WEB_URL"`
 	UserWebURL           string  `default:""               envconfig:"USER_WEB_URL"`
-	CheckoutRedirectURL  string  `default:""               envconfig:"CHECKOUT_REDIRECT_URL"` // TODO: クライアント側修正が完了し次第削除
 }
 
 //nolint:revive
