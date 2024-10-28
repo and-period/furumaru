94a93e849730b729c9bd2d15e0a2f5083790ce8e
taba2424 <tadashi-112@hotmail.co.jp>
2024-10-28 13:15:46 +0900

    fix(messenger): fix small

--- api/internal/messenger/database/tidb/tidb.go
+++ api/internal/messenger/database/tidb/tidb.go
@@ -1,15 +1,9 @@
 package tidb
 
 import (
-	"context"
-	"errors"
-	"fmt"
-
 	"github.com/and-period/furumaru/api/internal/messenger/database"
 	"github.com/and-period/furumaru/api/internal/messenger/database/mysql"
 	apmysql "github.com/and-period/furumaru/api/pkg/mysql"
-	gmysql "github.com/go-sql-driver/mysql"
-	"gorm.io/gorm"
 )
 
 func NewDatabase(db *apmysql.Client) *database.Database {
