f8ae389ef0615e59828b106b665c32506bf8c28d
wf-yamaday <wf.yamaday.job@gmail.com>
2024-11-04 13:35:57 +0900

    fix: 体験紐づけ部分のUIを編集

--- web/admin/src/components/molecules/VideoLinkedExperienceItem.vue
+++ web/admin/src/components/molecules/VideoLinkedExperienceItem.vue
@@ -1,27 +1,21 @@
 <script setup lang="ts">
-import type { Experience } from '~/types/api'
+import type { Experience } from "~/types/api";
 
 interface Props {
-  item: Experience
+  item: Experience;
 }
 
-const props = defineProps<Props>()
+const props = defineProps<Props>();
 </script>
 
 <template>
-  <div class="d-flex align-center ga-4">
-    <v-responsive
-      :aspect-ratio="16 / 9"
-      class="border"
-      :width="250"
-    >
-      <video
-        :src="item.promotionVideoUrl"
-        controls
-        class="w-100"
-      />
-    </v-responsive>
-    <div class="flex-grow-1 d-flex flex-column ga-3">
+  <v-row>
+    <v-col cols="4">
+      <v-responsive :aspect-ratio="16 / 9" class="border" :width="250">
+        <video :src="item.promotionVideoUrl" controls class="w-100" />
+      </v-responsive>
+    </v-col>
+    <v-col cols="8" class="flex-grow-1 d-flex flex-column ga-3">
       <p class="text-subtitle-1">
         {{ item.title }}
       </p>
@@ -31,6 +25,6 @@ const props = defineProps<Props>()
       <div>
         {{ item.hostCity }}
       </div>
-    </div>
-  </div>
+    </v-col>
+  </v-row>
 </template>
