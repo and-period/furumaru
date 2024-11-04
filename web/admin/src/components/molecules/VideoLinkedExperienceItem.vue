ae8b27150c46c4e3bf84aa41c06ecfcf35c35709
wf-yamaday <wf.yamaday.job@gmail.com>
2024-11-04 21:59:09 +0900

    style: lintエラーを修正

--- web/admin/src/components/molecules/VideoLinkedExperienceItem.vue
+++ web/admin/src/components/molecules/VideoLinkedExperienceItem.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
-import type { Experience } from "~/types/api";
+import type { Experience } from '~/types/api'
 
 interface Props {
-  item: Experience;
+  item: Experience
 }
 
-const props = defineProps<Props>();
+const props = defineProps<Props>()
 </script>
 
 <template>
   <v-row>
     <v-col cols="4">
-      <v-responsive :aspect-ratio="16 / 9" class="border" :width="250">
-        <video :src="item.promotionVideoUrl" controls class="w-100" />
+      <v-responsive
+        :aspect-ratio="16 / 9"
+        class="border"
+        :width="250"
+      >
+        <video
+          :src="item.promotionVideoUrl"
+          controls
+          class="w-100"
+        />
       </v-responsive>
     </v-col>
-    <v-col cols="8" class="flex-grow-1 d-flex flex-column ga-3">
+    <v-col
+      cols="8"
+      class="flex-grow-1 d-flex flex-column ga-3"
+    >
       <p class="text-subtitle-1">
         {{ item.title }}
       </p>
