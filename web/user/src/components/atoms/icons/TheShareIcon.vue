3ecdf2b5dbb4693f2f5bf6f3d4fc64dd1ceb1699
濵田広大 <hamany0326@gmail.com>
2024-11-12 21:29:24 +0900

    Fix: fixed lint

--- web/user/src/components/atoms/icons/TheShareIcon.vue
+++ web/user/src/components/atoms/icons/TheShareIcon.vue
@@ -10,5 +10,23 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" class="max-w-[16px] max-y-[16px]" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-dasharray="28" stroke-dashoffset="28" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 6l2 -2c1 -1 3 -1 4 0l1 1c1 1 1 3 0 4l-5 5c-1 1 -3 1 -4 0M11 18l-2 2c-1 1 -3 1 -4 0l-1 -1c-1 -1 -1 -3 0 -4l5 -5c1 -1 3 -1 4 0"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="28;0"/></path></svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="max-w-[16px] max-y-[16px]"
+    viewBox="0 0 24 24"
+  ><path
+    fill="none"
+    stroke="currentColor"
+    stroke-dasharray="28"
+    stroke-dashoffset="28"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M13 6l2 -2c1 -1 3 -1 4 0l1 1c1 1 1 3 0 4l-5 5c-1 1 -3 1 -4 0M11 18l-2 2c-1 1 -3 1 -4 0l-1 -1c-1 -1 -1 -3 0 -4l5 -5c1 -1 3 -1 4 0"
+  ><animate
+    fill="freeze"
+    attributeName="stroke-dashoffset"
+    dur="0.6s"
+    values="28;0"
+  /></path></svg>
 </template>
