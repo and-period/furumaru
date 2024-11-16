504cbba8d0ca2fef2de7521d1c16068f997e4920
taba2424 <tadashi-112@hotmail.co.jp>
2024-11-17 00:30:54 +0900

    feat(store): スポット関連のビジネスロジック実装 (#2501)
    * feat(store): スポット関連のビジネスロジック実装
    
    * fix(store): テストコードの修正
    
    * fix(store): SpotテーブルのDDL修正
    
    * fix(store): テストコードの修正

--- api/internal/store/entity/spot_test.go
+++ api/internal/store/entity/spot_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSpot(t *testing.T) {
+func TestSpotByUser(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
@@ -26,11 +26,12 @@ func TestSpot(t *testing.T) {
 				Latitude:     35.6895,
 			},
 			expect: &Spot{
+				UserType:     SpotUserTypeUser,
 				UserID:       "user-id",
 				Name:         "東京タワー",
 				Description:  "おすすめの観光地です。",
 				ThumbnailURL: "https://example.com/image.jpg",
-				Approved:     false,
+				Approved:     true,
 				Longitude:    139.6917,
 				Latitude:     35.6895,
 			},
@@ -67,7 +68,83 @@ func TestSpot(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			actual, err := NewSpot(tt.params)
+			actual, err := NewSpotByUser(tt.params)
+			if err != nil {
+				assert.True(t, tt.hasErr, err)
+				return
+			}
+			assert.False(t, tt.hasErr)
+
+			actual.ID = "" // ignore
+			assert.Equal(t, tt.expect, actual)
+		})
+	}
+}
+
+func TestSpotByAdmin(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		params *SpotParams
+		expect *Spot
+		hasErr bool
+	}{
+		{
+			name: "success",
+			params: &SpotParams{
+				UserID:       "user-id",
+				Name:         "東京タワー",
+				Description:  "おすすめの観光地です。",
+				ThumbnailURL: "https://example.com/image.jpg",
+				Longitude:    139.6917,
+				Latitude:     35.6895,
+			},
+			expect: &Spot{
+				UserType:        SpotUserTypeAdmin,
+				UserID:          "user-id",
+				Name:            "東京タワー",
+				Description:     "おすすめの観光地です。",
+				ThumbnailURL:    "https://example.com/image.jpg",
+				Approved:        true,
+				ApprovedAdminID: "user-id",
+				Longitude:       139.6917,
+				Latitude:        35.6895,
+			},
+			hasErr: false,
+		},
+		{
+			name: "invalid longitude",
+			params: &SpotParams{
+				UserID:       "user-id",
+				Name:         "東京タワー",
+				Description:  "おすすめの観光地です。",
+				ThumbnailURL: "https://example.com/image.jpg",
+				Longitude:    200.0,
+				Latitude:     35.6895,
+			},
+			expect: nil,
+			hasErr: true,
+		},
+		{
+			name: "invalid latitude",
+			params: &SpotParams{
+				UserID:       "user-id",
+				Name:         "東京タワー",
+				Description:  "おすすめの観光地です。",
+				ThumbnailURL: "https://example.com/image.jpg",
+				Longitude:    139.6917,
+				Latitude:     100.0,
+			},
+			expect: nil,
+			hasErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			actual, err := NewSpotByAdmin(tt.params)
 			if err != nil {
 				assert.True(t, tt.hasErr, err)
 				return
