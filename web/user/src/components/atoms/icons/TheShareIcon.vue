695700a9a40151da51549636fb5c286b7d8b45be
koudai hamada <hamany0326@gmail.com>
2024-11-18 23:01:14 +0900

    Feat/sns share (#2493)
    * Add: added sns share x
    
    * Add: added link copy
    
    * Add: added icon
    
    * Fix: fixed lint
    
    * Fix: fixed lint
    
    * Add: added i18n

---
+++ web/user/src/components/atoms/icons/TheShareIcon.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+interface Props {
+  id: string
+  fill?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  fill: 'white',
+})
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="max-w-[16px] max-y-[16px]"
+    viewBox="0 0 24 24"
+  ><path
+    fill="none"
+    stroke="currentColor"
+    stroke-dasharray="28"
+    stroke-dashoffset="28"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M13 6l2 -2c1 -1 3 -1 4 0l1 1c1 1 1 3 0 4l-5 5c-1 1 -3 1 -4 0M11 18l-2 2c-1 1 -3 1 -4 0l-1 -1c-1 -1 -1 -3 0 -4l5 -5c1 -1 3 -1 4 0"
+  ><animate
+    fill="freeze"
+    attributeName="stroke-dashoffset"
+    dur="0.6s"
+    values="28;0"
+  /></path></svg>
+</template>
--- web/user/src/components/atoms/live/TheLiveDescription.vue
+++ web/user/src/components/atoms/live/TheLiveDescription.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { datetimeformatterFromUnixtime } from '~/lib/dayjs'
 import type { I18n } from '~/types/locales'
+import type { Snackbar } from '~/types/props'
 
 interface Props {
   title: string
@@ -16,6 +17,8 @@ interface Props {
 }
 
 const i18n = useI18n()
+
+const snackbarItems = ref<Snackbar[]>([])
 
 const dt = (str: keyof I18n['lives']['details']) => {
   return i18n.t(`lives.details.${str}`)
@@ -39,6 +42,24 @@ const handleCLickCoordinator = () => {
   emits('click:coordinator', props.coordinatorId)
 }
 
+const handleClickCopyButton = async () => {
+  snackbarItems.value.push({
+    text: i18n.t('lives.details.linkCopied'),
+    isShow: true,
+  })
+  await navigator.clipboard.writeText(window.location.href)
+}
+
+const handleClickXButton = () => {
+  const shareXUrl = `https://twitter.com/intent/tweet?text=${props.title}&url=${window.location.href}`
+  window.open(shareXUrl, '_blank')
+}
+
+const handleClickFacebookButton = () => {
+  const shareFacebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`
+  window.open(shareFacebookUrl, '_blank')
+}
+
 const showDetail = ref<boolean>(false)
 
 const handleClickShowDetailButton = () => {
@@ -47,31 +68,77 @@ const handleClickShowDetailButton = () => {
 </script>
 
 <template>
+  <template
+    v-for="(snackbarItem, i) in snackbarItems"
+    :key="i"
+  >
+    <the-snackbar
+      v-model:is-show="snackbarItem.isShow"
+      :text="snackbarItem.text"
+    />
+  </template>
   <div class="mt-2 px-4">
-    <div class="flex items-center gap-2">
-      <template v-if="isArchive">
-        <div
-          class="flex max-w-fit items-center justify-center rounded border-2 border-main px-2 font-bold text-main"
-        >
-          {{ dt('archivedStreamText') }}
-        </div>
-      </template>
-      <template v-else-if="isLiveStreaming">
-        <div
-          class="flex max-w-fit items-center justify-center rounded border-2 border-orange bg-orange px-2 font-bold text-white"
-        >
-          <div class="mr-2 pt-[2px]">
-            <the-live-icon />
+    <div class="md:flex block md:justify-between">
+      <div class="flex items-center gap-2">
+        <template v-if="isArchive">
+          <div
+            class="flex max-w-fit items-center justify-center rounded border-2 border-main px-2 font-bold text-main"
+          >
+            {{ dt('archivedStreamText') }}
           </div>
-          <div class="align-middle">
-            LIVE
+        </template>
+        <template v-else-if="isLiveStreaming">
+          <div
+            class="flex max-w-fit items-center justify-center rounded border-2 border-orange bg-orange px-2 font-bold text-white"
+          >
+            <div class="mr-2 pt-[2px]">
+              <the-live-icon />
+            </div>
+            <div class="align-middle">
+              LIVE
+            </div>
           </div>
-        </div>
-      </template>
+        </template>
 
-      <div class="text-[14px] tracking-[1.4px] after:content-['〜']">
-        {{ datetimeformatterFromUnixtime(startAt) }}
+        <div class="text-[14px] tracking-[1.4px] after:content-['〜']">
+          {{ datetimeformatterFromUnixtime(startAt) }}
+        </div>
       </div>
+      <the-dropdown-with-icon ref="dropdownRef">
+        <template #icon>
+          <div class="flex md:mt-0 mt-2 text-[14px] tracking-[1.4px] ">
+            <the-sns-share-icon />
+            <p class="ml-2">
+              SHARE
+            </p>
+          </div>
+        </template>
+        <template #content>
+          <div class="flex flex-col">
+            <button
+              class="px-4 py-2 text-left flex hover:bg-gray-200 items-center"
+              @click="handleClickCopyButton"
+            >
+              <the-share-icon class="mr-2" />
+              {{ dt('linkCopy') }}
+            </button>
+            <button
+              class="px-4 py-2 text-left flex hover:bg-gray-200 items-center"
+              @click="handleClickXButton"
+            >
+              <the-share-x-icon class="mr-2" />
+              X (Twitter)
+            </button>
+            <button
+              class="px-4 py-2 text-left flex hover:bg-gray-200 items-center"
+              @click="handleClickFacebookButton"
+            >
+              <the-share-facebook-icon class="mr-2" />
+              Facebook
+            </button>
+          </div>
+        </template>
+      </the-dropdown-with-icon>
     </div>
     <p class="mt-2 line-clamp-1 tracking-[1.6px]">
       {{ title }}
