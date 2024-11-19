f0399fff21cb898766bc2107917ae9b52e8cb795
sea-kai <sea_rr10_ch@outlook.com>
2024-11-20 01:43:36 +0900

    delete: 不要になったLambdaを削除

--- infra/serverless/functions/extractMediaStoreDomain/handler.js
+++
@@ -1,7 +0,0 @@
-exports.extractDomainName = async (event) => {
-    console.log(event);
-    const domainName = event.ContainerStatus.Container.Endpoint;
-    const domainNameWithoutHttp = domainName.replace('https://', '');
-    console.log(domainNameWithoutHttp);
-    return domainNameWithoutHttp;
-}
