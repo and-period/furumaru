bfa4fd3ddbdaf305bf53f08265befab5037834db
sea-kai <sea_rr10_ch@outlook.com>
2024-11-20 11:59:09 +0900

    chore: MediaStore廃止に合わせた構成変更に伴い、StepFunctionsを修正 (#2509)

--- infra/serverless/functions/calculateMP4StartTime/handler.js
+++
@@ -1,9 +0,0 @@
-const dayjs = require('dayjs');
-
-exports.calculateMP4StartTime = async (event) => {
-    console.log(event);
-    const startTime = event[0].ChannelInput.StartTime;
-    const fiveMinutesAgo = dayjs(startTime).subtract(5, 'minute').format('YYYY-MM-DDTHH:mm:ss.SSSZ');
-    console.log(fiveMinutesAgo);
-    return fiveMinutesAgo;
-}
