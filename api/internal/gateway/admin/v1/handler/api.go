454df34ef0ef5244718eb80e53029b9d54816e00
taba2424 <tadashi-112@hotmail.co.jp>
2024-11-24 11:57:30 +0900

    fix(api): 500エラー時に本番環境でもレスポンスを返すように (#2516)

--- api/internal/gateway/admin/v1/handler/api.go
+++ api/internal/gateway/admin/v1/handler/api.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -181,7 +180,6 @@ func (h *handler) Routes(rg *gin.RouterGroup) {
 func (h *handler) httpError(ctx *gin.Context, err error) {
 	res, code := util.NewErrorResponse(err)
 	h.reportError(ctx, err, res)
-	h.filterResponse(res)
 	ctx.JSON(code, res)
 	ctx.Abort()
 }
@@ -196,14 +194,6 @@ func (h *handler) unauthorized(ctx *gin.Context, err error) {
 
 func (h *handler) forbidden(ctx *gin.Context, err error) {
 	h.httpError(ctx, status.Error(codes.PermissionDenied, err.Error()))
-}
-
-func (h *handler) filterResponse(res *util.ErrorResponse) {
-	if res == nil || !strings.Contains(h.env, "prd") {
-		return
-	}
-	// 本番環境の場合、エラーメッセージは返却しない
-	res.Detail = ""
 }
 
 func (h *handler) reportError(ctx *gin.Context, err error, res *util.ErrorResponse) {
