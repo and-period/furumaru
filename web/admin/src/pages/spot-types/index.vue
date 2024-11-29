fb4cd5b82e5cbd13c5667865ba2e6dd875458c6f
taba2424 <tadashi-112@hotmail.co.jp>
2024-11-30 01:15:02 +0900

    feat(admin): スポット種別管理ページの実装

---
+++ web/admin/src/pages/spot-types/index.vue
@@ -0,0 +1,154 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import type { VDataTable } from 'vuetify/lib/components/index.mjs'
+
+import { useAlert, usePagination } from '~/lib/hooks'
+import { useAuthStore, useCommonStore, useSpotTypeStore } from '~/store'
+import type { CreateSpotTypeRequest, UpdateSpotTypeRequest } from '~/types/api'
+
+const commonStore = useCommonStore()
+const authStore = useAuthStore()
+const spotTypeStore = useSpotTypeStore()
+const pagination = usePagination()
+const { alertType, isShow, alertText, show } = useAlert('error')
+
+const { role } = storeToRefs(authStore)
+const { spotTypes, total } = storeToRefs(spotTypeStore)
+
+const loading = ref<boolean>(false)
+const newDialog = ref<boolean>(false)
+const editDialog = ref<boolean>(false)
+const deleteDialog = ref<boolean>(false)
+const newFormData = ref<CreateSpotTypeRequest>({
+  name: '',
+})
+const editFormData = ref<UpdateSpotTypeRequest>({
+  name: '',
+})
+
+const fetchState = useAsyncData(async (): Promise<void> => {
+  await fetchSpotTypes()
+})
+
+watch(pagination.itemsPerPage, (): void => {
+  fetchSpotTypes()
+})
+
+const fetchSpotTypes = async (): Promise<void> => {
+  try {
+    await spotTypeStore.fetchSpotTypes(pagination.itemsPerPage.value, pagination.offset.value)
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+}
+
+const isLoading = (): boolean => {
+  return fetchState?.pending?.value || loading.value
+}
+
+const handleUpdatePage = async (page: number): Promise<void> => {
+  pagination.updateCurrentPage(page)
+  await fetchSpotTypes()
+}
+
+const handleCreate = async (): Promise<void> => {
+  try {
+    loading.value = true
+    await spotTypeStore.createSpotType(newFormData.value)
+    commonStore.addSnackbar({
+      message: 'スポット種別を追加しました。',
+      color: 'info',
+    })
+    fetchState.refresh()
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+  finally {
+    newDialog.value = false
+    loading.value = false
+  }
+}
+
+const handleUpdate = async (spotTypeId: string): Promise<void> => {
+  try {
+    loading.value = true
+    await spotTypeStore.updateSpotType(spotTypeId, editFormData.value)
+    commonStore.addSnackbar({
+      message: 'スポット種別を更新しました。',
+      color: 'info',
+    })
+    fetchState.refresh()
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+  finally {
+    editDialog.value = false
+    loading.value = false
+  }
+}
+
+const handleDelete = async (spotTypeId: string): Promise<void> => {
+  try {
+    loading.value = true
+    await spotTypeStore.deleteSpotType(spotTypeId)
+    commonStore.addSnackbar({
+      message: 'スポット種別を削除しました。',
+      color: 'info',
+    })
+    fetchState.refresh()
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+  finally {
+    deleteDialog.value = false
+    loading.value = false
+  }
+}
+
+try {
+  await fetchState.execute()
+}
+catch (err) {
+  console.log('failed to setup', err)
+}
+</script>
+
+<template>
+  <templates-product-tag-list
+    v-model:new-form-data="newFormData"
+    v-model:edit-form-data="editFormData"
+    v-model:new-dialog="newDialog"
+    v-model:edit-dialog="editDialog"
+    v-model:delete-dialog="deleteDialog"
+    v-model:sort-by="sortBy"
+    :loading="isLoading()"
+    :role="role"
+    :is-alert="isShow"
+    :alert-type="alertType"
+    :alert-text="alertText"
+    :product-tags="spotTypes"
+    :table-items-per-page="pagination.itemsPerPage.value"
+    :table-items-total="total"
+    @click:update-page="handleUpdatePage"
+    @click:update-items-per-page="pagination.handleUpdateItemsPerPage"
+    @submit:create="handleCreate"
+    @submit:update="handleUpdate"
+    @submit:delete="handleDelete"
+  />
+</template>
---
+++ web/admin/src/store/spot-type.ts
@@ -0,0 +1,108 @@
+import { defineStore } from 'pinia'
+
+import { apiClient } from '~/plugins/api-client'
+import type { CreateSpotTypeRequest, SpotType, UpdateSpotTypeRequest } from '~/types/api'
+
+export const useSpotTypeStore = defineStore('spotType', {
+  state: () => ({
+    spotType: {} as SpotType,
+    spotTypes: [] as SpotType[],
+    total: 0,
+  }),
+
+  actions: {
+    /**
+     * スポット種別一覧を取得する非同期関数
+     * @param limit 取得上限数
+     * @param offset 取得開始位置
+     * @param orders ソートキー
+     */
+    async fetchSpotTypes(limit = 20, offset = 0): Promise<void> {
+      try {
+        const res = await apiClient.spotTypeApi().v1ListSpotTypes(limit, offset, '')
+        this.spotTypes = res.data.spotTypes
+        this.total = res.data.total
+      }
+      catch (err) {
+        return this.errorHandler(err)
+      }
+    },
+
+    /**
+     * スポット種別を検索する非同期関数
+     * @param name スポット種別名(あいまい検索)
+     * @param spotTypeIds stateの更新時に残しておく必要があるスポット種別情報
+     */
+    async searchSpotTypes(name = '', spotTypeIds: string[] = []): Promise<void> {
+      try {
+        const res = await apiClient.spotTypeApi().v1ListSpotTypes(undefined, undefined, name)
+        const spotTypes: SpotType[] = []
+        this.spotTypes.forEach((spotType: SpotType): void => {
+          if (!spotTypeIds.includes(spotType.id)) {
+            return
+          }
+          spotTypes.push(spotType)
+        })
+        res.data.spotTypes.forEach((spotType: SpotType): void => {
+          if (spotTypes.find((v): boolean => v.id === spotType.id)) {
+            return
+          }
+          spotTypes.push(spotType)
+        })
+        this.spotTypes = spotTypes
+        this.total = res.data.total
+      }
+      catch (err) {
+        return this.errorHandler(err)
+      }
+    },
+
+    /**
+     * スポット種別を新規登録する非同期関数
+     * @param payload
+     */
+    async createSpotType(payload: CreateSpotTypeRequest): Promise<void> {
+      try {
+        const res = await apiClient.spotTypeApi().v1CreateSpotType(payload)
+        this.spotTypes.unshift(res.data.spotType)
+      }
+      catch (err) {
+        return this.errorHandler(err, {
+          400: '必須項目が不足しているか、内容に誤りがあります。',
+          409: 'このスポット名はすでに登録されています。',
+        })
+      }
+    },
+
+    /**
+     * スポット種別を更新する非同期関数
+     * @param spotTypeId スポット種別ID
+     * @param payload
+     */
+    async updateSpotType(spotTypeId: string, payload: UpdateSpotTypeRequest): Promise<void> {
+      try {
+        await apiClient.spotTypeApi().v1UpdateSpotType(spotTypeId, payload)
+      }
+      catch (err) {
+        return this.errorHandler(err, {
+          400: '必須項目が不足しているか、内容に誤りがあります。',
+          404: 'このスポット種別は存在しません。',
+          409: 'このスポット種別名はすでに登録されています。',
+        })
+      }
+    },
+
+    /**
+     * スポット種別を削除する非同期関数
+     * @param spotTypeId スポット種別ID
+     */
+    async deleteSpotType(spotTypeId: string): Promise<void> {
+      try {
+        await apiClient.spotTypeApi().v1DeleteSpotType(spotTypeId)
+      }
+      catch (err) {
+        this.errorHandler(err, { 404: 'このスポット種別は存在しません。' })
+      }
+    },
+  },
+})
