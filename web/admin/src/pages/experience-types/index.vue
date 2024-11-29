4db3ac3d2011e30cd6557ca7829f9e1e7b411a40
taba2424 <tadashi-112@hotmail.co.jp>
2024-11-30 01:21:17 +0900

    feat(admin): 体験種別管理ページの実装 (#2533)
    * feat(admin): 体験種別管理ページの実装
    
    * fix(admin): fix for reviews

---
+++ web/admin/src/pages/experience-types/index.vue
@@ -0,0 +1,166 @@
+<script lang="ts" setup>
+import { useAlert, usePagination } from '~/lib/hooks'
+import { useAuthStore, useCommonStore, useExperienceTypeStore } from '~/store'
+
+const commonStore = useCommonStore()
+const authStore = useAuthStore()
+const experienceTypeStore = useExperienceTypeStore()
+const pagination = usePagination()
+const { alertType, isShow, alertText, show } = useAlert('error')
+
+const { role } = storeToRefs(authStore)
+const { experienceTypes, total } = storeToRefs(experienceTypeStore)
+
+const loading = ref<boolean>(false)
+const sortBy = ref<VDataTable['sortBy']>([])
+const newDialog = ref<boolean>(false)
+const editDialog = ref<boolean>(false)
+const deleteDialog = ref<boolean>(false)
+const newFormData = ref<CreateExperienceTypeRequest>({
+  name: '',
+})
+const editFormData = ref<UpdateExperienceTypeRequest>({
+  name: '',
+})
+
+const fetchState = useAsyncData(async (): Promise<void> => {
+  await fetchExperienceTypes()
+})
+
+watch(pagination.itemsPerPage, (): void => {
+  fetchExperienceTypes()
+})
+watch(sortBy, (): void => {
+  fetchState.refresh()
+})
+
+const fetchExperienceTypes = async (): Promise<void> => {
+  try {
+    const orders: string[] = sortBy.value.map((item: any) => {
+      switch (item.order) {
+        case 'asc':
+          return item.key
+        case 'desc':
+          return `-${item.key}`
+        default:
+          return item.order ? item.key : `-${item.key}`
+      }
+    }) || []
+
+    await experienceTypeStore.fetchExperienceTypes(pagination.itemsPerPage.value, pagination.offset.value, orders)
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+}
+
+const isLoading = (): boolean => {
+  return fetchState?.pending?.value || loading.value
+}
+
+const handleUpdatePage = async (page: number): Promise<void> => {
+  pagination.updateCurrentPage(page)
+  await fetchExperienceTypes()
+}
+
+const handleCreate = async (): Promise<void> => {
+  try {
+    loading.value = true
+    await experienceTypeStore.createExperienceType(newFormData.value)
+    commonStore.addSnackbar({
+      message: '体験種別を追加しました。',
+      color: 'info',
+    })
+    fetchState.refresh()
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+  finally {
+    newDialog.value = false
+    loading.value = false
+  }
+}
+
+const handleUpdate = async (experienceTypeId: string): Promise<void> => {
+  try {
+    loading.value = true
+    await experienceTypeStore.updateExperienceType(experienceTypeId, editFormData.value)
+    commonStore.addSnackbar({
+      message: '体験種別を更新しました。',
+      color: 'info',
+    })
+    fetchState.refresh()
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+  finally {
+    editDialog.value = false
+    loading.value = false
+  }
+}
+
+const handleDelete = async (experienceTypeId: string): Promise<void> => {
+  try {
+    loading.value = true
+    await experienceTypeStore.deleteExperienceType(experienceTypeId)
+    commonStore.addSnackbar({
+      message: '体験種別を削除しました。',
+      color: 'info',
+    })
+    fetchState.refresh()
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      show(err.message)
+    }
+    console.log(err)
+  }
+  finally {
+    deleteDialog.value = false
+    loading.value = false
+  }
+}
+
+try {
+  await fetchState.execute()
+}
+catch (err) {
+  console.log('failed to setup', err)
+}
+</script>
+
+<template>
+  <templates-experience-type-list
+    v-model:new-form-data="newFormData"
+    v-model:edit-form-data="editFormData"
+    v-model:new-dialog="newDialog"
+    v-model:edit-dialog="editDialog"
+    v-model:delete-dialog="deleteDialog"
+    v-model:sort-by="sortBy"
+    :loading="isLoading()"
+    :role="role"
+    :is-alert="isShow"
+    :alert-type="alertType"
+    :alert-text="alertText"
+    :experience-types="experienceTypes"
+    :table-items-per-page="pagination.itemsPerPage.value"
+    :table-items-total="total"
+    @click:update-page="handleUpdatePage"
+    @click:update-items-per-page="pagination.handleUpdateItemsPerPage"
+    @update:sort-by="fetchState.refresh"
+    @submit:create="handleCreate"
+    @submit:update="handleUpdate"
+    @submit:delete="handleDelete"
+  />
+</template>
--- web/admin/src/store/experience-type.ts
+++ web/admin/src/store/experience-type.ts
@@ -1,5 +1,6 @@
+import { ca } from 'vuetify/locale'
 import { apiClient } from '~/plugins/api-client'
-import type { ExperienceType } from '~/types/api'
+import type { CreateExperienceTypeRequest, ExperienceType } from '~/types/api'
 
 export const useExperienceTypeStore = defineStore('experienceType', {
   state: () => ({
@@ -26,6 +27,78 @@ export const useExperienceTypeStore = defineStore('experienceType', {
       }
       catch (err) {
         return this.errorHandler(err)
+      }
+    },
+
+    /**
+     * 体験カテゴリを検索する非同期関数
+     */
+    async searchExperienceTypes(name = '', experienceTypeIds: string[] = []): Promise<void> {
+      try {
+        const res = await apiClient.experienceTypeApi().v1ListExperienceTypes(undefined, undefined, name)
+        const experienceTypes: ExperienceType[] = []
+        this.experienceTypes.forEach((experienceType: ExperienceType): void => {
+          if (!experienceTypeIds.includes(experienceType.id)) {
+            return
+          }
+          experienceTypes.push(experienceType)
+        })
+        res.data.experienceTypes.forEach((experienceType: ExperienceType): void => {
+          if (experienceTypes.find((v): boolean => v.id === experienceType.id)) {
+            return
+          }
+          experienceTypes.push(experienceType)
+        })
+        this.experienceTypes = experienceTypes
+        this.totalItems = res.data.total
+      }
+      catch (err) {
+        return this.errorHandler(err)
+      }
+    },
+
+    /**
+     * 体験カテゴリを新規登録する非同期関数
+     */
+    async createExperienceType(params: CreateExperienceTypeRequest): Promise<void> {
+      try {
+        await apiClient.experienceTypeApi().v1CreateExperienceType(params)
+      }
+      catch (err) {
+        return this.errorHandler(err, {
+          400: '必須項目が不足しているか、内容に誤りがあります。',
+          409: 'この体験カテゴリ名はすでに登録されています。',
+        })
+      }
+    },
+
+    /**
+     * 体験カテゴリを更新する非同期関数
+     */
+    async updateExperienceType(experienceTypeId: string, payload: CreateExperienceTypeRequest): Promise<void> {
+      try {
+        await apiClient.experienceTypeApi().v1UpdateExperienceType(experienceTypeId, payload)
+      }
+      catch (err) {
+        return this.errorHandler(err, {
+          400: '必須項目が不足しているか、内容に誤りがあります。',
+          404: 'この体験カテゴリは存在しません。',
+          409: 'この体験カテゴリ名はすでに登録されています。',
+        })
+      }
+    },
+
+    /**
+     * 体験カテゴリを削除する非同期関数
+     */
+    async deleteExperienceType(experienceTypeId: string): Promise<void> {
+      try {
+        await apiClient.experienceTypeApi().v1DeleteExperienceType(experienceTypeId)
+      }
+      catch (err) {
+        return this.errorHandler(err, {
+          404: 'この体験カテゴリは存在しません。',
+        })
       }
     },
   },
