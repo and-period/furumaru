9b54dc6a46190035d500bf210869d9854dccad05
wf-yamaday <wf.yamaday.job@gmail.com>
2024-12-12 23:33:32 +0900

    wip: ダミーデータの追加

--- web/user/src/pages/experiences/index.vue
+++ web/user/src/pages/experiences/index.vue
@@ -20,32 +20,72 @@ const items = [
     description: '酒蔵見学＆どぶろく体験ツアー',
     imgSrc: 'https://assets.furumaru.and-period.co.jp/products/media/image/gsztHcK7CvWhDyhYRvpTT4.jpg',
   },
+  {
+    id: 3,
+    position: { lat: 35.62539905972506, lng: 139.5175404502735 },
+    title: 'よみうりランド',
+    description: '遊園地',
+    imgSrc: 'https://lh5.googleusercontent.com/p/AF1QipMcu3Mp8owZq5nbe2FEAtE21wg9T7LA21tjbwgg=w426-h240-k-no',
+  },
 ]
+
+const renderer = ref<undefined | { render: (obj: { count: number, position: any }) => any }>(undefined)
+
+onMounted(() => {
+  const svg = window.btoa(`
+  <svg fill="#604C3F" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 240 240">
+    <circle cx="120" cy="120" opacity=".8" r="70" />
+  </svg>`)
+
+  renderer.value = {
+    render: ({ count, position }: { count: number, position: any }) =>
+      new google.maps.Marker({
+        label: {
+          text: String(count),
+          color: 'white',
+        },
+        position,
+        icon: {
+          url: `data:image/svg+xml;base64,${svg}`,
+          scaledSize: new google.maps.Size(75, 75),
+        },
+        // adjust zIndex to be above other markers
+        zIndex: Number(google.maps.Marker.MAX_ZINDEX) + count,
+      }),
+    // new google.maps.marker.AdvancedMarkerElement({
+    //   title: String(count),
+    //   position,
+    // },
+    // ),
+  }
+})
 </script>
 
 <template>
   <div class="bg-white px-[15px] py-[48px] text-main md:px-[36px]">
     <div class="container mx-auto">
-      <GoogleMap
-        :api-key="config.public.GOOGLE_MAPS_API_KEY"
-        style="width: 100%; height: 700px"
-        :center="center"
-        :zoom="10"
-      >
-        <MarkerCluster>
-          <template
-            v-for="item in items"
-            :key="item.id"
-          >
-            <the-experience-marker
-              :position="item.position"
-              :title="item.title"
-              :description="item.description"
-              :img-src="item.imgSrc"
-            />
-          </template>
-        </MarkerCluster>
-      </GoogleMap>
+      <ClientOnly>
+        <GoogleMap
+          :api-key="config.public.GOOGLE_MAPS_API_KEY"
+          style="width: 100%; height: 700px"
+          :center="center"
+          :zoom="10"
+        >
+          <MarkerCluster :options="{ renderer: renderer }">
+            <template
+              v-for="item in items"
+              :key="item.id"
+            >
+              <the-experience-marker
+                :position="item.position"
+                :title="item.title"
+                :description="item.description"
+                :img-src="item.imgSrc"
+              />
+            </template>
+          </MarkerCluster>
+        </GoogleMap>
+      </ClientOnly>
     </div>
   </div>
 </template>
