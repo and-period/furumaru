cebb7816e0529be83200c3ac2ddc49e9bf448d9e
taba2424 <tadashi-112@hotmail.co.jp>
2024-12-21 02:26:54 +0900

    fix(store): fix next token

--- api/internal/media/database/mysql/broadcast_comment.go
+++ api/internal/media/database/mysql/broadcast_comment.go
@@ -51,9 +51,9 @@ func (c *broadcastComment) List(
 		if err != nil {
 			return nil, "", fmt.Errorf("database: failed to parse next token: %s: %w", err.Error(), database.ErrInvalidArgument)
 		}
-		stmt = stmt.Where("created_at <= ?", time.Unix(0, nsec))
+		stmt = stmt.Where("created_at >= ?", time.Unix(0, nsec))
 	}
-	stmt = stmt.Order("created_at DESC")
+	stmt = stmt.Order("created_at ASC")
 
 	if err := stmt.Find(&comments).Error; err != nil {
 		return nil, "", dbError(err)
