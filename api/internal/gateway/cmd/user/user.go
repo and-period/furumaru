794f2a77863e44cddcd5018350833526f6be5ee4
taba2424 <tadashi-112@hotmail.co.jp>
2024-12-22 22:29:44 +0900

    fix(gateway): user-gatewayでGoogle Map APIが利用できるように (#2575)

--- api/internal/gateway/cmd/user/user.go
+++ api/internal/gateway/cmd/user/user.go
@@ -22,49 +22,51 @@ import (
 
 type app struct {
 	*cobra.Command
-	debugMode            bool
-	logger               *zap.Logger
-	waitGroup            *sync.WaitGroup
-	slack                slack.Client
-	newRelic             *newrelic.Application
-	v1                   v1.Handler
-	AppName              string  `default:"user-gateway"   envconfig:"APP_NAME"`
-	Environment          string  `default:"none"           envconfig:"ENV"`
-	Port                 int64   `default:"8080"           envconfig:"PORT"`
-	MetricsPort          int64   `default:"9090"           envconfig:"METRICS_PORT"`
-	ShutdownDelaySec     int64   `default:"20"             envconfig:"SHUTDOWN_DELAY_SEC"`
-	LogPath              string  `default:""               envconfig:"LOG_PATH"`
-	LogLevel             string  `default:"info"           envconfig:"LOG_LEVEL"`
-	TraceSampleRate      float64 `default:"0.0"            envconfig:"TRACE_SAMPLE_RATE"`
-	ProfileSampleRate    float64 `default:"0.0"            envconfig:"PROFILE_SAMPLE_RATE"`
-	DBTimeZone           string  `default:"Asia/Tokyo"     envconfig:"DB_TIMEZONE"`
-	TiDBHost             string  `default:"127.0.0.1"      envconfig:"TIDB_HOST"`
-	TiDBPort             string  `default:"4000"           envconfig:"TIDB_PORT"`
-	TiDBUsername         string  `default:""               envconfig:"TIDB_USERNAME"`
-	TiDBPassword         string  `default:""               envconfig:"TIDB_PASSWORD"`
-	TiDBSecretName       string  `default:""               envconfig:"TIDB_SECRET_NAME"`
-	GinMode              string  `default:"release"        envconfig:"GIN_MODE"`
-	NewRelicLicense      string  `default:""               envconfig:"NEW_RELIC_LICENSE"`
-	NewRelicSecretName   string  `default:""               envconfig:"NEW_RELIC_SECRET_NAME"`
-	SentryDsn            string  `default:""               envconfig:"SENTRY_DSN"`
-	SentrySecretName     string  `default:""               envconfig:"SENTRY_SECRET_NAME"`
-	AWSRegion            string  `default:"ap-northeast-1" envconfig:"AWS_REGION"`
-	S3Bucket             string  `default:""               envconfig:"S3_BUCKET"`
-	S3TmpBucket          string  `default:""               envconfig:"S3_TMP_BUCKET"`
-	CognitoUserPoolID    string  `default:""               envconfig:"COGNITO_USER_POOL_ID"`
-	CognitoUserClientID  string  `default:""               envconfig:"COGNITO_USER_CLIENT_ID"`
-	SQSQueueURL          string  `default:""               envconfig:"SQS_QUEUE_URL"`
-	SQSMockEnabled       bool    `default:"false"          envconfig:"SQS_MOCK_ENABLED"`
-	KomojuHost           string  `default:""               envconfig:"KOMOJU_HOST"`
-	KomojuClientID       string  `default:""               envconfig:"KOMOJU_CLIENT_ID"`
-	KomojuClientPassword string  `default:""               envconfig:"KOMOJU_CLIENT_PASSWORD"`
-	KomojuSecretName     string  `default:""               envconfig:"KOMOJU_SECRET_NAME"`
-	CheckoutAutoCaptured bool    `default:"false"          envconfig:"CHECKOUT_AUTO_CAPTURED"`
-	SlackAPIToken        string  `default:""               envconfig:"SLACK_API_TOKEN"`
-	SlackChannelID       string  `default:""               envconfig:"SLACK_CHANNEL_ID"`
-	SlackSecretName      string  `default:""               envconfig:"SLACK_SECRET_NAME"`
-	AminWebURL           string  `default:""               envconfig:"ADMIN_WEB_URL"`
-	UserWebURL           string  `default:""               envconfig:"USER_WEB_URL"`
+	debugMode                bool
+	logger                   *zap.Logger
+	waitGroup                *sync.WaitGroup
+	slack                    slack.Client
+	newRelic                 *newrelic.Application
+	v1                       v1.Handler
+	AppName                  string  `default:"user-gateway"   envconfig:"APP_NAME"`
+	Environment              string  `default:"none"           envconfig:"ENV"`
+	Port                     int64   `default:"8080"           envconfig:"PORT"`
+	MetricsPort              int64   `default:"9090"           envconfig:"METRICS_PORT"`
+	ShutdownDelaySec         int64   `default:"20"             envconfig:"SHUTDOWN_DELAY_SEC"`
+	LogPath                  string  `default:""               envconfig:"LOG_PATH"`
+	LogLevel                 string  `default:"info"           envconfig:"LOG_LEVEL"`
+	TraceSampleRate          float64 `default:"0.0"            envconfig:"TRACE_SAMPLE_RATE"`
+	ProfileSampleRate        float64 `default:"0.0"            envconfig:"PROFILE_SAMPLE_RATE"`
+	DBTimeZone               string  `default:"Asia/Tokyo"     envconfig:"DB_TIMEZONE"`
+	TiDBHost                 string  `default:"127.0.0.1"      envconfig:"TIDB_HOST"`
+	TiDBPort                 string  `default:"4000"           envconfig:"TIDB_PORT"`
+	TiDBUsername             string  `default:""               envconfig:"TIDB_USERNAME"`
+	TiDBPassword             string  `default:""               envconfig:"TIDB_PASSWORD"`
+	TiDBSecretName           string  `default:""               envconfig:"TIDB_SECRET_NAME"`
+	GinMode                  string  `default:"release"        envconfig:"GIN_MODE"`
+	NewRelicLicense          string  `default:""               envconfig:"NEW_RELIC_LICENSE"`
+	NewRelicSecretName       string  `default:""               envconfig:"NEW_RELIC_SECRET_NAME"`
+	SentryDsn                string  `default:""               envconfig:"SENTRY_DSN"`
+	SentrySecretName         string  `default:""               envconfig:"SENTRY_SECRET_NAME"`
+	AWSRegion                string  `default:"ap-northeast-1" envconfig:"AWS_REGION"`
+	S3Bucket                 string  `default:""               envconfig:"S3_BUCKET"`
+	S3TmpBucket              string  `default:""               envconfig:"S3_TMP_BUCKET"`
+	CognitoUserPoolID        string  `default:""               envconfig:"COGNITO_USER_POOL_ID"`
+	CognitoUserClientID      string  `default:""               envconfig:"COGNITO_USER_CLIENT_ID"`
+	SQSQueueURL              string  `default:""               envconfig:"SQS_QUEUE_URL"`
+	SQSMockEnabled           bool    `default:"false"          envconfig:"SQS_MOCK_ENABLED"`
+	KomojuHost               string  `default:""               envconfig:"KOMOJU_HOST"`
+	KomojuClientID           string  `default:""               envconfig:"KOMOJU_CLIENT_ID"`
+	KomojuClientPassword     string  `default:""               envconfig:"KOMOJU_CLIENT_PASSWORD"`
+	KomojuSecretName         string  `default:""               envconfig:"KOMOJU_SECRET_NAME"`
+	GoogleSecretName         string  `default:""               envconfig:"GOOGLE_SECRET_NAME"`
+	GoogleMapsPlatformAPIKey string  `default:""               envconfig:"GOOGLE_MAPS_PLATFORM_API_KEY"`
+	CheckoutAutoCaptured     bool    `default:"false"          envconfig:"CHECKOUT_AUTO_CAPTURED"`
+	SlackAPIToken            string  `default:""               envconfig:"SLACK_API_TOKEN"`
+	SlackChannelID           string  `default:""               envconfig:"SLACK_CHANNEL_ID"`
+	SlackSecretName          string  `default:""               envconfig:"SLACK_SECRET_NAME"`
+	AminWebURL               string  `default:""               envconfig:"ADMIN_WEB_URL"`
+	UserWebURL               string  `default:""               envconfig:"USER_WEB_URL"`
 }
 
 //nolint:revive
