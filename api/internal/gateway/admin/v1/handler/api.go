3775c9f10a7224185e8da4e2e00c2e9a6767c82a
taba2424 <tadashi-112@hotmail.co.jp>
2025-01-13 19:03:04 +0900

    feat(gateway): rbacのデバッグログを追加

--- api/internal/gateway/admin/v1/handler/api.go
+++ api/internal/gateway/admin/v1/handler/api.go
@@ -244,6 +244,7 @@ func (h *handler) syncEnforcer(ctx context.Context) error {
 	h.syncMutex.Lock()
 	h.enforcer = enforcer
 	h.syncMutex.Unlock()
+	h.logger.Debug("Synced enforcer", zap.String("model", rbacModel), zap.String("policy", rbacPolicy))
 	return nil
 }
 
@@ -353,6 +354,7 @@ func (h *handler) enforce(ctx *gin.Context, admin *uentity.AdminAuth) error {
 			return nil
 		}
 	}
+	h.logger.Debug("Enforce failed", zap.String("adminId", admin.AdminID), zap.Strings("groupIds", admin.GroupIDs))
 	return fmt.Errorf("handler: you don't have the correct permissions: %w", exception.ErrForbidden)
 }
 
