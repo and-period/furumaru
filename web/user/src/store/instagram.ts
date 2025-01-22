3a3c354163b3a093172f52668c0c2abbaaf1b111
sea-kai <sea_rr10_ch@outlook.com>
2025-01-23 03:02:17 +0900

    feat: Appreview用の仮取得処理

--- web/user/src/store/instagram.ts
+++ web/user/src/store/instagram.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import type { InstagramOEmbed } from '~/types/api'
 
 export const useInstagramStore = defineStore('instagram', {
@@ -10,11 +11,24 @@ export const useInstagramStore = defineStore('instagram', {
 
   actions: {
     async getInstagramOEmbed() {
-      const response: InstagramOEmbed
-        = await this.instagramApiClient().getInstagramOEmbedContent('https://www.instagram.com/p/DBwRdmPvSy7/?igsh=MWxsY291djNtcjQ4OA==')
-
-      this.instagramOEmbed = response
-      this.OEmbedHTML = response.html
+      console.log('getInstagramOEmbed')
+      const url = 'https://graph.facebook.com/v22.0/instagram_oembed'
+      const postUrl = 'https://www.instagram.com/p/DBwRdmPvSy7/?igsh=MWxsY291djNtcjQ4OA=='
+      const runtimeConfig = useRuntimeConfig()
+      try {
+        const response = await axios.get(url, {
+          params: {
+            url: postUrl,
+            access_token: runtimeConfig.public.INSTAGRAM_ACCESS_TOKEN,
+            omitscript: true,
+          },
+        })
+        return response.data
+      }
+      catch (e) {
+        console.error(e)
+      }
+      return {} as InstagramOEmbed
     },
   },
 
