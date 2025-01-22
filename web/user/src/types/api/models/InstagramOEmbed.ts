10c35db8bf211390baa60b7206b6eea17bf4f9fa
sea-kai <sea_rr10_ch@outlook.com>
2025-01-23 01:49:52 +0900

    feat:  instagramの仮APIの型定義追加

---
+++ web/user/src/types/api/models/InstagramOEmbed.ts
@@ -0,0 +1,11 @@
+export interface InstagramOEmbed {
+  author_name: string | undefined;
+  html: string,
+  version: number | undefined;
+  provider_name: string | undefined;
+  provider_url: string | undefined;
+  type: string | undefined,
+  width: number | undefined,
+  thumbnail_width: number | undefined,
+  thumbnail_height: number | undefined,
+}
--- web/user/src/types/api/models/index.ts
+++ web/user/src/types/api/models/index.ts
@@ -61,6 +61,7 @@ export * from './GuestCheckoutProductRequest';
 export * from './GuestCheckoutResponse';
 export * from './GuestCheckoutStateResponse';
 export * from './GuestPreCheckoutExperienceResponse';
+export * from './InstagramOEmbed';
 export * from './Live';
 export * from './LiveComment';
 export * from './LiveCommentsResponse';
