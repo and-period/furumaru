f289112e926010a25e62eb9ccf00cd36c83e7d43
taba2424 <tadashi-112@hotmail.co.jp>
2025-01-23 12:59:16 +0900

    fix(gateway): 決済種別を追加 (#2681)

--- api/internal/gateway/admin/v1/service/order_payment_test.go
+++ api/internal/gateway/admin/v1/service/order_payment_test.go
@@ -63,6 +63,16 @@ func TestPaymentMethodType(t *testing.T) {
 			expect:     PaymentMethodTypeAUPay,
 		},
 		{
+			name:       "paidy",
+			methodType: entity.PaymentMethodTypePaidy,
+			expect:     PaymentMethodTypePaidy,
+		},
+		{
+			name:       "pay easy",
+			methodType: entity.PaymentMethodTypePayEasy,
+			expect:     PaymentMethodTypePayEasy,
+		},
+		{
 			name:       "none",
 			methodType: entity.PaymentMethodTypeNone,
 			expect:     PaymentMethodTypeNone,
@@ -133,6 +143,16 @@ func TestPaymentMethodType_StoreEntity(t *testing.T) {
 			name:       "au pay",
 			methodType: PaymentMethodTypeAUPay,
 			expect:     entity.PaymentMethodTypeAUPay,
+		},
+		{
+			name:       "paidy",
+			methodType: PaymentMethodTypePaidy,
+			expect:     entity.PaymentMethodTypePaidy,
+		},
+		{
+			name:       "pay easy",
+			methodType: PaymentMethodTypePayEasy,
+			expect:     entity.PaymentMethodTypePayEasy,
 		},
 		{
 			name:       "none",
