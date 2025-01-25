bdcc390986f8747429862f6f7b16d5b5f00dd6c5
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2025-01-26 08:59:11 +0900

    fix: sentryの設定を環境変数が設定されている場合のみにロードするように修正

--- web/admin/nuxt.config.ts
+++ web/admin/nuxt.config.ts
@@ -108,7 +108,8 @@ export default defineNuxtConfig({
       sourcemap: true,
     },
     plugins: [
-      sentryVitePlugin({
+      process.env.SENTRY_AUTH_TOKEN
+      && sentryVitePlugin({
         org: process.env.SENTRY_ORGANIZATION,
         project: process.env.SENTRY_PROJECT,
         authToken: process.env.SENTRY_AUTH_TOKEN,
