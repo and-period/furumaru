4f9640cfba830765e25c9c5d5829d7e013fea541
sea-kai <sea_rr10_ch@outlook.com>
2025-03-06 03:41:08 +0900

    fix: 型定義と命名変更

---
+++ web/user/src/types/api/models/InstagramPost.ts
@@ -0,0 +1,10 @@
+export interface InstagramPost {
+  caption : string | undefined;
+  comments_count: number | undefined;
+  id: string | undefined;
+  like_count: number | undefined,
+  media_type: string | undefined,
+  media_url: string | undefined;
+  permalink: string | undefined;
+  timestamp: string | undefined;
+}
--- web/user/src/types/api/models/index.ts
+++ web/user/src/types/api/models/index.ts
@@ -61,7 +61,7 @@ export * from './GuestCheckoutProductRequest';
 export * from './GuestCheckoutResponse';
 export * from './GuestCheckoutStateResponse';
 export * from './GuestPreCheckoutExperienceResponse';
-export * from './InstagramOEmbed';
+export * from './InstagramPost';
 export * from './Live';
 export * from './LiveComment';
 export * from './LiveCommentsResponse';
