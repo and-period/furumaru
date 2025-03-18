0145da5b65f4512782dbcdf494fd96699320e34d
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2025-03-18 22:47:13 +0900

    レビューの星表示の修正 (#2766)

--- web/user/src/components/atoms/review/TheRatingStar.vue
+++ web/user/src/components/atoms/review/TheRatingStar.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 interface Props {
   rate: number
+  id?: string
 }
 
-defineProps<Props>()
+withDefaults(defineProps<Props>(), {
+  id: Math.random().toString(36).substring(2, 15),
+})
 
 /**
  * 1～5の星（i）に対して、どのくらい塗りつぶすかを 0～1 で返す関数
@@ -28,7 +31,7 @@ const getStarFill = (rate: number, i: number): number => {
         <!-- グラデーション定義 -->
         <defs>
           <linearGradient
-            :id="'starGradient' + i"
+            :id="`${id}starGradient${i}`"
             x1="0%"
             y1="0%"
             x2="100%"
@@ -50,7 +53,7 @@ const getStarFill = (rate: number, i: number): number => {
         </defs>
         <!-- パス全体に上記のグラデーションを適用する -->
         <path
-          :fill="'url(#starGradient' + i + ')'"
+          :fill="`url(#${id}starGradient${i})`"
           d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
         />
       </svg>
