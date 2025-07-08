3670c6037aa8c3d34c6dce5feb924ef14c52bec9
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2025-07-08 23:45:29 +0900

    LINEの友達追加のバナー追加 (#2872)

--- web/user/src/layouts/default.vue
+++ web/user/src/layouts/default.vue
@@ -144,6 +144,12 @@ const onScroll = () => {
   }
 }
 
+const showBanner = ref<boolean>(true)
+
+const handleClickCloseBanner = () => {
+  showBanner.value = false
+}
+
 const handleClickBuyButton = () => {
   router.push('/purchase')
 }
@@ -216,6 +222,40 @@ onUnmounted(() => {
     <main class="grow overflow-hidden">
       <div class="mx-auto pb-16">
         <slot />
+        <div
+          v-if="showBanner"
+          class="fixed bottom-10 size-10 md:left-8 left-4 bg-white border-line border-2 md:p-3 p-2 md:w-[170px] md:h-[170px] w-[120px] h-[120px] z-20 rounded-xl flex flex-col"
+        >
+          <div class="text-right text-line w-full h-5">
+            <button @click="handleClickCloseBanner">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="md:w-5 md:h-5 w-4 h-4"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <a
+            class="flex flex-col items-center md:gap-4 gap-2 grow justify-center"
+            href="https://lin.ee/49SOeUC"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <the-line-icon />
+            <p class="text-xs md:text-[16px] md:leading-[24px]">
+              友達追加でお得な情報をGET！
+            </p>
+          </a>
+        </div>
       </div>
     </main>
     <the-app-footer :menu-items="footerMenuList" />
