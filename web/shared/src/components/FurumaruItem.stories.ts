80dbc9048823aa91f7aa6f64801b34ad7f4d00ca
wf-yamaday <wf.yamaday.job@gmail.com>
2025-08-03 01:24:31 +0900

    feat: コンポーネントの実装

--- web/shared/src/components/FurumaruItem.stories.ts
+++ web/shared/src/components/FurumaruItem.stories.ts
@@ -16,5 +16,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
 	args: {
 		name: 'Furumaru',
+		price: 3000,
 	},
 }
--- web/shared/src/components/FurumaruItem.vue
+++ web/shared/src/components/FurumaruItem.vue
@@ -1,24 +1,63 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
 interface Props {
 	name: string;
+	price: number
+	addToCartButtonText?: string;
+	selectLabelText?: string
 }
 
-defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+	addToCartButtonText: 'カゴに入れる',
+	selectLabelText: '数量'
+});
+
+const quantity = ref<number>(1)
+
+const priceString = computed<string>(() => {
+  return new Intl.NumberFormat('ja-JP', {
+    style: 'currency',
+    currency: 'JPY',
+  }).format(props.price)
+})
+
+const stokeValues = computed<number>(() => {
+	return 10
+})
 </script>
 
 <template>
-	<div class="flex flex-col">
-		<p>
-			{{ name }}
-		</p>
-		<label for="select">数量</label>
-		<select id="select">
-			<option value="option1">Option 1</option>
-			<option value="option2">Option 2</option>
-			<option value="option3">Option 3</option>
-		</select>
-		<button class="">
-			カゴに入れる
-		</button>
-	</div>
+  <div class="flex flex-col text-main w-full font-semibold gap-2">
+    <p>
+      {{ name }}
+    </p>
+
+    <p>
+      {{ priceString }}
+    </p>
+
+    <div class="inline-flex gap-2 text-xs items-center">
+      <div class="inline-flex gap-1 items-center">
+        <label for="select">{{ selectLabelText }}</label>
+        <select
+          id="select"
+          v-model="quantity"
+          class="border border-main py-1 pl-1"
+        >
+          <template
+            v-for="n in stokeValues"
+            :key="n"
+          >
+            <option :value="n">
+              {{ n }}
+            </option>
+          </template>
+        </select>
+      </div>
+      <button class="bg-orange text-white py-1 px-2">
+        {{ addToCartButtonText }}
+      </button>
+    </div>
+  </div>
 </template>
