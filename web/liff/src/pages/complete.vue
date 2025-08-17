180fb7cf9140a3d3ec9c3001984be35a04b210d5
濵田広大 <hamany0326@gmail.com>
2025-08-17 23:34:35 +0900

    Add: added purchase complete page

---
+++ web/liff/src/pages/complete.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col items-center justify-center bg-white">
+      <img src="/complete.svg" alt="完了" class="w-40 h-40 mt-8" />
+      <p class=" mt-8 text-base text-main">ご注文ありがとうございます！</p>
+      <p class=" mt-2 text-base text-main">ご購入手続きが完了しました。</p>
+      <p class=" mt-8 text-sm text-main">お客様の注文番号は「111111111」です</p>
+    </div>
+    <div class="px-4">
+      <p class="mt-8 text-sm text-main">
+        ご注文内容の詳細は、注文履歴からご確認いただけます。
+      </p>
+      <p class="mt-2 text-sm text-main">
+        ご不明点がございましたら、LINEトーク画面からお問い合わせください。
+      </p>
+    </div>
+  </div>
+</template>
