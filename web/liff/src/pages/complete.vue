ff6d942844afbba13f11a67a727e87841c9f66c9
濵田広大 <hamany0326@gmail.com>
2025-08-18 00:01:58 +0900

    Fix: fixed lint

--- web/liff/src/pages/complete.vue
+++ web/liff/src/pages/complete.vue
@@ -4,10 +4,20 @@
 <template>
   <div>
     <div class="flex flex-col items-center justify-center bg-white">
-      <img src="/complete.svg" alt="完了" class="w-40 h-40 mt-8" />
-      <p class=" mt-8 text-base text-main">ご注文ありがとうございます！</p>
-      <p class=" mt-2 text-base text-main">ご購入手続きが完了しました。</p>
-      <p class=" mt-8 text-sm text-main">お客様の注文番号は「111111111」です</p>
+      <img
+        src="/complete.svg"
+        alt="完了"
+        class="w-40 h-40 mt-8"
+      >
+      <p class=" mt-8 text-base text-main">
+        ご注文ありがとうございます！
+      </p>
+      <p class=" mt-2 text-base text-main">
+        ご購入手続きが完了しました。
+      </p>
+      <p class=" mt-8 text-sm text-main">
+        お客様の注文番号は「111111111」です
+      </p>
     </div>
     <div class="px-4">
       <p class="mt-8 text-sm text-main">
