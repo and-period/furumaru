90aedd9aacd082442bd8bb4263e355fd27de13e8
Yuki Yamada <e155417k@st.u-gakugei.ac.jp>
2025-10-09 16:34:22 +0900

    [web/user] `/api`が機能しないのを修正 (#3131)

--- web/user/nuxt.config.ts
+++ web/user/nuxt.config.ts
@@ -3,6 +3,7 @@ import { sentryVitePlugin } from '@sentry/vite-plugin'
 export default defineNuxtConfig({
   ssr: true,
   srcDir: 'src',
+  serverDir: 'src/server',
   app: {
     head: {
       titleTemplate: '%s - 産地直送のお取り寄せ通販【ふるマル】',
