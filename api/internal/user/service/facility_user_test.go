3939cf7fd381ffd757f3735a2d316a670757202a
taba2424 <tadashi-112@hotmail.co.jp>
2025-11-04 23:27:20 +0900

    fix: 施設ユーザー更新APIのバリデーション修正

--- api/internal/user/service/facility_user_test.go
+++ api/internal/user/service/facility_user_test.go
@@ -13,117 +13,6 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
-func TestCreateFacilityUser(t *testing.T) {
-	t.Parallel()
-
-	now := jst.Date(2025, 8, 26, 12, 0, 0, 0)
-	lastCheckInAt := jst.Date(2025, 8, 27, 12, 0, 0, 0)
-
-	tests := []struct {
-		name      string
-		setup     func(ctx context.Context, mocks *mocks)
-		input     *user.CreateFacilityUserInput
-		expectErr error
-	}{
-		{
-			name: "success",
-			setup: func(ctx context.Context, mocks *mocks) {
-				expectUser := &entity.User{
-					Type:       entity.UserTypeFacilityUser,
-					Registered: false,
-					FacilityUser: entity.FacilityUser{
-						ExternalID:    "external-id",
-						ProducerID:    "producer-id",
-						Lastname:      "田中",
-						Firstname:     "太郎",
-						LastnameKana:  "たなか",
-						FirstnameKana: "たろう",
-						ProviderType:  entity.UserAuthProviderTypeLINE,
-						Email:         "test@example.com",
-						PhoneNumber:   "+819012345678",
-						LastCheckInAt: lastCheckInAt,
-					},
-				}
-				mocks.db.FacilityUser.EXPECT().
-					Create(ctx, gomock.Any()).
-					DoAndReturn(func(_ context.Context, user *entity.User) error {
-						expectUser.ID = user.ID
-						expectUser.FacilityUser.UserID = user.ID
-						assert.Equal(t, expectUser, user)
-						return nil
-					})
-			},
-			input: &user.CreateFacilityUserInput{
-				ProducerID:    "producer-id",
-				ProviderType:  entity.UserAuthProviderTypeLINE,
-				ProviderID:    "external-id",
-				Lastname:      "田中",
-				Firstname:     "太郎",
-				LastnameKana:  "たなか",
-				FirstnameKana: "たろう",
-				Email:         "test@example.com",
-				PhoneNumber:   "+819012345678",
-				LastCheckInAt: lastCheckInAt,
-			},
-			expectErr: nil,
-		},
-		{
-			name:  "validation error - missing required fields",
-			setup: func(ctx context.Context, mocks *mocks) {},
-			input: &user.CreateFacilityUserInput{
-				ProducerID:   "",
-				ProviderType: entity.UserAuthProviderTypeLINE,
-				ProviderID:   "external-id",
-				Email:        "test@example.com",
-			},
-			expectErr: exception.ErrInvalidArgument,
-		},
-		{
-			name:  "validation error - invalid last checkin at (future date)",
-			setup: func(ctx context.Context, mocks *mocks) {},
-			input: &user.CreateFacilityUserInput{
-				ProducerID:    "producer-id",
-				ProviderType:  entity.UserAuthProviderTypeLINE,
-				ProviderID:    "external-id",
-				Lastname:      "田中",
-				Firstname:     "太郎",
-				LastnameKana:  "たなか",
-				FirstnameKana: "たろう",
-				Email:         "test@example.com",
-				PhoneNumber:   "+819012345678",
-				LastCheckInAt: now.Add(-time.Hour), // future date
-			},
-			expectErr: exception.ErrInvalidArgument,
-		},
-		{
-			name: "database error",
-			setup: func(ctx context.Context, mocks *mocks) {
-				mocks.db.FacilityUser.EXPECT().Create(ctx, gomock.Any()).Return(assert.AnError)
-			},
-			input: &user.CreateFacilityUserInput{
-				ProducerID:    "producer-id",
-				ProviderType:  entity.UserAuthProviderTypeLINE,
-				ProviderID:    "external-id",
-				Lastname:      "田中",
-				Firstname:     "太郎",
-				LastnameKana:  "たなか",
-				FirstnameKana: "たろう",
-				Email:         "test@example.com",
-				PhoneNumber:   "+819012345678",
-				LastCheckInAt: lastCheckInAt,
-			},
-			expectErr: exception.ErrInternal,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, testService(tt.setup, func(ctx context.Context, t *testing.T, service *service) {
-			_, err := service.CreateFacilityUser(ctx, tt.input)
-			assert.ErrorIs(t, err, tt.expectErr)
-		}, withNow(now)))
-	}
-}
-
 func TestGetFacilityUser(t *testing.T) {
 	t.Parallel()
 
@@ -274,9 +163,121 @@ func TestGetFacilityUser(t *testing.T) {
 	}
 }
 
+func TestCreateFacilityUser(t *testing.T) {
+	t.Parallel()
+
+	now := jst.Date(2025, 8, 26, 12, 0, 0, 0)
+	lastCheckInAt := jst.Date(2025, 8, 27, 12, 0, 0, 0)
+
+	tests := []struct {
+		name      string
+		setup     func(ctx context.Context, mocks *mocks)
+		input     *user.CreateFacilityUserInput
+		expectErr error
+	}{
+		{
+			name: "success",
+			setup: func(ctx context.Context, mocks *mocks) {
+				expectUser := &entity.User{
+					Type:       entity.UserTypeFacilityUser,
+					Registered: false,
+					FacilityUser: entity.FacilityUser{
+						ExternalID:    "external-id",
+						ProducerID:    "producer-id",
+						Lastname:      "田中",
+						Firstname:     "太郎",
+						LastnameKana:  "たなか",
+						FirstnameKana: "たろう",
+						ProviderType:  entity.UserAuthProviderTypeLINE,
+						Email:         "test@example.com",
+						PhoneNumber:   "+819012345678",
+						LastCheckInAt: lastCheckInAt,
+					},
+				}
+				mocks.db.FacilityUser.EXPECT().
+					Create(ctx, gomock.Any()).
+					DoAndReturn(func(_ context.Context, user *entity.User) error {
+						expectUser.ID = user.ID
+						expectUser.FacilityUser.UserID = user.ID
+						assert.Equal(t, expectUser, user)
+						return nil
+					})
+			},
+			input: &user.CreateFacilityUserInput{
+				ProducerID:    "producer-id",
+				ProviderType:  entity.UserAuthProviderTypeLINE,
+				ProviderID:    "external-id",
+				Lastname:      "田中",
+				Firstname:     "太郎",
+				LastnameKana:  "たなか",
+				FirstnameKana: "たろう",
+				Email:         "test@example.com",
+				PhoneNumber:   "+819012345678",
+				LastCheckInAt: lastCheckInAt,
+			},
+			expectErr: nil,
+		},
+		{
+			name:  "validation error - missing required fields",
+			setup: func(ctx context.Context, mocks *mocks) {},
+			input: &user.CreateFacilityUserInput{
+				ProducerID:   "",
+				ProviderType: entity.UserAuthProviderTypeLINE,
+				ProviderID:   "external-id",
+				Email:        "test@example.com",
+			},
+			expectErr: exception.ErrInvalidArgument,
+		},
+		{
+			name:  "validation error - invalid last checkin at (future date)",
+			setup: func(ctx context.Context, mocks *mocks) {},
+			input: &user.CreateFacilityUserInput{
+				ProducerID:    "producer-id",
+				ProviderType:  entity.UserAuthProviderTypeLINE,
+				ProviderID:    "external-id",
+				Lastname:      "田中",
+				Firstname:     "太郎",
+				LastnameKana:  "たなか",
+				FirstnameKana: "たろう",
+				Email:         "test@example.com",
+				PhoneNumber:   "+819012345678",
+				LastCheckInAt: now.Add(-time.Hour), // past date
+			},
+			expectErr: exception.ErrInvalidArgument,
+		},
+		{
+			name: "database error",
+			setup: func(ctx context.Context, mocks *mocks) {
+				mocks.db.FacilityUser.EXPECT().Create(ctx, gomock.Any()).Return(assert.AnError)
+			},
+			input: &user.CreateFacilityUserInput{
+				ProducerID:    "producer-id",
+				ProviderType:  entity.UserAuthProviderTypeLINE,
+				ProviderID:    "external-id",
+				Lastname:      "田中",
+				Firstname:     "太郎",
+				LastnameKana:  "たなか",
+				FirstnameKana: "たろう",
+				Email:         "test@example.com",
+				PhoneNumber:   "+819012345678",
+				LastCheckInAt: lastCheckInAt,
+			},
+			expectErr: exception.ErrInternal,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, testService(tt.setup, func(ctx context.Context, t *testing.T, service *service) {
+			_, err := service.CreateFacilityUser(ctx, tt.input)
+			assert.ErrorIs(t, err, tt.expectErr)
+		}, withNow(now)))
+	}
+}
+
 func TestUpdateFacilityUser(t *testing.T) {
 	t.Parallel()
 
+	now := jst.Date(2025, 8, 26, 12, 0, 0, 0)
 	lastCheckInAt := jst.Date(2025, 8, 27, 12, 0, 0, 0)
 
 	tests := []struct {
@@ -395,7 +396,7 @@ func TestUpdateFacilityUser(t *testing.T) {
 				LastnameKana:  "たなか",
 				FirstnameKana: "たろう",
 				PhoneNumber:   "+819012345678",
-				LastCheckInAt: jst.Now().Add(time.Hour), // future date
+				LastCheckInAt: now.Add(-time.Hour), // past date
 			},
 			expectErr: exception.ErrInvalidArgument,
 		},
@@ -421,6 +422,6 @@ func TestUpdateFacilityUser(t *testing.T) {
 		t.Run(tt.name, testService(tt.setup, func(ctx context.Context, t *testing.T, service *service) {
 			err := service.UpdateFacilityUser(ctx, tt.input)
 			assert.ErrorIs(t, err, tt.expectErr)
-		}))
+		}, withNow(now)))
 	}
 }
